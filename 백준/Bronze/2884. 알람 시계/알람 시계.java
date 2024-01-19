db845dccb314316abd702272dd137e2a230ed018
sungholion <102495970+sungholion@users.noreply.github.com>
2024-01-19 10:50:33 +0900

    [Bronze III] Title: 알람 시계, Time: 152 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] str = br.readLine().split(" ");
+		int h = Integer.parseInt(str[0]);
+		int m = Integer.parseInt(str[1]);
+		
+		if (m - 45 < 0) {
+			h-= 1;
+			if(h < 0)
+				h = 23;
+			m += 15;
+		}
+		else
+			m -= 45;
+		
+		System.out.println(h + " " + m);
+	}
+}
