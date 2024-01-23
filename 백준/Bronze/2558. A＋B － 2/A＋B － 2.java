bd919407e37c4091485932af0cc83b9f71d13682
sungholion <102495970+sungholion@users.noreply.github.com>
2024-01-23 15:59:06 +0900

    [Bronze V] Title: A+B - 2, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.spi.FileSystemProvider;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		System.out.println(a+b);
+		
+		
+	}
+}
+
+
+
