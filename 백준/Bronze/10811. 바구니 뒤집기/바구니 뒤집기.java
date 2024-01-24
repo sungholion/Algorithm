8234aaaea2a9cb75ae8ac8c2999b72d643908faf
sungholion <102495970+sungholion@users.noreply.github.com>
2024-01-24 12:21:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 148 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n+1];
+		
+		for(int i=1; i<n+1; i++) 
+			arr[i] = i;
+			
+		
+		for(int cnt=0; cnt<m; cnt++) {
+			st = new StringTokenizer(br.readLine());
+			int i= Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int temp;
+			
+			while(i<j) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				i++;
+				j--;
+			}
+			
+		}
+
+		for(int i=1; i<n+1; i++)
+			System.out.print(arr[i] + " ");
+	}
+}
