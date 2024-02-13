db650aeb503466390419a64365e01b626498160b
조성호 <102495970+sungholion@users.noreply.github.com>
2024-02-13 17:32:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		long[] table = new long[1001];
+		
+		table[1] = 1;
+		table[2] = 2;
+		
+		for(int i=3; i<=n; i++)
+			table[i] = (table[i-2] + table[i-1]) % 10007;
+		
+		System.out.println(table[n]);
+		
+	}
+	
+}
