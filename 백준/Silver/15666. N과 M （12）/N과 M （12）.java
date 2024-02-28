229123f126bcf6a777a9ca5dae759bf01967f538
조성호 <102495970+sungholion@users.noreply.github.com>
2024-02-28 15:37:20 +0900

    [Silver II] Title: N과 M (12), Time: 280 ms, Memory: 24964 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,75 @@
+/*
+문제
+N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
+N개의 자연수 중에서 M개를 고른 수열
+
+입력
+첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+출력
+한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] num;
+	static int[] input;
+	static boolean[] visited;
+	static HashSet<String> set = new LinkedHashSet<>();
+	static StringBuilder sb = new StringBuilder();
+	static int o;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		num = new int[M];
+		visited = new boolean[N+1];
+		input = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			input[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(input);
+		
+		dfs(0, 0);
+		Iterator iter = set.iterator();
+		while(iter.hasNext()){
+		    System.out.println(iter.next());
+		}
+		    
+	}
+	
+	static void dfs(int start, int depth) {
+		if (depth == M) {
+			sb.setLength(0);
+			
+			for(int x : num) {
+				sb.append(x).append(" ");
+			}
+			set.add(sb.toString());
+			
+			return;
+		}
+		
+		for(int i=start; i<N; i++) {
+				num[depth] = input[i];
+				dfs(i, depth + 1);
+		}
+		
+	}
+	
+}
