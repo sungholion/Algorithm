63f7a5d6ed8561852233d6968a01346dbf980888
조성호 <102495970+sungholion@users.noreply.github.com>
2024-03-12 17:39:03 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1568 ms, Memory: 329936 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Node implements Comparable<Node>{
+    int A;
+    int B;
+    int val;
+
+    Node(int A, int B, int val){
+        this.A = A;
+        this.B = B;
+        this.val = val;
+    }
+
+    @Override
+    public int compareTo(Node a){
+        return this.val - a.val;
+    }
+
+}
+
+public class Main
+{
+    static int V, E;
+    static ArrayList<Node> list = new ArrayList<>();
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < E; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int val = Integer.parseInt(st.nextToken());
+            list.add(new Node(A, B, val));
+        }
+
+        Collections.sort(list);
+
+        parent = new int[V + 1];
+        for(int i = 1; i <= V; i++)
+            parent[i] = i;
+
+        int sum = 0;
+        int max = -1;
+        for(Node a : list){
+            int A = a.A;
+            int B = a.B;
+            int val = a.val;
+
+            if(find(A) == find(B))
+                continue;
+
+            sum += val;
+            union(A, B);
+            max = val;
+        }
+
+        bw.write(sum-max + "");
+        bw.close();
+        br.close();
+    }
+
+    static int find(int A){
+        if(parent[A] == A)
+            return A;
+        else
+            return parent[A] = find(parent[A]);
+    }
+
+    static void union(int A, int B){
+        int a = find(A);
+        int b = find(B);
+
+        if(a != b)
+            parent[b] = a;
+    }
+
+}
