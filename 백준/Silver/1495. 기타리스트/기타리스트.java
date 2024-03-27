382d16f422e2c11dedb848a084d20d786d48acfe
조성호 <102495970+sungholion@users.noreply.github.com>
2024-03-27 11:39:47 +0900

    [Silver I] Title: 기타리스트, Time: 152 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -1,60 +1,56 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-
-	static int N, S, M;
-	static int[] arr;
-
-	public static void main(String[] args) throws Exception {
-
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] input = br.readLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		S = Integer.parseInt(input[1]);
-		M = Integer.parseInt(input[2]);
-
-		arr = new int[M + 1];
-
-		for (int i = 0; i <= M; i++) {
-			arr[i] = -1;
-		}
-		arr[S] = 0;
-
-		input = br.readLine().split(" ");
-
-		for (int i = 1; i <= N; i++) {
-			int V = Integer.parseInt(input[i - 1]);
-			List<Integer> list = new ArrayList<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());	// 곡 수
+		int s = Integer.parseInt(st.nextToken());	// 시작 볼륨
+		int m = Integer.parseInt(st.nextToken());	// 최대 볼륨
+		
+		int[] cur_vol = new int[m+1];	// 조절후 볼륨 기록할 배열
+		for(int i=0; i<=m; i++)			// 초기화
+			cur_vol[i] = -1;
+		cur_vol[s] = 0;					// 초기 음량 ex) 음량이 s인 곡은 0번째이다.
+		
+		st = new StringTokenizer(br.readLine());	// 곡마다 조절할 수 있는 음량
+		for(int i=1; i<=n; i++) {
+			int cur_control = Integer.parseInt(st.nextToken());
+			ArrayList<Integer> control = new ArrayList<>();
 			
-			for (int j = 0; j <= M; j++) {
-				if (arr[j] == i - 1) { // i-1번째에서 가능한 볼륨들을 찾는다.
-					int res1 = j + V;
-					int res2 = j - V;
-
-					if (0 <= res1 && res1 <= M)
-						list.add(res1);
-					if (0 <= res2 && res2 <= M)
-						list.add(res2);
+			for(int j=0; j<=m; j++) {
+				if(cur_vol[j] == i-1) {
+					int c_plus = j + cur_control;
+					int c_minus = j - cur_control;
+					
+					if(c_plus >=0 && c_plus <= m) 
+						control.add(c_plus);
+					if(c_minus >= 0 & c_minus <= m) 
+						control.add(c_minus);
 				}
 			}
 			
-			for (int n : list) {
-				arr[n] = i;
+			for(int c : control) 
+				cur_vol[c] = i;
+		}
+		
+	
+		int ans = -1;
+		for(int i=0; i<=m; i++) {
+			if(cur_vol[i] == n) {
+				if(ans <= i) ans = i;
 			}
+				
 		}
-
-		int max = -1;
-		for (int i = 0; i <= M; i++) {
-			if (arr[i] == N)
-				max = Math.max(max, i);
-		}
-
-		bw.write(max + "\n");
+		
+		bw.write(ans + "");
 		bw.flush();
-
+		bw.close();
+		br.close();
 	}
-
+	
 }
