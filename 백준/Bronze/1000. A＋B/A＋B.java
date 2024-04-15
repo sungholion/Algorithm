4c656edcc965b91695afc783f5b11299cddd9a49
조성호 <102495970+sungholion@users.noreply.github.com>
2024-04-15 23:04:22 +0900

    [Bronze V] Title: A+B, Time: 140 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int result = a + b;
+        sb.append(result);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
