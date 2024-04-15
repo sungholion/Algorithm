88e4828ad0336bc529f532eaff92ef272abb22a8
조성호 <102495970+sungholion@users.noreply.github.com>
2024-04-15 23:35:49 +0900

    [Bronze V] Title: 시험 성적, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,19 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int score = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        if (score >= 90 && score <=100)
-            System.out.println("A");
-        else if (score >= 80 && score <90)
-            System.out.println("B");
-        else if (score >= 70 && score <80)
-            System.out.println("C");
-        else if (score >= 60 && score <70)
-            System.out.println("D");
-        else
-            System.out.println("F");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+
+        if(a>=90) sb.append("A");
+        else if(a>=80) sb.append("B");
+        else if (a>=70) sb.append("C");
+        else if(a>=60) sb.append("D");
+        else sb.append("F");
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
