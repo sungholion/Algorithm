219c2b9548925b6372bc8053114d22b7b4d0f469
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-21 13:03:21 +0900

    [Bronze V] Title: Hello World, Time: 92 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello World!");
-    }
+import java.util.*;
+import java.io.*;
+
+public class Main{
+   public static void main(String[] args) throws IOException{
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+       bw.write("Hello World!");
+       bw.flush();
+       bw.close();
+   }
 }
