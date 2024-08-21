8471cc8ecb9e6a5c85123cc636b81d138fd21a5b
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-21 13:02:43 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 96 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,28 +1,27 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+public class Main{
+   public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       StringBuilder sb = new StringBuilder();
+       StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+       int a = Integer.parseInt(st.nextToken());
+       int b = Integer.parseInt(st.nextToken());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+       if(a > b){
+          sb.append(">");
+       } else if(a < b){
+          sb.append("<");
+       } else {
+          sb.append("==");
+       }
 
-        if(a > b)
-            sb.append(">").append("\n");
-        else if(a < b)
-            sb.append("<").append("\n");
-        else
-            sb.append("==").append("\n");
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
+       bw.write(sb.toString());
+       bw.flush();
+       br.close();
+       bw.close();
+   }
 }
