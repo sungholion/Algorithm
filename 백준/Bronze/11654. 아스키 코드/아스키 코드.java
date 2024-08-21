a3305a3addc7ea252b418b3e93ab5384600f3be2
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-21 13:41:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,11 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class Main{
+   public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       StringBuilder sb = new StringBuilder();
 
-        char c = sc.next().charAt(0);
-        System.out.println((int)c);
+       int x = (int)br.readLine().charAt(0);
 
-    }
+       sb.append(x).append("\n");
+
+       bw.write(sb.toString());
+       bw.flush();
+       br.close();
+       bw.close();
+   }
 }
