151b3b960e87cfae4c5f93f49024ff3087d606af
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-21 12:56:43 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,10 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a-b);
-    }
+public class Main{
+   public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       StringBuilder sb = new StringBuilder();
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+       int a = Integer.parseInt(st.nextToken());
+       int b = Integer.parseInt(st.nextToken());
+       int result = a - b;
+
+       sb.append(result);
+       bw.write(sb.toString());
+       bw.flush();
+       br.close();
+       bw.close();
+   }
 }
