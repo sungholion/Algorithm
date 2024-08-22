da211a029d9975949bec5e86db628edf4ab0e113
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-22 14:12:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 116 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,39 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		char[] str = br.readLine().toCharArray();
-		
-		char[] alpa = new char[26];
-		char start = 'a';
-		for(int i=0; i<26; i++) {
-			alpa[i] = (char) ((int)start + i);
-		}
-		
-		int[] alpa_c = new int[26];
-		for(int i=0; i<26; i++)
-			alpa_c[i] = -1;
-		
-		for(int i=0; i<str.length; i++) {
-			for(int j=0; j<alpa.length; j++) {
-				if(str[i] == alpa[j] && alpa_c[j] == -1) {
-					alpa_c[j] = i;
-				}
-			}
-		}
-		
-		for(int i=0; i<alpa_c.length; i++) {
-			if(i == 25)
-				System.out.print(alpa_c[i]);
-			else
-				System.out.print(alpa_c[i] + " ");
-		}
-		
-	}
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      int[] cnt = new int[26];
+      for(int i = 0; i < 26; i++){
+         cnt[i] = -1;
+      }
+
+      String str = br.readLine();
+
+      for(int i = 0; i < str.length(); i++){
+         if(cnt[str.charAt(i) - 'a'] == -1){
+            cnt[str.charAt(i) - 'a'] = i;
+         }
+      }
+
+      for(int i = 0; i < 26; i++){
+         bw.write(cnt[i] + " ");
+      }
+
+
+      bw.flush();
+      br.close();
+      bw.close();
+   }
 }
-
-
-
