623a36a87e29fdb4a3b76c5abadcabe0d0185a04
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-22 10:17:05 +0900

    [Bronze III] Title: 알람 시계, Time: 124 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,25 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] str = br.readLine().split(" ");
-		int h = Integer.parseInt(str[0]);
-		int m = Integer.parseInt(str[1]);
-		
-		if (m - 45 < 0) {
-			h-= 1;
-			if(h < 0)
-				h = 23;
-			m += 15;
-		}
-		else
-			m -= 45;
-		
-		System.out.println(h + " " + m);
-	}
+      int h = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+
+      if(m - 45 >= 0){
+         m -= 45;
+      }
+      else if(m - 45 < 0){
+         h -= 1;
+         m = m + 60 - 45;
+      }
+
+      if(h < 0){
+         h+= 24;
+      }
+
+      bw.write(h + " " + m + "\n");
+      bw.flush();
+      br.close();
+      bw.close();
+   }
 }
