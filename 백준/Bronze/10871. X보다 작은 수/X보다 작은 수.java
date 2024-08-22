575f6837423828a10330f5191855583da924405d
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-22 10:00:30 +0900

    [Bronze V] Title: X보다 작은 수, Time: 164 ms, Memory: 17252 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,19 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int x = sc.nextInt();
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for ( int i = 0 ; i < n ; i++ )
-            arr[i] = sc.nextInt();
+      int n = Integer.parseInt(st.nextToken());
+      int x = Integer.parseInt(st.nextToken());
 
-        for ( int i = 0 ; i < n ; i++ ){
-            if (arr[i] < x)
-                System.out.print(arr[i] + " ");
-        }
+      st = new StringTokenizer(br.readLine());
+      int[] arr = new int[n];
+      for(int i = 0; i < n; i++){
+         arr[i] = Integer.parseInt(st.nextToken());
+         if(arr[i] < x){
+            bw.write(arr[i] + " ");
+         }
+      }
 
-    }
+      bw.flush();
+      br.close();
+      bw.close();
+   }
 }
