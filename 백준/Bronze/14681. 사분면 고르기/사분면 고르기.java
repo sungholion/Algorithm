5cfd81735742e6c332a39c01170287db21e5b611
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-22 14:29:38 +0900

    [Bronze V] Title: 사분면 고르기, Time: 96 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,18 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		
-		if (x > 0 && y > 0)
-			System.out.println(1);
-		else if (x < 0 && y > 0)
-			System.out.println(2);
-		else if (x < 0 && y < 0)
-			System.out.println(3);
-		else if (x > 0 && y < 0)
-			System.out.println(4);
-	}
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+      int x = Integer.parseInt(br.readLine());
+      int y = Integer.parseInt(br.readLine());
+
+      if(x > 0 && y > 0){
+         bw.write("1");
+      }
+      else if(x < 0 && y > 0){
+         bw.write("2");
+      }
+      else if(x < 0 && y < 0){
+         bw.write("3");
+      }
+      else if(x > 0 && y < 0){
+         bw.write("4");
+      }
+
+      bw.flush();
+      br.close();
+      bw.close();
+   }
 }
