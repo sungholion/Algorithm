ef0da920523fc63cd8ed18c0d2eece7882f0ee0b
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-23 16:50:52 +0900

    [Bronze I] Title: 팰린드롬수, Time: 96 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,39 +1,32 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        String n = "-1";
-        String ans = "";
-        while(true) {
-        	if(n.equals("0"))
-        		break;
-        	n = br.readLine();
-        	
-        	ans = "yes";
-        	for(int i=0; i<n.length()/2; i++) {
-        		if(n.charAt(i) != n.charAt(n.length()-i-1)) {
-        			ans = "no";
-        			break;
-        		}
-        	}
-        	
-        	if(!n.equals("0"))
-        		System.out.println(ans);
-        	
-        }
-     
-        
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+      while (true) {
+         String str = br.readLine();
+
+         boolean flag = true;
+         if (str.equals("0")) break;
+         for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)){
+               flag = false;
+               break;
+            }
+         }
+         
+         if (flag) bw.write("yes\n");
+         else bw.write("no\n");
+      }
+
+      bw.flush();
+      bw.close();
+      br.close();
+   }
 }
+
+
