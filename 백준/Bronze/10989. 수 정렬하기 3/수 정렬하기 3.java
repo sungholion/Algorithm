281f8f043fd714fce102a12e2d8dee964d845390
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-23 17:16:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2364 ms, Memory: 363444 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,29 +1,31 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        
-        for (int i = 0; i < n; i++) 
-            arr[i] = Integer.parseInt(br.readLine());
-        
-       Arrays.sort(arr);
-        
-        for (int i = 0; i < n; i++)
-            bw.write(arr[i] + "\n");
-        
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+
+      int n = Integer.parseInt(br.readLine());
+
+      int[] arr = new int[n];
+      for(int i = 0; i < n; i++){
+         arr[i] = Integer.parseInt(br.readLine());
+      }
+
+      Arrays.sort(arr);
+
+      for(int i = 0; i < n; i++){
+         sb.append(arr[i]).append("\n");
+      }
+
+
+      bw.write(sb.toString());
+      bw.flush();
+      bw.close();
+      br.close();
+   }
 }
+
+
