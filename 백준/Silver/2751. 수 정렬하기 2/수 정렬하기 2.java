5c5cc26a25e920c31e8bf51a8dde003ae5af4524
조성호 <102495970+sungholion@users.noreply.github.com>
2024-08-27 10:27:52 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1304 ms, Memory: 131348 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,27 +1,30 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
 
-public class Main {
+public class Main{
+   public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      int n = Integer.parseInt(br.readLine());
+      ArrayList<Integer> list = new ArrayList<>();
 
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer> lst = new ArrayList<>();
+      for(int i = 0; i < n; i++){
+         list.add(Integer.parseInt(br.readLine()));
+      }
 
-        for(int i=0; i<n; i++)
-            lst.add(Integer.parseInt(br.readLine()));
+      Collections.sort(list); // Array.sort는 시간 초과
 
-        Collections.sort(lst);
+      for(int x : list){
+         sb.append(x).append("\n");
+      }
 
-        for(int i : lst)
-            bw.write(i+"\n");
-
-        bw.flush();
-        bw.close();
-        br.close();
-
-    }
+      bw.write(sb.toString());
+      bw.flush();
+      bw.close();
+      br.close();
+   }
 }
+
+
