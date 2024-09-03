5ae2de2ceb1e042190edcb5c8b1bbb5c3d4efa83
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-03 17:27:37 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19336 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,58 +1,100 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine()); // 명령어 수
-		Queue<Integer> q = new LinkedList<>();
-		int[] arr = new int[n];
-		
-		int start = 0;
-		int end = 0;
-		for(int tc=0; tc<n; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String ord = st.nextToken();
-			if(ord.equals("push")) {
-				int x = Integer.parseInt(st.nextToken());
-				arr[end++] = x;
-			}
-			else if(ord.equals("pop")) {
-				if(end == start)
-					bw.write(-1 + "\n");
-				else
-					bw.write(arr[start++] + "\n");
-			}
-			else if(ord.equals("size")) {
-				bw.write(end-start + "\n");
-			}
-			else if(ord.equals("empty")) {
-				if(end == start) bw.write(1 + "\n");
-				else bw.write(0 + "\n");
-			}
-			else if(ord.equals("front")) {
-				if(end == start)
-					bw.write(-1 + "\n");
-				else
-					bw.write(arr[start] + "\n");
-			}
-			else if(ord.equals("back")) {
-				if(end == start)
-					bw.write(-1 + "\n");
-				else
-					bw.write(arr[end-1] + "\n");
-			}
-		}
-		bw.flush();
-		bw.close();
-		br.close();
-	}	
+   public static int[] q;
+   public static int f;
+   public static int r;
+
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringBuilder sb = new StringBuilder();
+
+      StringTokenizer st;
+      int N = Integer.parseInt(br.readLine());
+      q = new int[N];
+
+      while (N-- > 0) {
+         st = new StringTokenizer(br.readLine());
+
+         switch (st.nextToken()) {
+            case "push":
+               push(Integer.parseInt(st.nextToken()));
+               break;
+
+            case "pop":
+               sb.append(pop()).append('\n');
+               break;
+
+            case "size":
+               sb.append(size()).append('\n');
+               break;
+
+            case "empty":
+               sb.append(empty()).append('\n');
+               break;
+
+            case "front":
+               sb.append(front()).append('\n');
+               break;
+
+            case "back":
+               sb.append(back()).append('\n');
+               break;
+         }
+      }
+
+      bw.write(sb.toString());
+      bw.flush();
+      bw.close();
+      br.close();
+   }
+
+   public static void push(int item) {
+      q[r++] = item;
+   }
+
+   public static int pop() {
+      if((r-f) == 0) {
+         return -1;
+      }
+      else {
+         int res = q[f++];
+         q[f - 1] = 0;
+         return res;
+      }
+   }
+
+   public static int size() {
+      return r - f;
+   }
+
+   public static int empty() {
+      if((r-f) == 0) {
+         return 1;
+      }
+      else {
+         return 0;
+      }
+   }
+
+   public static int front() {
+      if((r-f) == 0) {
+         return -1;
+      }
+      else {
+         return q[f];
+      }
+   }
+
+   public static int back() {
+      if((r-f) == 0) {
+         return -1;
+      }
+      else {
+         return q[r-1];
+      }
+   }
+
 }
