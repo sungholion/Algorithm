c515eeb82b1106fa9db7ddd975af1d0f14818769
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-04 13:54:32 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 45664 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,34 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=1; i<=n; i++)
-        	q.add(i);
-        
-        while(q.size()!=1) {
-        	q.poll();	// 제일 위 카드 버리기
-        	int re_insert = q.poll();
-        	q.add(re_insert);
-        }
-        
-        bw.write(q.poll() + "\n");
-        
-   
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+      int n = Integer.parseInt(br.readLine());
+      Queue<Integer> q= new LinkedList<>();
+
+      for(int i = 1; i <= n; i++){
+         q.offer(i);
+      }
+
+      while(true){
+         if(q.size() != 1){
+            q.poll();
+            q.offer(q.poll());
+         } else {
+            bw.write(q.poll() + "\n");
+            break;
+         }
+      }
+
+      bw.flush();
+      bw.close();
+      br.close();
+   }
 }
