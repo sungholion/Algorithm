b929b348cf7ffb90a1e541b2745d54e299b27c65
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-04 17:26:06 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,51 +1,41 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int tc = Integer.parseInt(br.readLine());
-		
-		for(int loop=0; loop < tc; loop++) {
-			String input = br.readLine();
-			
-			Stack<Character> s = new Stack<>();
-			boolean pair = true;
-			for(int i=0; i<input.length(); i++) {
-				if(input.charAt(i) == '(') {
-					s.push(input.charAt(i));
-				}
-				else if(input.charAt(i) == ')') {
-					if(!s.isEmpty()) {
-						if(s.pop()!= '(') {
-							pair = false;
-							break;
-						}
-						else continue;
-					}
-					else 
-						pair = false;
-					
-				}
-			}
-			if(!s.isEmpty() || pair == false) {
-				bw.write("NO\n");
-			}
-			else {
-				bw.write("YES\n");
-			}
-			
-		}
-		
-		
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+public class Main {
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+      int n = Integer.parseInt(br.readLine());
+      while (n-- > 0) {
+         String s = br.readLine();
+         Stack<Character> stack = new Stack<>();
+
+         boolean flag = true;
+         for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '('){
+               stack.push(c);
+            } else if(c == ')'){
+               if(!stack.isEmpty()){
+                  char pop = stack.pop();
+                  if(pop != '(') flag = false;
+               } else {
+                  flag = false;
+               }
+            }
+         }
+
+         if(!stack.isEmpty()){flag = false;}
+         if(flag){
+            bw.write("YES\n");
+         } else {
+            bw.write("NO\n");
+         }
+      }
+
+      bw.flush();
+      bw.close();
+      br.close();
+   }
 }
