931979faed87138ea509e505d872bb3327a16e4c
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-05 17:20:22 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 136 ms, Memory: 24608 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,35 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	int N = Integer.parseInt(st.nextToken());
-	int K = Integer.parseInt(st.nextToken());
-	
-	LinkedList<Integer> lst = new LinkedList<>();
-	for(int i=1; i<=N; i++) lst.add(i);
-	
-	bw.write("<");
-	int next = 0;
-	while(!lst.isEmpty()) {
-		next = (next + K -1) % lst.size();
-		bw.write(String.valueOf(lst.get(next)));
-		if(lst.size()!=1) bw.write(", ");
-		lst.remove(next);
-	}
-	bw.write(">");
-	
-	bw.flush();
-	bw.close();
-	br.close();
-	
-	}
+public class Main {
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      StringBuilder sb = new StringBuilder();
+
+      int n = Integer.parseInt(st.nextToken());
+      int k = Integer.parseInt(st.nextToken());
+
+      Queue<Integer> q = new LinkedList<>();
+
+      for (int i = 1; i <= n; i++) {
+         q.offer(i);
+      }
+
+      sb.append("<");
+      while (q.size() > 1) {
+         for(int i = 0; i < k-1; i++){
+            q.offer(q.poll());
+         }
+
+         sb.append(q.poll()).append(", ");
+      }
+      sb.append(q.poll()).append(">");
+
+      bw.write(sb.toString());
+
+      bw.flush();
+      bw.close();
+      br.close();
+   }
 }
