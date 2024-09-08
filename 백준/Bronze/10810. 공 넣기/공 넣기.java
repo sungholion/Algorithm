a1dbe8a74c7ee8cd4858ae97e53ab1cb9a1410d1
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-08 15:34:06 +0900

    [Bronze III] Title: 공 넣기, Time: 120 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,29 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[] arr = new int[n+1];
-		
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int ball_num = Integer.parseInt(st.nextToken());
-			
-			for(int j=start; j<=end; j++) {
-				arr[j] = ball_num;
-			}
-		}
-		
-		for(int i=1; i<n+1; i++)
-			System.out.print(arr[i] + " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        for(int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for(int l = i; l <= j; l++){
+                arr[l] = k;
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            bw.write(arr[i] + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
