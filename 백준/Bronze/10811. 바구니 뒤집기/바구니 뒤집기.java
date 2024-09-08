b6285d8d0360e220587e676fc1c2650085e8becb
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-08 15:50:31 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15880 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,37 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[] arr = new int[n+1];
-		
-		for(int i=1; i<n+1; i++) 
-			arr[i] = i;
-			
-		
-		for(int cnt=0; cnt<m; cnt++) {
-			st = new StringTokenizer(br.readLine());
-			int i= Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			int temp;
-			
-			while(i<j) {
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				i++;
-				j--;
-			}
-			
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for(int i=1; i<n+1; i++)
-			System.out.print(arr[i] + " ");
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        for(int i = 1;i <= n; i++){
+            arr[i] = i;
+        }
+        int[] arrReverse = new int[n+1];
+
+        for(int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            while(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            bw.write(arr[i] + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
