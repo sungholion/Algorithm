895cdb06a0108061d42b69c9085cad800d1ecc49
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-08 15:36:27 +0900

    [Bronze II] Title: 공 바꾸기, Time: 116 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,34 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[] arr = new int[n+1];
-		
-		for(int i=1; i<n+1; i++) 
-			arr[i] = i;
-			
-		
-		for(int cnt=0; cnt<m; cnt++) {
-			st = new StringTokenizer(br.readLine());
-			int i= Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			int temp;
-			
-			temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-			
-		}
-		
-		for(int i=1; i<n+1; i++)
-			System.out.print(arr[i] + " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+        for(int i = 1;i <= n; i++){
+            arr[i] = i;
+        }
+
+        for(int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        for(int i = 1; i <= n; i++){
+            bw.write(arr[i] + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
