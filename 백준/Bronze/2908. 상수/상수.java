79e9c518a66f2cc2a2803ae43569ae822228d11e
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-08 15:55:47 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 16200 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,26 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String input = br.readLine();
-		StringBuffer str = new StringBuffer(input);
-		String reversedStr = str.reverse().toString();
-		
-		String[] org = reversedStr.split(" ");
-		
-		int a = Integer.parseInt(org[0]);
-		int b = Integer.parseInt(org[1]);
-		
-		if(a > b)
-			System.out.println(a);
-		else
-			System.out.println(b);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String a = st.nextToken();
+        String b = st.nextToken();
+
+        a = String.valueOf(a.charAt(2) + "" + a.charAt(1) + a.charAt(0));
+        b = String.valueOf(b.charAt(2) + "" + b.charAt(1) + b.charAt(0));
+
+        if(Integer.parseInt(a) > Integer.parseInt(b)) {
+            bw.write(a);
+        } else {
+            bw.write(b);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
-
-
-
