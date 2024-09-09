021f389bc63a3d3672e833aed278b21a247b3f5f
조성호 <102495970+sungholion@users.noreply.github.com>
2024-09-09 11:26:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -6,22 +6,24 @@ public class Main {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-      int n = Integer.parseInt(br.readLine());
+      int N = Integer.parseInt(br.readLine()); //배달해야하는 무게가 총 몇 kg
+      br.close();
 
-      if (n == 4 || n == 7) {
-         bw.write(-1 + "\n");
-      }
-      else if (n % 5 == 0) {
-         bw.write(n / 5 + "\n");
-      }
-      else if (n % 5 == 1 || n % 5 == 3) {
-         bw.write((n / 5) + 1 + "\n");
-      }
-      else if (n % 5 == 2 || n % 5 == 4) {
-         bw.write((n / 5) + 2 + "\n");
-      }
+      int basket =  0; //총 몇개의 봉지가 들어가는지
 
-
+      while(N>0){
+         if(N % 5  == 0){ //5로 나누어지면
+            basket += N/5;
+            break;
+         }else { //만약 아니라면 3KG이거나 봉지에 담지 나눠담지 못하는 것.
+            N -= 3;
+            basket++;
+         }
+         if(N < 0){ //차피 나눠지지 않는다면 else문에서 -3을 했기 때문에 0보다 작을 것임...
+            basket = -1;
+         }
+      }
+      bw.write(basket + "");
       bw.flush();
       bw.close();
       br.close();
