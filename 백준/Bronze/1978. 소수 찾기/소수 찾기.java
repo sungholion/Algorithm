a3231d3877db700e1e92e92bff793a083d1edb6c
조성호 <102495970+sungholion@users.noreply.github.com>
2024-10-01 14:57:21 +0900

    [Bronze II] Title: 소수 찾기, Time: 116 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,21 +1,34 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int total = 0;
-		for(int l=0; l<n; l++) {
-			int input = sc.nextInt();
-			int cnt = 0;
-			for(int i=2; i<=input; i++) {
-				if(input % i == 0)
-					cnt++;
-			}
-			if(cnt == 1)
-				total++;
-		}
-		System.out.println(total);
-	}
+public class Main {
+
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+      int n = Integer.parseInt(br.readLine());
+      int total = 0;
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int i = 0; i < n; i++) {
+         int a = Integer.parseInt(st.nextToken());
+
+         int cnt = 0;
+         for(int j = 1; j<= a; j++){
+            if (a % j == 0) {
+               cnt++;
+            }
+         }
+         if(cnt == 2){
+            total++;
+         }
+      }
+
+      bw.write(total+"\n");
+      bw.flush();
+      br.close();
+      bw.close();
+
+   }
+
 }
