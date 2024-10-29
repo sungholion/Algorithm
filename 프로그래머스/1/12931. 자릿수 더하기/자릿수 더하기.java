35c7acf231608369910f05be6a9cc8788a30b184
조성호 <102495970+sungholion@users.noreply.github.com>
2024-10-29 19:42:44 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,17 +1,10 @@
-import java.util.*;
-
 public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        int div = 1;
-        
-        while(true){
-            answer += ((n%(div*10))/div);
-            div*= 10;
-            if(div > n){
-                break;
-            }
+        while (n != 0) {
+            answer += n % 10;
+            n /= 10;
         }
 
         return answer;
