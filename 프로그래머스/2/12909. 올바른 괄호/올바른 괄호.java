334296e5569540d63c593e51a64c65357247d766
조성호 <102495970+sungholion@users.noreply.github.com>
2024-10-31 20:34:05 +0900

    [level 2] Title: 올바른 괄호, Time: 17.60 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -7,21 +7,16 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push(s.charAt(i));
-            } 
-            else if(s.charAt(i) == ')')
-            {    
-                if(stack.isEmpty()){
-                   answer = false;
+            char c = s.charAt(i);
+            
+            if(c == '('){
+                stack.push(c);
+            } else if(c == ')'){
+                if(!stack.isEmpty()){
+                    stack.pop();    
+                } else{
+                    answer = false;
                     break;
-                }
-                else{
-                    char p = stack.pop();
-                    if(p!= '('){
-                        answer = false;
-                        break;
-                    }
                 }
             }
         }
@@ -29,8 +24,7 @@ class Solution {
         if(!stack.isEmpty()){
             answer = false;
         }
-        
-        
+  
         return answer;
     }
 }
