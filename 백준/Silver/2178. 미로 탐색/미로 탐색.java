8a2f4295b62b6428032f354fe03c998a7a6f8b1f
조성호 <102495970+sungholion@users.noreply.github.com>
2024-12-10 13:54:24 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,79 +1,73 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Coord{
-	int x;
-	int y;
-	
-	public Coord(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    int x;
+    int y;
+
+    public Coord(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
 
-public class Main {
-	static int n, m;
-	static int[][] maze;
-	static int[][] visited;
-	static int minGo;
-	static int[] dx = {-1, 1, 0, 0};	// 상하좌우
-	static int[] dy = {0, 0, -1, 1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		maze = new int[n+1][m+1];
-		visited = new int[n+1][m+1];
-		
-		for(int i=1; i<=n; i++) {
-			String input = br.readLine();
-			for(int j=1; j<=m; j++) {
-				maze[i][j] = input.charAt(j-1) - '0';
-			}
-		}
-		
-		bfs(new Coord(1, 1));
-		
-		bw.write(visited[n][m] + "\n");
-		
-		bw.flush();
-		bw.close();
-		br.close();
-		
-	}
-	
-	static void bfs(Coord start) {
-		Queue<Coord> q = new ArrayDeque<>();
-		
-		visited[start.x][start.y] = 1;
-		q.offer(start);
-		
-		while(!q.isEmpty()) {
-			Coord cur = q.poll();
-			
-			for(int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if(nx >=1 && ny>=1 && nx <= n && ny <= m && visited[nx][ny] == 0 && maze[nx][ny] != 0) {
-					Coord next = new Coord(nx,ny);
-					visited[next.x][next.y] = visited[cur.x][cur.y] + 1;
-					q.offer(next);
-				}
-			}
-		}
-		
-		
-		return;
-	}
+public class Main{
+    static int n,m;
+    static int[][] map;
+    static int[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visited = new int[n][m];
+
+        for(int i=0; i<n; i++){
+            String temp = br.readLine();
+            for(int j=0; j<m; j++){
+                map[i][j] = temp.charAt(j) - '0';
+            }
+        }
+
+        bfs(new Coord(0, 0));
+
+        bw.write(visited[n-1][m-1] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void bfs(Coord start){
+        Queue<Coord> q = new ArrayDeque<>();
+        visited[start.x][start.y] = 1;
+        q.offer(start);
+
+        while(!q.isEmpty()){
+            Coord cur = q.poll();
+
+            for(int i=0; i<4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] != 0 || map[nx][ny] != 1){
+                    continue;
+                }
+
+                visited[nx][ny] = visited[cur.x][cur.y] + 1;
+                q.offer(new Coord(nx, ny));
+
+            }
+        }
+
+    }
+
+
 }
