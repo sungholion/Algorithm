6d896d80452320955e2d608f650f6e07397b94b5
조성호 <102495970+sungholion@users.noreply.github.com>
2024-12-12 15:12:34 +0900

    [Silver I] Title: 안전 영역, Time: 264 ms, Memory: 37276 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,88 +1,85 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Coord{
-	int x;
-	int y;
-	
-	public Coord(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    int x;
+    int y;
+
+    public Coord(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
 
-public class Main {
-	static int N;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
-	static int maxHeight;
-	static int maxSafeZone = 1;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] > maxHeight) maxHeight = map[i][j];
-			}
-		}
-		//---------------------------------------------------입력
-		
-		for(int rain=1; rain<=maxHeight; rain++) {
-			visited = new boolean[N][N];
-			int tmpSafeZone = 0;
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(map[i][j] > rain && !visited[i][j]) {
-						tmpSafeZone++;
-						bfs(new Coord(i,j), rain);
-					}
-				}
-			}
-			if(tmpSafeZone > maxSafeZone) maxSafeZone = tmpSafeZone;
-		}
-		
-		
-		bw.write(maxSafeZone + "");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	static void bfs(Coord start, int cur_rain) {
-		Queue<Coord> q = new ArrayDeque<>();
-		visited[start.x][start.y] = true;
-		q.offer(start);
-		
-		while(!q.isEmpty()) {
-			Coord cur = q.poll();
-			
-			for(int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if(nx >=0 && ny >= 0 && nx < N && ny <N && !visited[nx][ny] && map[nx][ny] > cur_rain) {
-					visited[nx][ny] = true;
-					q.offer(new Coord(nx, ny));
-				}
-			}
-		}
-		
-		return;
-	}
-	
+public class Main{
+    static int n;
+    static int[][] map;
+    static boolean[][] vis;
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+
+        int maxH = 0;
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                int x = Integer.parseInt(st.nextToken());
+                map[i][j] = x;
+                maxH = Math.max(maxH, x);
+            }
+        }
+
+        int maxCnt = 0;
+        for(int h=0; h<=maxH; h++){
+            vis = new boolean[n][n];
+            int cnt = 0;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(map[i][j] > h && !vis[i][j]){
+                        cnt++;
+                        bfs(new Coord(i, j), h);
+                    }
+                }
+            }
+            maxCnt = Math.max(maxCnt, cnt);
+        }
+
+        bw.write(maxCnt + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void bfs(Coord start, int h){
+        Queue<Coord> q = new ArrayDeque<>();
+
+        vis[start.x][start.y] = true;
+        q.offer(start);
+        int cnt = 1;
+
+        while(!q.isEmpty()){
+            Coord cur = q.poll();
+
+            for(int i=0; i<4; i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n || map[nx][ny] <= h || vis[nx][ny]){
+                    continue;
+                }
+
+                vis[nx][ny] = true;
+                q.offer(new Coord(nx, ny));
+            }
+        }
+    }
+
 }
