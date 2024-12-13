01884a63e3d0613ab27611862f8b47709946eea1
조성호 <102495970+sungholion@users.noreply.github.com>
2024-12-13 18:37:26 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 16188 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,142 +1,133 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
-class Point{
-	int x;
-	int y;
-	
-	Point(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
+class Coord{
+    int x;
+    int y;
+
+    public Coord(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
 
-public class Main {
-	static int n;
-	static char[][] map;
-	static boolean[][] visited;
-	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0 , -1, 1};
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(br.readLine());
-		map = new char[n][n];
-		
-		for(int i=0; i<n; i++) {
-			String str = br.readLine();
-			for(int j=0; j<n; j++) {
-				map[i][j] = str.charAt(j);
-			}
-		}
-		
-		int cnt_normal = 0;
-		visited = new boolean[n][n];
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(!visited[i][j]) {
-					bfs_nomal(new Point(i, j));
-					cnt_normal++;
-				}
-			}
-		}
-		
-		int cnt_red_green = 0;
-		visited = new boolean[n][n];
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(!visited[i][j]) {
-					bfs_red_green(new Point(i, j));
-					cnt_red_green++;
-				}
-			}
-		}
-		
-		sb.append(cnt_normal + " " + cnt_red_green);
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	static void bfs_nomal(Point start) {
-		Queue<Point> q = new LinkedList<>();
-		q.offer(start);
-		visited[start.x][start.y] = true;
-		
-		while(!q.isEmpty()) {
-			Point cur = q.poll();
-			
-			for(int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if(!rangeCheck(nx, ny)) continue;
-				
-				if(map[cur.x][cur.y] == map[nx][ny]) {	// 색깔 같으면
-					q.offer(new Point(nx, ny));
-					visited[nx][ny] = true;
-				}
-				
-				
-			}
-		}
-	}
-	
-	static void bfs_red_green(Point start) {
-		Queue<Point> q = new LinkedList<>();
-		q.offer(start);
-		visited[start.x][start.y] = true;
-		
-		while(!q.isEmpty()) {
-			Point cur = q.poll();
-			char cur_c = map[cur.x][cur.y];
-			
-			for(int i=0; i<4; i++) {
-				int nx = cur.x + dx[i];
-				int ny = cur.y + dy[i];
-				
-				if(!rangeCheck(nx, ny)) continue;
-				
-				if(cur_c == 'R') {
-					if(map[nx][ny] == 'R' || map[nx][ny] == 'G') {
-						q.offer(new Point(nx, ny));
-						visited[nx][ny] = true;
-					}
-				}
-				
-				else if(cur_c == 'G') {
-					if(map[nx][ny] == 'G' || map[nx][ny] == 'R') {
-						q.offer(new Point(nx, ny));
-						visited[nx][ny] = true;	
-					}
-				}
-				
-				else if(cur_c == 'B') {
-					if(map[nx][ny] == cur_c) {
-						q.offer(new Point(nx, ny));
-						visited[nx][ny] = true;
-					}
-				}
-				
-			
-				
-			}
-		}
-	}
+public class Main{
 
-	static boolean rangeCheck(int x, int y) {
-		if(x >=0 && y >=0 && x < n && y < n && !visited[x][y])
-			return true;
-		return false;
-	}
+    static int n;
+    static char[][] map;
+    static boolean[][] vis;
+    static int dx[] = {-1, 1, 0, 0};
+    static int dy[] = {0, 0, -1, 1};
+    static int bfsCnt;
+    static int redGreenBfsCnt;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        map = new char[n][n];
+
+        for(int i=0; i<n; i++){
+            String temp = br.readLine();
+            for(int j=0; j<n; j++){
+                map[i][j] = temp.charAt(j);
+            }
+        }
+
+        vis = new boolean[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j]){
+                    bfs(new Coord(i, j), map[i][j]);
+                }
+            }
+        }
+
+        vis = new boolean[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j]){
+                    redGreenBfs(new Coord(i, j), map[i][j]);
+
+                }
+            }
+        }
+
+        sb.append(bfsCnt + " " + redGreenBfsCnt + "\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    private static void bfs(Coord start, char color){
+        Queue<Coord> q = new ArrayDeque<>();
+        vis[start.x][start.y] = true;
+        bfsCnt++;
+        q.offer(start);
+
+        while(!q.isEmpty()){
+            Coord cur = q.poll();
+
+            for(int i=0; i<4; i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n || vis[nx][ny]){
+                    continue;
+                }
+
+                if(color == map[nx][ny]){
+                    vis[nx][ny] = true;
+                    q.offer(new Coord(nx, ny));
+                }
+
+            }
+        }
+
+    }
+
+    private static void redGreenBfs(Coord start, char color){
+        Queue<Coord> q = new ArrayDeque<>();
+        vis[start.x][start.y] = true;
+        redGreenBfsCnt++;
+        q.offer(start);
+
+        while(!q.isEmpty()){
+            Coord cur = q.poll();
+
+            for(int i=0; i<4; i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n || vis[nx][ny]){
+                    continue;
+                }
+
+                if(color == 'R'){
+                    if(map[nx][ny] == 'R' || map[nx][ny] == 'G'){
+                        vis[nx][ny] = true;
+                        q.offer(new Coord(nx, ny));
+                    }
+                } else if(color == 'G'){
+                    if(map[nx][ny] == 'G' || map[nx][ny] == 'R'){
+                        vis[nx][ny] = true;
+                        q.offer(new Coord(nx, ny));
+                    }
+                } else if(color == 'B'){
+                    if(map[nx][ny] == 'B'){
+                        vis[nx][ny] = true;
+                        q.offer(new Coord(nx, ny));
+                    }
+                }
+
+
+
+            }
+        }
+    }
 }
