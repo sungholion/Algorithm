b205efc7910cd354cdc1d6f2788e8ae9d268f1e9
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-01 19:24:36 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Coord{
     int x;
@@ -14,20 +12,23 @@ class Coord{
 }
 
 public class Main{
-    static int n,m;
+    static int n, m;
     static int[][] map;
-    static int[][] visited;
+    static int[][] vis;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+
         map = new int[n][m];
-        visited = new int[n][m];
+        vis = new int[n][m];
 
         for(int i=0; i<n; i++){
             String temp = br.readLine();
@@ -38,36 +39,34 @@ public class Main{
 
         bfs(new Coord(0, 0));
 
-        bw.write(visited[n-1][m-1] + "\n");
+        sb.append(vis[n-1][m-1]).append("\n");
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
-
     }
 
     static void bfs(Coord start){
         Queue<Coord> q = new ArrayDeque<>();
-        visited[start.x][start.y] = 1;
+        
+        vis[start.x][start.y] = 1;
         q.offer(start);
 
         while(!q.isEmpty()){
             Coord cur = q.poll();
-
-            for(int i=0; i<4; i++) {
+            
+            for(int i=0; i<4; i++){
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] != 0 || map[nx][ny] != 1){
+                
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m || map[nx][ny] == 0 || vis[nx][ny] != 0){
                     continue;
                 }
 
-                visited[nx][ny] = visited[cur.x][cur.y] + 1;
+                vis[nx][ny] = vis[cur.x][cur.y] + 1;
                 q.offer(new Coord(nx, ny));
-
             }
         }
-
     }
-
 
 }
