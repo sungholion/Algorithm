5a77af88a99e4108cf896a81c5c532522c607ea8
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-03 00:07:01 +0900

    [Gold V] Title: 상범 빌딩, Time: 164 ms, Memory: 17820 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -2,72 +2,65 @@ import java.io.*;
 import java.util.*;
 
 class Coord{
-    int z;
     int x;
     int y;
-    int time;
+    int z;
 
-    public Coord(int z, int x, int y, int time){
-        this.z = z;
+    public Coord(int x, int y, int z){
         this.x = x;
         this.y = y;
-        this.time = time;
+        this.z = z;
     }
 }
 
-public class Main{
-
-    static int l, r, c;
+public class Main {
+    static int L, R, C;
     static char[][][] map;
-    static boolean[][][] vis;
-    static int dz[] = {-1, 1, 0, 0, 0, 0};
-    static int dx[] = {0, 0, -1, 1, 0, 0};
-    static int dy[] = {0, 0, 0, 0, -1, 1};
+    static int[][][] vis;
+    static int[] dx = {-1, 1, 0, 0, 0, 0};
+    static int[] dy = {0, 0, -1, 1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
     static Coord start;
     static Coord end;
-    static int ans;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
         while(true){
-            st = new StringTokenizer(br.readLine());
-            l = Integer.parseInt(st.nextToken());
-            r = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            L = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
 
-            if(l == 0 && r == 0 && c == 0){
+            if(L == 0 && R == 0 && C == 0){
                 break;
             }
 
-            map = new char[l][r][c];
-            vis = new boolean[l][r][c];
+            map = new char[L][R][C];
+            vis = new int[L][R][C];
 
-            for(int i=0; i<l; i++){
-                for(int j=0; j<r; j++){
+            for(int l=0; l<L; l++){
+                for(int r=0; r<R; r++){
                     String temp = br.readLine();
-                    for(int k=0; k<c; k++){
-                        map[i][j][k] = temp.charAt(k);
-                        if(map[i][j][k] == 'S'){
-                            start = new Coord(i, j, k, 0);
-                        } else if(map[i][j][k] == 'E'){
-                            end = new Coord(i, j, k, 0);
+                    for(int c=0; c<C; c++){
+                        map[l][r][c] = temp.charAt(c);
+                        if(map[l][r][c] == 'S'){
+                            start = new Coord(l, r, c);
+                        } else if(map[l][r][c] == 'E'){
+                            end = new Coord(l, r, c);
                         }
                     }
                 }
-                String changeLine = br.readLine();
+                br.readLine();
             }
 
-            ans = bfs(start);
-
+            int ans = bfs();
             if(ans == -1){
                 sb.append("Trapped!\n");
-            } else {
-                sb.append("Escaped in " + ans + " minute(s).\n");
+            } else{
+                sb.append("Escaped in ").append(ans).append(" minute(s).\n");
             }
-
         }
 
         bw.write(sb.toString());
@@ -76,35 +69,31 @@ public class Main{
         br.close();
     }
 
-    private static int bfs(Coord start){
+    static int bfs(){
         Queue<Coord> q = new ArrayDeque<>();
-        vis[start.z][start.x][start.y] = true;
         q.offer(start);
+        vis[start.x][start.y][start.z] = 1; // 구분을 위함. 나중에 -1
 
         while(!q.isEmpty()){
             Coord cur = q.poll();
 
+            if((cur.x == end.x) && (cur.y == end.y) && (cur.z == end.z)){
+                return vis[cur.x][cur.y][cur.z] - 1;
+            }
+
             for(int i=0; i<6; i++){
-                int nz = cur.z + dz[i];
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
+                int nz = cur.z + dz[i];
 
-                if(nz < 0 || nx < 0 || ny < 0 || nz >= l || nx >= r || ny >= c || vis[nz][nx][ny] || map[nz][nx][ny] == '#'){
+                if(nx < 0 || ny < 0 || nz < 0 || nx >= L || ny >= R || nz >= C || map[nx][ny][nz] == '#' || vis[nx][ny][nz] != 0){
                     continue;
                 }
 
-                vis[nz][nx][ny] = true;
-                ans++;
-                q.offer(new Coord(nz, nx, ny, cur.time + 1));
-
-                if(map[nz][nx][ny] == 'E'){
-                    return cur.time + 1;
-                }
-
-
+                vis[nx][ny][nz] = vis[cur.x][cur.y][cur.z] + 1;
+                q.offer(new Coord(nx, ny, nz));
             }
         }
-
         return -1;
     }
 
