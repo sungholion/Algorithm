3aded061f9db4145d2ad0213628fc7ffcc6344cd
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-03 01:04:30 +0900

    [Gold V] Title: 토마토, Time: 624 ms, Memory: 96708 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -2,49 +2,49 @@ import java.io.*;
 import java.util.*;
 
 class Coord{
-    int z;
     int x;
     int y;
+    int z;
 
-    public Coord(int z, int x, int y){
-        this.z = z;
+    public Coord(int x, int y, int z){
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 }
 
-public class Main{
-    static int m, n, h;
+public class Main {
+    static int M, N, H;
     static int[][][] map;
-    static boolean[][][] vis;
-    static int dx[] = {-1, 1, 0, 0, 0, 0};
-    static int dy[] = {0, 0, -1, 1, 0, 0};
-    static int dz[] = {0, 0, 0, 0, -1, 1};
+    static int[] dx = {-1, 1, 0, 0, 0, 0};
+    static int[] dy = {0, 0, -1, 1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
     static Queue<Coord> q = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
-        map = new int[h][n][m];
-        vis = new boolean[h][n][m];
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<h; i++){
-            for(int j=0; j<n; j++){
+        map = new int[H][N][M];
+
+        for(int i=0; i<H; i++){
+            for(int j=0; j<N; j++){
                 st = new StringTokenizer(br.readLine());
-                for(int k=0; k<m; k++){
+                for(int k=0; k<M; k++){
                     map[i][j][k] = Integer.parseInt(st.nextToken());
                 }
             }
         }
 
-        for(int i=0; i<h; i++){
-            for(int j=0; j<n; j++){
-                for(int k=0; k<m; k++){
+        for(int i=0; i<H; i++){
+            for(int j=0; j<N; j++){
+                for(int k=0; k<M; k++){
                     if(map[i][j][k] == 1){
                         q.offer(new Coord(i, j, k));
                     }
@@ -52,31 +52,30 @@ public class Main{
             }
         }
 
-        bw.write(bfs() + "\n");
+        sb.append(bfs()).append("\n");
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
-
     }
 
     static int bfs(){
-
         while(!q.isEmpty()){
             Coord cur = q.poll();
 
             for(int i=0; i<6; i++){
-                int nz = cur.z + dz[i];
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
+                int nz = cur.z + dz[i];
 
-                if(nz < 0 || nx < 0 || ny < 0 || nz >= h || nx >= n || ny >= m || map[nz][nx][ny] != 0){
+                if(nx < 0 || ny < 0 || nz < 0 || nx >= H || ny >= N || nz >= M || map[nx][ny][nz] != 0){
                     continue;
                 }
 
-                map[nz][nx][ny] = map[cur.z][cur.x][cur.y] + 1;
-                q.offer(new Coord(nz, nx, ny));
-
+                map[nx][ny][nz] = map[cur.x][cur.y][cur.z] + 1;
+                q.offer(new Coord(nx, ny, nz));
             }
+
         }
 
         int max = Integer.MIN_VALUE;
@@ -84,31 +83,28 @@ public class Main{
         if(checkTomato()){
             return -1;
         } else{
-            for(int i=0; i<h; i++){
-                for(int j=0; j<n; j++){
-                    for(int k=0; k<m; k++){
-                        if(max < map[i][j][k]){
+            for(int i=0; i<H; i++){
+                for(int j=0; j<N; j++){
+                    for(int k=0; k<M; k++){
+                        if(map[i][j][k] > max){
                             max = map[i][j][k];
                         }
                     }
-
                 }
             }
         }
 
         return max - 1;
-
     }
 
-    private static boolean checkTomato(){
-        for(int i=0; i<h; i++){
-            for(int j=0; j<n; j++){
-                for(int k=0; k<m; k++){
+    static boolean checkTomato(){
+        for(int i=0; i<H; i++){
+            for(int j=0; j<N; j++){
+                for(int k=0; k<M; k++){
                     if(map[i][j][k] == 0){
                         return true;
                     }
                 }
-
             }
         }
         return false;
