0fde08bb7e39a5528c451ecda1796985e340e995
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-02 17:58:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,77 +1,88 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+class Coord{
+    int x;
+    int y;
+
+    public Coord(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main{
-	static int n;
-	static int[][] map;
-	static boolean[][] visited;
-	static int dx[] = {0, 0, -1, 1};
-	static int dy[] = {-1, 1, 0, 0};
-	static int cnt;
-	static List<Integer> ans;
-	
-	public static void dfs(int x, int y) {
-		visited[x][y] = true;
-		
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			
-			if(range_check(nx, ny)) {
-				cnt++;
-				dfs(nx, ny);
-			}
-		}
-	}
-	
-	public static boolean range_check(int x, int y) {
-		if(x>=0 && y>=0 && x<n && y<n && !visited[x][y] && map[x][y] == 1)
-			return true;
-		else return false;
-	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		map = new int[n][n];
-		visited = new boolean[n][n];
-		ans = new LinkedList<>();
-		
-		for(int i=0; i<n; i++){
-			String input = br.readLine();
-			for(int j=0; j<n; j++) {
-				map[i][j] = input.charAt(j) - '0';
-			}
-		}
-		
-		cnt = 1; // 첫 집도 세아림
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				if(map[i][j] == 1 && !visited[i][j]) {
-					dfs(i, j);
-					ans.add(cnt);
-					cnt = 1;
-				}
-			}
-		}
-		
-		Collections.sort(ans);
-		bw.write(ans.size() + "\n");
-		for(int a : ans)
-			bw.write(a + "\n");
-		
-		bw.flush();
-		br.close();
-		bw.close();
-	}
-	
+    static int n;
+    static int[][] map;
+    static boolean[][] vis;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int total;
+    static LinkedList<Integer> list = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        vis = new boolean[n][n];
+
+        for(int i=0; i<n; i++){
+            String temp = br.readLine();
+            for(int j=0; j<n; j++){
+                map[i][j] = temp.charAt(j) - '0';
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map[i][j] == 1 && !vis[i][j]){
+                    total++;
+                    bfs(new Coord(i,j));
+                }
+            }
+        }
+
+        sb.append(total).append("\n");
+
+        Collections.sort(list);
+        for(int area : list){
+            sb.append(area).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void bfs(Coord start){
+        Queue<Coord> q = new ArrayDeque<>();
+
+        vis[start.x][start.y] = true;
+        q.offer(start);
+        int area = 1;
+        
+        while(!q.isEmpty()){
+            Coord cur = q.poll();
+
+            for(int i=0; i<4; i++){
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n || vis[nx][ny] || map[nx][ny] == 0){
+                    continue;
+                }
+
+                vis[nx][ny] = true;
+                q.offer(new Coord(nx, ny));
+                area++;
+            }
+        }
+
+        list.add(area);
+    }
+    
 }
