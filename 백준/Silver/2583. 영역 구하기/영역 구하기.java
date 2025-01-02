21011c51a5f92ce9b702764f331e50618bdc5cb4
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-02 20:15:52 +0900

    [Silver I] Title: 영역 구하기, Time: 112 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -2,103 +2,98 @@ import java.io.*;
 import java.util.*;
 
 class Coord{
-    int y;
     int x;
+    int y;
 
-    public Coord(int y, int x){
-        this.y = y;
+    public Coord(int x, int y){
         this.x = x;
+        this.y = y;
     }
 }
 
 public class Main{
     static int m, n, k;
     static int[][] map;
-    static boolean[][] visited;
+    static boolean[][] vis;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static List<Integer> list = new LinkedList<>();
-    public static void main(String[] args) throws IOException{
+    static int total;
+    static LinkedList<Integer> list = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
+        
         map = new int[m][n];
-        visited = new boolean[m][n];
-
-        for(int kc=0; kc<k; kc++){
+        vis = new boolean[m][n];
+        
+        for(int kc = 0; kc < k; kc++){
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+            int startX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
 
-            for(int i = y1; i < y2; i++){
-                for(int j = x1; j < x2; j++){
-                    map[i][j] = -1;
+            for(int i = startX; i < endX; i++){
+                for(int j = startY; j < endY; j++){
+                    map[i][j] = 1;
                 }
             }
         }
 
-        int cnt = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(map[i][j] != -1 && !visited[i][j]){
-                    cnt++;
+                if(map[i][j] == 0 && !vis[i][j]){
+                    total++;
                     bfs(new Coord(i, j));
                 }
             }
         }
 
-        Collections.sort(list);
+        sb.append(total).append("\n");
 
-        bw.write(cnt + "\n");
-        for(int i=0; i<list.size(); i++){
-            bw.write(list.get(i) + " ");
+        Collections.sort(list);
+        for(int area : list){
+            sb.append(area).append(" ");
         }
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
-
-//        for(int i=0; i<m; i++){
-//            for(int j=0; j<n; j++){
-//                System.out.print(map[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
     }
 
     static void bfs(Coord start){
         Queue<Coord> q = new ArrayDeque<>();
 
-        visited[start.y][start.x] = true;
-        int width = 1;
+        vis[start.x][start.y] = true;
         q.offer(start);
+        int area = 1;
 
         while(!q.isEmpty()){
             Coord cur = q.poll();
 
             for(int i=0; i<4; i++){
-                int ny = cur.y + dy[i];
                 int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
-                if(ny < 0 || nx < 0 || ny >= m || nx >= n || map[ny][nx] == -1 || visited[ny][nx]){
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n || vis[nx][ny] || map[nx][ny] == 1){
                     continue;
                 }
 
-                width++;
-                visited[ny][nx] = true;
-                q.offer(new Coord(ny, nx));
-
+                area++;
+                vis[nx][ny] = true;
+                q.offer(new Coord(nx, ny));
             }
         }
-
-        list.add(width);
-
+        list.add(area);
     }
+
 }
