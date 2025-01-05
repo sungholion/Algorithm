5ed6d517934b6d78fd94a0d6aab55565af192ba9
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 22:25:44 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,21 +1,23 @@
-import java.util.*;
 import java.io.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-      int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-      long result = 1;
-      for(int i = 1; i <= n; i++){
-         result *= i;
-      }
+        if(n == 0){
+            bw.write("1\n");
+        } else{
+            int res = 1;
+            for(int i=1; i<=n; i++){
+                res *= i;
+            }
+            bw.write(res + "\n");
+        }
 
-      bw.write(String.valueOf(result));
-      bw.flush();
-      br.close();
-      bw.close();
-   }
+        bw.flush();
+        bw.close();
+    }
 }
