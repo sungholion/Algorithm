7dd3168f9c85d90d8fdf5775fa1af6265be69530
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 21:47:41 +0900

    [Bronze V] Title: 오늘 날짜, Time: 244 ms, Memory: 18912 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,10 +1,17 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.*;
 import java.io.*;
+import java.text.*;
+import java.util.*;
 
 public class Main{
-   public static void main(String[] args) throws IOException{
-      System.out.println(LocalDate.now());
-   }
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+        df.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+        bw.write(df.format(date));
+        bw.flush();
+        bw.close();
+    }
 }
