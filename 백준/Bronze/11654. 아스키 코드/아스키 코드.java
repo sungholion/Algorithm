728942e3b6bc26eda8b7510c86ff4ce6243211f3
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 23:15:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 15816 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,19 +1,14 @@
-import java.util.*;
 import java.io.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       StringBuilder sb = new StringBuilder();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-       int x = (int)br.readLine().charAt(0);
+        char c = br.readLine().charAt(0);
+        bw.write((int)c + "\n");
 
-       sb.append(x).append("\n");
-
-       bw.write(sb.toString());
-       bw.flush();
-       br.close();
-       bw.close();
-   }
+        bw.flush();
+        bw.close();
+    }
 }
