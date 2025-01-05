245009350a20e45de1f70ff35cb40b4cc2618d3d
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 23:42:32 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[9];
-		
-		for (int i=0; i<9; i++)
-			arr[i] = sc.nextInt();
-		
-		int max = arr[0], max_idx = 0;
-		for (int i=1; i<9; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
-				max_idx = i;
-			}
-		}
-		
-		System.out.println(max);
-		System.out.println(max_idx + 1);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[10];
+        for(int i=1; i<=9; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+        int maxIdx = -1;
+        for(int i=1; i<=9; i++){
+            if(arr[i] > max){
+                max = arr[i];
+                maxIdx = i;
+            }
+        }
+
+        bw.write(max + "\n" + maxIdx);
+        bw.flush();
+        bw.close();
+    }
 }
