ba78ddfe9aa2c65b409e43265eaa6a840a53728e
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 21:56:00 +0900

    [Bronze V] Title: A-B, Time: 120 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,21 +1,17 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       StringBuilder sb = new StringBuilder();
-       StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-       int a = Integer.parseInt(st.nextToken());
-       int b = Integer.parseInt(st.nextToken());
-       int result = a - b;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-       sb.append(result);
-       bw.write(sb.toString());
-       bw.flush();
-       br.close();
-       bw.close();
-   }
+        bw.write((a-b) + "\n");
+        bw.flush();
+        bw.close();
+    }
 }
