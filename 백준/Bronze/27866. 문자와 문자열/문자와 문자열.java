e6fc7c76a79d42d654347583e9eaf24677e86107
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 23:31:43 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,20 +1,15 @@
-import java.util.*;
 import java.io.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       StringBuilder sb = new StringBuilder();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-       String str = br.readLine();
-       int i = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
 
-       sb.append(str.charAt(i-1)).append("\n");
-
-       bw.write(sb.toString());
-       bw.flush();
-       br.close();
-       bw.close();
-   }
+        bw.write(str.charAt(n-1) + "\n");
+        bw.flush();
+        bw.close();
+    }
 }
