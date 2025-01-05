3439c08b1a1aab52618905797d3b59f7d6819ea0
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 23:34:50 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		while (sc.hasNextLine()) {
-			String str = sc.nextLine();
-			System.out.println(str);
-		}
-		
-		
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str;
+        while((str=br.readLine())!= null){
+            bw.write(str+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
