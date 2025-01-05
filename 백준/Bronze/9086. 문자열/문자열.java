b88892edefa5901c16e457f59bdf281b2e17ac2e
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 23:38:14 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = sc.nextInt();
-        sc.nextLine();
+        int t = Integer.parseInt(br.readLine());
 
-        for (int i=0; i<t; i++){
-            String str = sc.nextLine();
-            System.out.print(str.charAt(0));
-            System.out.print(str.charAt(str.length()-1));
-            System.out.println();
+        for(int tc=0; tc<t; tc++){
+            String str = br.readLine();
+            char c1 = str.charAt(0);
+            char c2 = str.charAt(str.length()-1);
+            bw.write(c1 + "" + c2 + "\n");
         }
+
+
+        bw.flush();
+        bw.close();
     }
 }
