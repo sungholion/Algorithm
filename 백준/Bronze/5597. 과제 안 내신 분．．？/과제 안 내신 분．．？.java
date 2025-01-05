7c66a3d651f439a50f457ac0f3a0d16b055e3dcc
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 22:48:01 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 116 ms, Memory: 15788 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] submit = new int[31]; // 제출 여부 확인
-        int n = 0; // 출석번호
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for ( int i = 0 ; i < 28 ; i++ ){
-            n =  sc.nextInt();
-            submit[n] = 1;
+        boolean[] vis = new boolean[31];
+
+        for(int i=1; i<=28; i++){
+            int num = Integer.parseInt(br.readLine());
+            vis[num] = true;
         }
 
-        for (int i=1; i<=30; i++){
-            if(submit[i] == 0){
-                System.out.println(i);
+        for(int i=1; i<=30; i++){
+            if(!vis[i]){
+                bw.write(i + "\n");
             }
         }
 
-
+        bw.flush();
+        bw.close();
     }
 }
