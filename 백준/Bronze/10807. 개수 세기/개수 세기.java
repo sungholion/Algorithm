d5842cb91b810c62b957280bb62fb12b7ddfa10a
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 22:44:35 +0900

    [Bronze V] Title: 개수 세기, Time: 120 ms, Memory: 16108 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,22 +1,31 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        
 
-        for ( int i = 0 ; i < n ; i++ )
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int v = Integer.parseInt(br.readLine());
 
-        int v = sc.nextInt();
-        int cnt = 0;
+        int res = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i] == v){
+                res++;
+            }
+        }
 
-        for ( int i = 0 ; i < n ; i++ )
-            if (arr[i] == v)
-                cnt++;
-
-        System.out.println(cnt);
-
+        bw.write(res + "\n");
+        bw.flush();
+        bw.close();
     }
 }
