ab6d638b18a97da5824270c9001c3a7eaecb6180
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 22:51:36 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 200 ms, Memory: 17916 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,33 +1,47 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] arr = new int[n][m];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for (int i=0; i<n; i++){
-            for (int j=0; j<m; j++){
-                arr[i][j] += sc.nextInt();
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+        int[][] arr3 = new int[n][m];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i=0; i<n; i++){
-            for (int j=0; j<m; j++){
-                arr[i][j] += sc.nextInt();
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                arr2[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-
-        for (int i=0; i<n; i++){
-            for (int j=0; j<m; j++){
-                System.out.print(arr[i][j] + " ");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr3[i][j] = arr1[i][j] + arr2[i][j];
             }
-            System.out.println();
         }
 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                bw.write(arr3[i][j] + " ");
+            }
+            bw.write("\n");
+        }
 
+        bw.flush();
+        bw.close();
     }
 }
