6373eb0ecefdb46b7e2119f75686af1a55359d3c
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 23:29:25 +0900

    [Bronze III] Title: 학점계산, Time: 120 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -1,38 +1,36 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String score = sc.nextLine();
+        String str = br.readLine();
+        char c1 = str.charAt(0);
 
-        if (score.equals("A+"))
-            System.out.println(4.3);
-        else if (score.equals("A0"))
-            System.out.println(4.0);
-        else if (score.equals("A-"))
-            System.out.println(3.7);
-        else if (score.equals("B+"))
-            System.out.println(3.3);
-        else if (score.equals("B0"))
-            System.out.println(3.0);
-        else if (score.equals("B-"))
-            System.out.println(2.7);
-        else if (score.equals("C+"))
-            System.out.println(2.3);
-        else if (score.equals("C0"))
-            System.out.println(2.0);
-        else if (score.equals("C-"))
-            System.out.println(1.7);
-        else if (score.equals("D+"))
-            System.out.println(1.3);
-        else if (score.equals("D0"))
-            System.out.println(1.0);
-        else if (score.equals("D-"))
-            System.out.println(0.7);
-        else if (score.equals("F"))
-            System.out.println(0.0);
+        double grade = 0;
 
+        if (c1 == 'A') grade = 4;
+        else if (c1 == 'B') grade = 3;
+        else if (c1 == 'C') grade = 2;
+        else if (c1 == 'D') grade = 1;
+        else if (c1 == 'F') {
+            grade = 0;
+            bw.write(grade + "\n");
+            bw.flush();
+            bw.close();
+            return;
+        }
 
+        char c2 = str.charAt(1);
+        if (c2 == '+') {
+            grade += 0.3;
+        } else if (c2 == '-') {
+            grade -= 0.3;
+        }
+
+        bw.write(grade + "\n");
+        bw.flush();
+        bw.close();
     }
 }
