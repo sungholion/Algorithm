71d598ab4be9a92d75296432a9eff0b60c883267
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-05 22:17:22 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.java
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long n = sc.nextLong();
-        long m = sc.nextLong();
-        System.out.println(Math.abs(n-m));
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        bw.write(Math.abs(n-m)+"\n");
+        bw.flush();
+        bw.close();
     }
 }
