2d27e10e39e84fcf97965305087224146480cd42
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-07 15:12:05 +0900

    [Bronze II] Title: 나머지, Time: 116 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,25 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		Set<Integer> myset = new HashSet<Integer>();
-		
-		for(int i=0; i<10; i++) {
-			int n = Integer.parseInt(br.readLine());
-			myset.add(n%42);
-		}
-		
-		System.out.println(myset.size());
-	
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int ans = 0;
+        boolean vis[] = new boolean[42];
+        for(int i=0; i<10; i++){
+            int n = Integer.parseInt(br.readLine());
+            vis[n%42] = true;
+        }
+
+        for(int i=0; i<42; i++){
+            if(vis[i]) {
+                ans++;
+            }
+        }
+
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+    }
 }
-
-
-
