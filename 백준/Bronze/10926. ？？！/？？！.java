08608ab5ca6c2f73da3745602f8eff261c8841e3
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-08 11:55:05 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,13 +1,15 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String str = br.readLine();
-        System.out.println(str + "??!");
-
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = "??!";
+		
+		String s = br.readLine();
+		System.out.println(s + str);
+	}
 }
