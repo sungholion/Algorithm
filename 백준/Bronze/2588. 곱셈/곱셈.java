a938ec9ed2c1d6b0843681fa1a7b7f5c23cb5e7f
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-09 09:44:50 +0900

    [Bronze III] Title: 곱셈, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,26 +1,32 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-
-        int res1 = a * (b % 10);
-        int res2 = a * ((b % 100) / 10);
-        int res3 = a * (b/100);
-        int res4 = res1 + (10 * res2) + (100 * res3);
-
-        bw.write(res1 + "\n");
-        bw.write(res2 + "\n");
-        bw.write(res3 + "\n");
-        bw.write(res4 + "\n");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main{
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		br.close();
         
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		StringBuilder sb = new StringBuilder();
+        
+		sb.append(A * (B%10));
+		sb.append('\n');
+		
+		sb.append(A * ((B%100)/10));
+		sb.append('\n');
+        
+		sb.append(A * (B/100));
+		sb.append('\n');
+        
+		sb.append(A * B);
+ 
+		System.out.print(sb);
+        
+	}
+ 
 }
