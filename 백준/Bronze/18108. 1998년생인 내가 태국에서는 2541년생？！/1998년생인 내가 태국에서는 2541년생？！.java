b29e50318b426153df0400d8b55b2dcad19f6b14
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-09 09:45:59 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,16 +1,14 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
-
-        bw.write(n-543 +"\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int year = Integer.parseInt(br.readLine());
+		
+		System.out.println(year - 543);
+	}
 }
