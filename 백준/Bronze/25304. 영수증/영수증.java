c1f660d6dcfe180b03b941bad7af7605dabafa3d
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-14 06:38:21 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,33 +1,20 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        int x = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        boolean flag = true;
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            x -= a * b;
-
-            if(x < 0){
-                flag = false;
-                break;
-            }
+        int total = 0;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            total += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
         }
-        
-        if(flag && x == 0) bw.write("Yes\n");
-        else bw.write("No\n");
-
-        bw.flush();
-        bw.close();
-        br.close();
+        if(X == total) System.out.println("Yes");
+        else System.out.println("No");
     }
 }
