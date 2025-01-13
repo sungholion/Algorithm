a4684e1ae37a5b1642738b21d80c73733d485b73
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-13 17:47:20 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,18 +1,15 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		int sum = 0;
-		for(int i=1; i<=n; i++)
-			sum+= i;
-		System.out.println(sum);
+	public static void main(String[] args) throws IOException {
+        
+		int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+		int s = 0;
+        
+		for( int i = 1 ; i <= N ; i++ ) s+=i;
+ 
+		System.out.println(s);
 	}
 }
-
-
-
