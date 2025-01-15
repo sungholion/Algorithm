83cfe39ca81a91885a44783d444c25d126c5dbb1
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-15 14:26:47 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 112 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -3,17 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int cnt = n/4;
-		
-		for(int i=0; i<cnt; i++)
-			System.out.print("long ");
-		System.out.println("int");
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N/4; i++) {
+            sb.append("long ");			
+        }
+        sb.append("int");
+        System.out.println(sb);			
+    }
 }
-
-
-
