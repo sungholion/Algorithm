89c2a60cfc490d909db64ad9216c0d4ce8e9b8cb
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-16 09:53:58 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,21 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+ 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String args[]) throws IOException {
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		for(int i=1; i<=t; i++) {
-			String[] input = br.readLine().split(" ");
-			int a = Integer.parseInt(input[0]);
-			int b = Integer.parseInt(input[1]);
-			System.out.println("Case #" + i +": " + (a+b));
+		int a = Integer.parseInt(br.readLine());
+ 
+		StringTokenizer st;
+		for (int i = 1; i <= a; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write("Case #"+i+": ");
+			bw.write(Integer.parseInt(st.nextToken())
+				+Integer.parseInt(st.nextToken())+"\n");
 		}
-			
+		br.close();
+		bw.flush();
+		bw.close();
 	}
+ 
 }
-
-
-
