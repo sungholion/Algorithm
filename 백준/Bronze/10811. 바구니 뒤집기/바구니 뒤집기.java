07156faeb1826ba573f4cd8f1c887697ecc0cc55
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-20 12:49:17 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,41 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[n+1];
-        for(int i = 1;i <= n; i++){
-            arr[i] = i;
-        }
-        int[] arrReverse = new int[n+1];
-
-        for(int t = 0; t < m; t++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-
-            while(i < j){
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++;
-                j--;
-            }
-        }
-
-        for(int i = 1; i <= n; i++){
-            bw.write(arr[i] + " ");
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strtk = new StringTokenizer(br.readLine()," ");
+		
+		int n = Integer.parseInt(strtk.nextToken());
+		int count = Integer.parseInt(strtk.nextToken()); 
+		
+		int temp = 0;
+		
+		int busket[] = new int[n];
+		for(int i=0;i<busket.length;i++) {
+			busket[i] = i+1;
+		}
+		
+		for(int i=0;i<count;i++) {
+			strtk = new StringTokenizer(br.readLine()," ");
+			
+			int a = Integer.parseInt(strtk.nextToken())-1;
+			int b = Integer.parseInt(strtk.nextToken())-1;
+			
+			while(a<b) {
+				temp = busket[a];
+				busket[a] = busket[b];
+				busket[b] = temp;
+				a++;
+				b--;
+			}
+		}
+		
+		br.close();
+		
+		for(int i=0;i<busket.length;i++) {
+			System.out.print(busket[i]+" ");
+		}
+		
+	}
 }
