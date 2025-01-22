9fe2c0a7e97d87aa65381f5bd36ad58c7fd90baf
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-22 09:50:39 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,26 +1,21 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        String a = st.nextToken();
-        String b = st.nextToken();
-
-        a = String.valueOf(a.charAt(2) + "" + a.charAt(1) + a.charAt(0));
-        b = String.valueOf(b.charAt(2) + "" + b.charAt(1) + b.charAt(0));
-
-        if(Integer.parseInt(a) > Integer.parseInt(b)) {
-            bw.write(a);
-        } else {
-            bw.write(b);
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+ 
+	public static void main(String[] args) throws IOException {
+		int A = 0;
+		int B = 0;
+		
+		A += System.in.read() - 48;
+		A += (System.in.read() - 48)*10;
+		A += (System.in.read() - 48)*100;
+		
+		System.in.read();
+		
+		B += System.in.read() - 48;
+		B += (System.in.read() - 48)*10;
+		B += (System.in.read() - 48)*100;
+		
+		System.out.println(A > B ? A : B);
+	}
 }
