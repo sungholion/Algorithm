784e358b5adf7b7c639a7017e46febb2523eb14c
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-24 14:50:55 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 124 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,32 +1,34 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		int[] num = new int[input.length];
-		int[] com = {1, 1, 2, 2, 2, 8};
-		int[] ans = new int[input.length];
 		
-		for(int i=0; i<input.length; i++) {
-			num[i] = Integer.parseInt(input[i]);
-		}
+		int king = 1;
+		int queen = 1;
+		int rook = 2;
+		int bishop = 2;
+		int knight = 2;
+		int pawn = 8;
 		
-		for(int i=0; i<num.length; i++) {
-			ans[i] = com[i] - num[i];
-		}
-		
-		for(int i=0; i<ans.length; i++) {
-			System.out.print(ans[i] +" ");
-		}
-		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		king = king - Integer.parseInt(st.nextToken());
+		queen = queen - Integer.parseInt(st.nextToken());
+		rook = rook - Integer.parseInt(st.nextToken());
+		bishop = bishop - Integer.parseInt(st.nextToken());
+		knight = knight - Integer.parseInt(st.nextToken());
+		pawn = pawn - Integer.parseInt(st.nextToken());
+ 
+		System.out.print(king + " ");
+		System.out.print(queen + " ");
+		System.out.print(rook + " ");
+		System.out.print(bishop + " ");
+		System.out.print(knight + " ");
+		System.out.print(pawn + " ");		
 		
 	}
 }
-
-
-
