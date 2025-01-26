2ca0a673807e05ac4951931e30df717af30e53d9
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-01-26 15:44:23 +0900

    [Bronze I] Title: 단어 공부, Time: 132 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,52 +1,36 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-
+ 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine().toUpperCase();
-		String[] str = input.split("");	// 원본 문자열
-		
-		LinkedHashSet<String> linkedhashSet = new LinkedHashSet<>(Arrays.asList(str));
-		String[] new_str = linkedhashSet.toArray(new String[0]); // 중복 제거 문자열
-		int[] cnt = new int[new_str.length]; // 중복 세아릴 배열
-		
-		for(int i=0; i<str.length; i++) {
-			for(int j=0; j<new_str.length; j++) {
-				if(str[i].equals(new_str[j])) {
-					cnt[j]++;
-				}
+ 
+		int[] arr = new int[26]; 
+ 
+		int c = System.in.read();
+ 
+		while (c > 64) {	
+			
+			if (c < 91) {
+				arr[c - 65]++;
+			} else {
+				arr[c - 97]++;
+			}
+			c = System.in.read();
+		}
+ 
+ 
+		int max = -1;
+		int ch = -2;	
+ 
+		for (int i = 0; i < 26; i++) {
+ 
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = i;
+			} else if (arr[i] == max) {
+				ch = -2;
 			}
 		}
-		
-		int max = cnt[0];
-		int max_idx = 0;
-		for(int i=1; i<cnt.length; i++) {
-			if(max < cnt[i]) {
-				max = cnt[i];
-				max_idx = i;
-			}
-				
-		}
-		
-		boolean ans = true;
-		for(int i=0; i<cnt.length; i++) {
-			if((cnt[i] == max) && (i!=max_idx)) {
-				ans = false;
-			}
-		}
-		
-		if(ans) {
-			System.out.println(new_str[max_idx]);
-		}
-		else
-			System.out.println("?");
-		
-		
-		
+		System.out.print((char) (ch+65));
 	}
-}	
+ 
+}
