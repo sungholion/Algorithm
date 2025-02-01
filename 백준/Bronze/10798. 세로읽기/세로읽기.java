72d03c29020a02ac1166b5fa4a1ea7ae687840a3
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-02-01 13:55:56 +0900

    [Bronze I] Title: 세로읽기, Time: 172 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,31 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		char[][] arr = new char[5][15];
-		int[] size = new int[5];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		for(int i=0; i<5; i++) {
-			char[] str = br.readLine().toCharArray();
-			arr[i] = str;
-			size[i] = str.length;
-		}
+		char[][] c_arr = new char[5][15];
+		int i,j;
+		int max = 0;
 		
-		for(int j=0; j<15; j++) {
-			for(int i=0; i<5; i++) {
-				if(j < arr[i].length) {
-					System.out.print(arr[i][j]);
-				}
+		for(i=0;i<5;i++) {
+			String str = sc.next();
+			
+			if(str.length()>max) 
+				max = str.length();
+			
+			for(j=0;j<str.length();j++) {
+				c_arr[i][j] = str.charAt(j);
 			}
 		}
 		
 		
-			
-		
+		for(i=0;i<max;i++) {
+			for(j=0;j<5;j++) {
+				if(c_arr[j][i] == '\0')
+					continue;
+				System.out.print(c_arr[j][i]);
+			}
+		}
+		sc.close();
 	}
 }
