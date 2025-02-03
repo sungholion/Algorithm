a063bd6acc5677ab58f812b57c37c8370803cbc8
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-02-03 14:40:14 +0900

    [Bronze I] Title: 팰린드롬수, Time: 96 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,32 +1,24 @@
-import java.math.BigInteger;
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-      while (true) {
-         String str = br.readLine();
-
-         boolean flag = true;
-         if (str.equals("0")) break;
-         for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)){
-               flag = false;
-               break;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String s = br.readLine(); 
+            int n = s.length(); 
+            boolean b = true;
+            if (s.equals("0")) break;
+            for (int i = 0; i <= n/2; i++) {
+                if (s.charAt(i) != s.charAt(n-i-1)) b = false; 
             }
-         }
-         
-         if (flag) bw.write("yes\n");
-         else bw.write("no\n");
-      }
-
-      bw.flush();
-      bw.close();
-      br.close();
-   }
+            if (b) {  
+                sb.append("yes\n");
+            } else {  
+                sb.append("no\n");
+            }
+        }
+        System.out.print(sb);
+    }
 }
-
-
