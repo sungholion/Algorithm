a4f117fa7898c88101d3ce49c4cc4b335e7ca684
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-03-05 02:01:27 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 180 ms, Memory: 18280 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + str + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
