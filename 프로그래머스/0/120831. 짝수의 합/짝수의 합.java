d0aa1b79f82ac56c0618639a3f5a067a26a53554
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-03-08 17:02:03 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,10 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=0; i<=n; i+=2){
+        for(int i=2; i<=n; i+=2){
             answer += i;
         }
-        
         return answer;
     }
 }
