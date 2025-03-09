c3963f7dca9eb46d38c21a691e264ebfda1406bd
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-03-09 18:08:30 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,25 @@
 class Solution {
+  
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[computers.length];
+        boolean[] vis = new boolean[n];
         
-        
-        for(int i = 0; i < computers.length; i++){
-            visited[i] = false;
-        }
-        
-         for(int i = 0; i < computers.length; i++){
-            if(visited[i] == false){
+        for(int i = 0; i < n; i++){
+            if(vis[i] == false){
                 answer++;
-                dfs(i, visited, computers);
-            }
+                dfs(i, vis, computers);
+            }            
         }
-        
         
         return answer;
     }
     
-    public void dfs(int node, boolean[] visited, int[][] computers){
-        visited[node] = true;
+    public void dfs(int node, boolean[] vis, int[][] computers){
+        vis[node] = true;
         
-        for(int i=0; i<computers.length; i++){
-            if(visited[i] == false && computers[node][i] == 1){
-                dfs(i, visited, computers);
+        for(int i = 0; i < computers.length; i++){
+            if(vis[i] == false && computers[node][i] == 1){
+                dfs(i, vis, computers);
             }
         }
     }
