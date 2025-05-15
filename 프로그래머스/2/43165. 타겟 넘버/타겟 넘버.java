0b7ef696f8c260e7260c17f7bcb540f0a3bcf85b
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-15 12:53:47 +0900

    [level 2] Title: 타겟 넘버, Time: 4.72 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,21 @@
-import java.util.*;  
-  
 class Solution {
-    int answer = 0;
-
+    int cnt = 0;
     public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
-
+        int answer = 0;
+        dfs(numbers, target, 0, 0);
+        answer = cnt;
         return answer;
     }
-
-    // 깊이 우선 탐색
-    public void dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length){ // 마지막 노드 까지 탐색한 경우
-            if(target == sum) answer++;
-        } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]); // 해당 노드의 값을 더하고 다음 깊이 탐색
-            dfs(numbers, depth + 1, target, sum - numbers[depth]); // 해당 노드의 값을 빼고 다음 깊이 탐색
+    
+    public void dfs(int[] numbers, int target, int depth, int sum){
+        if(depth == numbers.length){
+            if(sum == target)
+                cnt++;
+            return;
         }
+        dfs(numbers, target, depth+1, sum + numbers[depth]);
+        dfs(numbers, target, depth+1, sum - numbers[depth]);
     }
+    
+
 }
