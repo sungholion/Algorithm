5fbdb15cdfda2b1d3c35053bbfc3bfb5e5d38350
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-15 14:01:40 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,3 +1,4 @@
+import java.awt.image.ImageProducer;
 import java.io.*;
 import java.util.*;
 
@@ -5,64 +6,56 @@ class Coord{
     int x;
     int y;
 
-    public Coord(int x, int y){
+    Coord(int x, int y){
         this.x = x;
         this.y = y;
     }
 }
 
-public class Main{
-    static int n, m;
-    static int[][] map;
+public class Main {
+    static int[][] arr;
     static int[][] vis;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int n, m;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        map = new int[n][m];
+        arr = new int[n][m];
         vis = new int[n][m];
+
 
         for(int i=0; i<n; i++){
             String temp = br.readLine();
             for(int j=0; j<m; j++){
-                map[i][j] = temp.charAt(j) - '0';
+                arr[i][j] = temp.charAt(j) - '0';
             }
         }
 
-        bfs(new Coord(0, 0));
+        bfs(new Coord(0,0));
+        System.out.println(vis[n-1][m-1]);
 
-        sb.append(vis[n-1][m-1]).append("\n");
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
     }
 
-    static void bfs(Coord start){
-        Queue<Coord> q = new ArrayDeque<>();
-        
-        vis[start.x][start.y] = 1;
-        q.offer(start);
+    public static void bfs(Coord node){
+        Queue<Coord> q = new LinkedList<>();
+        q.offer(node);
+        vis[node.x][node.y] = 1;
 
         while(!q.isEmpty()){
             Coord cur = q.poll();
-            
+
             for(int i=0; i<4; i++){
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
-                
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m || map[nx][ny] == 0 || vis[nx][ny] != 0){
+
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m || vis[nx][ny] != 0 || arr[nx][ny] != 1){
                     continue;
                 }
-
                 vis[nx][ny] = vis[cur.x][cur.y] + 1;
                 q.offer(new Coord(nx, ny));
             }
