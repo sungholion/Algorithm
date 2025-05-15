25a3dd0457b1e471ea524920914ff051b0ef232b
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-15 22:51:56 +0900

    [level 3] Title: 단어 변환, Time: 0.32 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,35 @@
 class Solution {
-    static boolean[] visited;
-    static int answer = 0;
-    
+    boolean[] vis;
+    int answer;
     public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
-
+        vis = new boolean[words.length];
         dfs(begin, target, words, 0);
+        
         return answer;
     }
     
-    public static void dfs(String begin, String target, String[] words, int cnt) {
-        if (begin.equals(target)) {
+    public void dfs(String begin, String target, String[] words, int cnt){
+        if(begin.equals(target)){
             answer = cnt;
             return;
         }
-
-        for (int i = 0; i < words.length; i++) {
-            if (visited[i]) {
+        
+        for(int i=0; i<words.length; i++){
+            if(vis[i]){
                 continue;
             }
-
-            int k = 0;    // 같은 스펠링 몇개인지 세기
-            for (int j = 0; j < begin.length(); j++) {
-                if (begin.charAt(j) == words[i].charAt(j)) {
-                    k++;
+            
+            int check = 0;
+            for(int j=0; j<begin.length(); j++){
+                if(begin.charAt(j) == words[i].charAt(j)){
+                    check++;
                 }
             }
-
-            if (k == begin.length() - 1) {  // 한글자 빼고 모두 같은 경우
-                visited[i] = true;
+            
+            if(check == begin.length()-1){
+                vis[i] = true;
                 dfs(words[i], target, words, cnt + 1);
-                visited[i] = false;
+                vis[i] = false;
             }
         }
     }
