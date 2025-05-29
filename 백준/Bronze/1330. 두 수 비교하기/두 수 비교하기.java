9d08b9ef821d5493bc523efdf61f11f1f6b5deae
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 11:51:32 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,27 +1,25 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-       StringBuilder sb = new StringBuilder();
-       StringTokenizer st = new StringTokenizer(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-       int a = Integer.parseInt(st.nextToken());
-       int b = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-       if(a > b){
-          sb.append(">");
-       } else if(a < b){
-          sb.append("<");
-       } else {
-          sb.append("==");
-       }
+        if(A > B){
+            bw.write(">");
+        } else if(A < B){
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
 
-       bw.write(sb.toString());
-       bw.flush();
-       br.close();
-       bw.close();
-   }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
