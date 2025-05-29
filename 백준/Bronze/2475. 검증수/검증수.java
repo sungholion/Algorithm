c8dae79699b9cd501dfbced27d921c730fc3815c
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 14:35:28 +0900

    [Bronze V] Title: 검증수, Time: 116 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int sum = 0;
-		for (int i=0; i<5; i++) {
-			int n = sc.nextInt();
-			sum += Math.pow(n, 2);
-		}
-		System.out.println(sum%10);
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+        for(int i=0; i<5; i++){
+            sum += (int) Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+
+        bw.write(sum % 10 + "\n");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
