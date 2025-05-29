c763652d8282df629b1406ec868a960d416033fd
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 12:16:23 +0900

    [Bronze V] Title: A×B, Time: 120 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        bw.write((a*b) + "\n");
+        br.close();
+        bw.write(A * B + "\n");
         bw.flush();
         bw.close();
     }
