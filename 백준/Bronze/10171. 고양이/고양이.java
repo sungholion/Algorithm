ea31ca2618617022f5120c70fc9c56884f8945ca
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 15:57:42 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,22 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+    public static void main(String[] args)throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\");
+        bw.newLine();	// 줄 구분자
+
+        bw.write(" )  ( ')");
+        bw.newLine();
+
+        bw.write("(  /  )");
+        bw.newLine();
+
+        bw.write(" \\(__)|");
+
+        bw.flush();
+        bw.close();
     }
 }
