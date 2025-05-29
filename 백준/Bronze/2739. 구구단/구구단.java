016d58d0ebb4610b3c5d71bbcd0abe9c9affc3eb
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 12:39:38 +0900

    [Bronze V] Title: 구구단, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,16 +1,19 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
 
         for(int i=1; i<=9; i++){
-            bw.write(n + " * " + i + " = " + (n*i) + "\n");
+            sb.append(N).append(" * ").append(i).append(" = ").append(N * i).append("\n");
         }
 
+        br.close();
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
