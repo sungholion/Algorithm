f14aff0d1a1a211d2689ce600030b234bc8731ab
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 15:40:26 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,21 +1,21 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-      int year = Integer.parseInt(br.readLine());
+        int year = Integer.parseInt(br.readLine());
 
-      if(year%4==0 && year%100!=0 || year%400==0)
-         bw.write("1");
-      else{
-         bw.write("0");
-      }
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            bw.write(1+"\n");
+        } else{
+            bw.write(0+"\n");
+        }
 
-      bw.flush();
-      br.close();
-      bw.close();
-   }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
