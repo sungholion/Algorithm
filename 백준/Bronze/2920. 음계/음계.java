270699a119d4fd2352aa718e99ba064b9d36a4c5
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 17:22:55 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -7,45 +7,23 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] asc = {1, 2, 3, 4, 5, 6, 7, 8};
-        int[] desc = {8, 7, 6, 5, 4, 3, 2, 1};
-
         int[] arr = new int[8];
-        for(int i = 0; i < 8; i++) {
+        String res = "";
+        for (int i = 0; i < arr.length; i++)
             arr[i] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < arr.length - 1; i++){
+            if(arr[i] == arr[i+1] - 1)
+                res = "ascending\n";
+            else if(arr[i] == arr[i+1] + 1)
+                res = "descending\n";
+            else{
+                res = "mixed\n";
+                break;
+            }
         }
 
-        boolean flag = true;
-        if(arr[0] == 1){
-            for(int i = 0; i < 8; i++) {
-                if(arr[i] != asc[i]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag)
-                bw.write("ascending\n");
-            else
-                bw.write("mixed\n");
-
-        } else if(arr[0] == 8){
-            for(int i = 0; i < 8; i++) {
-                if(arr[i] != desc[i]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag)
-                bw.write("descending\n");
-            else
-                bw.write("mixed\n");
-
-        } else{
-            bw.write("mixed\n");
-        }
-
-
-
+        bw.write(res);
         br.close();
         bw.flush();
         bw.close();
