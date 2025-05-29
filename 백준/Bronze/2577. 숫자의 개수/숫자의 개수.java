10555066de9d7ab869019e260a961958edd84e81
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 16:21:33 +0900

    [Bronze II] Title: 숫자의 개수, Time: 116 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,27 +1,32 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
-        String res = a * b * c + "";
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int total = A * B * C;
 
-        int[] num = new int[10];
-        for(int i=0; i<res.length(); i++){
-            int idx = res.charAt(i) - '0';
-            num[idx]++;
+        int[] numbers = new int[10];
+        String str = String.valueOf(total);
+
+        for(int i = 0; i <= 9; i++){
+            for(int j=0; j<str.length(); j++){
+                char c = str.charAt(j);
+                if(c - '0' == i){
+                    numbers[i]++;
+                }
+            }
         }
 
-        for(int x : num){
+        for(int x : numbers)
             bw.write(x + "\n");
-        }
 
-
+        br.close();
         bw.flush();
         bw.close();
     }
