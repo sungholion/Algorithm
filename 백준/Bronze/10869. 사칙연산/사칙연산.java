9e00c925cdf621618fcacee182b13a30db8ceef2
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 12:37:18 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -5,16 +5,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
 
-        bw.write((a+b) + "\n");
-        bw.write((a-b) + "\n");
-        bw.write((a*b) + "\n");
-        bw.write((a/b) + "\n");
-        bw.write((a%b) + "\n");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        br.close();
+        bw.write(String.valueOf(A+B) + "\n");
+        bw.write(String.valueOf(A-B) + "\n");
+        bw.write(String.valueOf(A*B) + "\n");
+        bw.write(String.valueOf(A/B) + "\n");
+        bw.write(String.valueOf(A%B) + "\n");
         bw.flush();
         bw.close();
     }
