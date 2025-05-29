04a4df37168f9869bc9e1b76fa8edb656cd5afb9
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 15:24:39 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,31 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-      int score = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-      if(score <= 100 && score >= 90){
-         bw.write("A");
-      }
-      else if(score >= 80 && score < 90){
-         bw.write("B");
-      }
-      else if(score >= 70 && score < 80){
-         bw.write("C");
-      }
-      else if(score >= 60 && score < 70){
-         bw.write("D");
-      }
-      else{
-         bw.write("F");
-      }
+        if(n <= 100 && n >= 90){
+            bw.write("A");
+        } else if(n < 90 && n >= 80){
+            bw.write("B");
+        } else if(n < 80 && n >= 70){
+            bw.write("C");
+        } else if(n < 70 && n >= 60){
+            bw.write("D");
+        } else{
+            bw.write("F");
+        }
 
-      bw.flush();
-      br.close();
-      bw.close();
-   }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
