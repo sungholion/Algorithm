cd82e66b8d1a2418d3bc1d6b9375eb2e71ed48ea
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 16:31:43 +0900

    [Bronze II] Title: 문자열 반복, Time: 136 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,29 +1,29 @@
-import javax.sql.rowset.serial.SerialStruct;
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        for(int tc=0; tc<t; tc++){
+        while(T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
+            String ans = "";
+            int R = Integer.parseInt(st.nextToken());
             String str = st.nextToken();
 
-            for(int i=0; i<str.length(); i++){
-                for(int j=0; j<n; j++){
-                    bw.write(str.charAt(i));
+            for(int i = 0; i < str.length(); i++){
+                for(int j = 0; j < R; j++){
+                    ans += str.charAt(i);
                 }
             }
-            bw.write("\n");
+            bw.write(ans + "\n");
+
         }
 
-
-
+        br.close();
         bw.flush();
         bw.close();
     }
