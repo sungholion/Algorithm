7157b5ce76ca0f385f5d16bda5d4e8a363a924d2
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-29 17:44:34 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,29 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         String str = br.readLine();
-        String alpa = "abcdefghijklmnopqrstuvwxyz";
-        int[] cnt = new int[26];
-        for(int i=0; i<26; i++){
-            cnt[i] = -1;
+
+        int[] alphabet = new int[26];
+        for(int i = 0; i < alphabet.length; i++){
+            alphabet[i] = -1;
         }
 
-        for(int i=0; i<str.length(); i++){
-            for(int j=0; j<alpa.length(); j++){
-                if(str.charAt(i) == alpa.charAt(j) && cnt[j] == -1){
-                    cnt[j] = i;
-                }
-            }
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            int idx = c - 'a';
+
+            if(alphabet[idx] == -1)
+                alphabet[idx] = i;
         }
 
-        for(int cntNum : cnt){
-            bw.write(cntNum + " ");
-        }
+        for(int x : alphabet)
+            bw.write(x + " ");
 
+        br.close();
         bw.flush();
         bw.close();
     }
