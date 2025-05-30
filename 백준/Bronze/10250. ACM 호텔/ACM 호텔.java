e6b9bf1cb3a2a160acb932463924fd4750ffb1b1
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-30 14:35:23 +0900

    [Bronze III] Title: ACM 호텔, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         while(T-- > 0){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,22 +14,15 @@ public class Main {
             int W = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
 
-            int y = N % H;
-            int x = N / H + 1;
-
-            if(y == 0){
-                y = H;
-                x--;
-            }
-
-            if(x < 10){
-                bw.write(y + "0" + x + "\n");
+            if(N % H == 0){
+                sb.append((H * 100) + (N / H)).append("\n");
             } else {
-                bw.write(y + "" + x + "\n");
+                sb.append(((N % H) * 100) + ((N/H) + 1)).append("\n");
             }
         }
 
         br.close();
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
