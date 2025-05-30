b5ea33e923937c0e23ee0c694a3a7efe3e2f1412
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-30 15:05:29 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 96 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,24 +1,28 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        boolean[] vis = new boolean[31];
+        boolean[] arr = new boolean[31];
 
-        for(int i=1; i<=28; i++){
-            int num = Integer.parseInt(br.readLine());
-            vis[num] = true;
+        for(int i=0; i<28; i++){
+            int n = Integer.parseInt(br.readLine());
+            arr[n] = true;
         }
 
-        for(int i=1; i<=30; i++){
-            if(!vis[i]){
-                bw.write(i + "\n");
-            }
+        for(int i = 1; i < 31; i++){
+            if(!arr[i])
+                sb.append(i).append("\n");
         }
 
+        br.close();
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
