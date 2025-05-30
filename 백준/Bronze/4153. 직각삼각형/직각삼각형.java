0008ebd079146e0acb1c9c9417f3ae01a26ce4bb
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-30 15:44:08 +0900

    [Bronze III] Title: 직각삼각형, Time: 88 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,39 +1,48 @@
-import java.util.*;
+import com.sun.source.tree.UsesTree;
+
 import java.io.*;
-
-public class Main{
-   public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-      while(true){
-         StringTokenizer st = new StringTokenizer(br.readLine());
-
-         int a = Integer.parseInt(st.nextToken());
-         int b = Integer.parseInt(st.nextToken());
-         int c = Integer.parseInt(st.nextToken());
-         int big = 0;
+import java.util.*;
 
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-         if(a == 0 && b == 0 && c == 0){
-            break;
-         }
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-         if(Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)){
-            bw.write("right\n");
-         } else if(Math.pow(a, 2) + Math.pow(c, 2) == Math.pow(b, 2)){
-            bw.write("right\n");
-         } else if(Math.pow(b, 2) + Math.pow(c, 2) == Math.pow(a, 2)){
-            bw.write("right\n");
-         }
-         else{
-            bw.write("wrong\n");
-         }
-      }
+            if(a == 0 && b == 0 && c == 0)
+                break;
 
-      bw.flush();
-      bw.close();
-      br.close();
-   }
+            if(a > b && a > c){
+                if((a*a) == (b*b + c*c)){
+                    sb.append("right\n");
+                } else{
+                    sb.append("wrong\n");
+                }
+            } else if(b > a && b > c){
+                if((b*b) == (a*a + c*c)){
+                    sb.append("right\n");
+                } else{
+                    sb.append("wrong\n");
+                }
+            } else if(c > a && c > b){
+                if((c*c) == (a*a + b*b)){
+                    sb.append("right\n");
+                } else{
+                    sb.append("wrong\n");
+                }
+            }
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
