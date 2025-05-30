5775bc6612c090ae1de232b18981870e9eec0b05
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-30 15:14:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,13 +1,18 @@
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
-        bw.write(str.length() + "\n");
+        sb.append(str.length());
 
+        br.close();
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
