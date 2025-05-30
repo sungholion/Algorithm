246005a398e5765d5923351617d34c8237beba0f
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-05-30 15:16:14 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -1,22 +1,26 @@
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
 
         for(int i=0; i<str.length(); i++){
             char c = str.charAt(i);
-
-            if(Character.isLowerCase(c)){
-                bw.write(Character.toUpperCase(c));
+            if(Character.isUpperCase(c)){
+                sb.append(Character.toLowerCase(c));
             } else{
-                bw.write(Character.toLowerCase(c));
+                sb.append(Character.toUpperCase(c));
             }
         }
 
+        br.close();
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
