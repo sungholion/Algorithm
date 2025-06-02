df3c11ca97e23b70bbe025757904d60b0e7a4d91
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-02 20:47:58 +0900

    [Silver V] Title: 단어 정렬, Time: 308 ms, Memory: 26032 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,40 +1,37 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
- 
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		String[] arr = new String[N];
- 
-		for (int i = 0; i < N; i++) 
-			arr[i] = br.readLine();
-		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				if (s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				} 
-				else {
-					return s1.length() - s2.length();
-				}
-			}
-		});
- 
-		StringBuilder sb = new StringBuilder();
- 
-		sb.append(arr[0]).append('\n');
-		
-		for (int i = 1; i < N; i++) {
-			if (!arr[i].equals(arr[i - 1])) {
-				sb.append(arr[i]).append('\n');
-			}
-		}
-		System.out.println(sb);
-	}
- 
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String[] str = new String[N];
+
+        for(int i = 0; i < N; i++)
+            str[i] = br.readLine();
+
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length())
+                    return o1.compareTo(o2);
+                else
+                    return o1.length() - o2.length();
+            }
+        });
+
+        sb.append(str[0]).append("\n");
+        for(int i=1; i<N; i++){
+            if(!str[i].equals(str[i-1]))
+                sb.append(str[i]).append("\n");
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
