7b438650db86770834402b71f8129b9965ca48b1
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-02 11:43:53 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,38 +1,43 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-
-    static int n, m, answer = Integer.MIN_VALUE;
-    static int[] arr;
-
-    public static void solution(int len, int start, int sum) {
-        if (sum > m) return;
-        if (len == 3) {
-            answer = Math.max(sum, answer);
-        }
-        else {
-            for (int i = start; i < n; i++) {
-                solution(len+1, i+1, sum+arr[i]);
-            }
-        }
-    }
-
+    static final int MOD = 1234567891;
+    static final int R = 31;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] card = new int[N];
 
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<card.length; i++){
+            card[i] = Integer.parseInt(st.nextToken());
         }
 
-        solution(0, 0, 0);
-        System.out.println(answer);
+        int Max = 0;
+        for(int i=0; i<card.length-2; i++){
+            int sum = 0;
+
+            for(int j=i+1; j<card.length-1; j++){
+                for(int k=j+1; k<card.length; k++){
+                    sum = card[i] + card[j] + card[k];
+                    if((sum <= M) && sum > Max){
+                        Max = sum;
+                    }
+                }
+            }
+        }
+
+        sb.append(Max);
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
