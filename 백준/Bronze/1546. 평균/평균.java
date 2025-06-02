5999a93ce023b707defec4ab2b032aac94900bff
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-02 11:59:35 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,28 +1,36 @@
-import java.util.Arrays;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		double arr[] = new double[Integer.parseInt(br.readLine())];
-        
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = Double.parseDouble(st.nextToken());
-		}
-		
-		double sum = 0;
-		Arrays.sort(arr);
-		
-		for(int i = 0; i < arr.length; i++) {
-			sum += ((arr[i] / arr[arr.length - 1]) * 100);
-		}
-		System.out.print(sum / arr.length);
-	}
+    static final int MOD = 1234567891;
+    static final int R = 31;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(num > M){
+                M = num;
+            }
+            score[i] = num;
+        }
+
+        double sum = 0;
+        for(int x : score){
+            sum += x / (double) M * 100;
+        }
+
+        sb.append(sum / N);
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
