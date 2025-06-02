c6196c5fb30f7a9a64d015a64c941ce94e47bb56
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-02 10:37:31 +0900

    [Bronze III] Title: 웰컴 키트, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,38 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-   public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-      StringTokenizer st;
 
-      int N = Integer.parseInt(br.readLine());  // 사람 수
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-      int[] sizeArr = new int[6];  // 사이즈별 필요한 티셔츠 수
-      st = new StringTokenizer(br.readLine());
-      for(int i=0; i<6; i++){
-         sizeArr[i] = Integer.parseInt(st.nextToken());
-      }
+        int N = Integer.parseInt(br.readLine());    // 총 사람 수
 
-      st = new StringTokenizer(br.readLine());
-      int T = Integer.parseInt(st.nextToken());  // 티셔츠 묶음 수
-      int P = Integer.parseInt(st.nextToken());     // 펜 묶음 수
-      int cntT = 0;  // 필요 티셔츠 묶음 수
+        int[] tSize = new int[6];
+        st = new StringTokenizer(br.readLine());    // 티셔츠 사이즈별 수요
+        for(int i=0; i<tSize.length; i++){
+            tSize[i] = Integer.parseInt(st.nextToken());
+        }
 
-      // ---------------------------------------------------------------------------------- 입력
+        st = new StringTokenizer(br.readLine());    // 한번에 구매하는 묶음
+        int T = Integer.parseInt(st.nextToken());   // 티셔츠
+        int P = Integer.parseInt(st.nextToken());   // 펜
 
-      for(int i=0; i<6; i++){
-         cntT += sizeArr[i] / T;
-         if(sizeArr[i] % T > 0){
-            cntT += 1;
-         }
-      }
+        int T_sum = 0;
+        int P_sum = 0;
+        int P_remain = 0;
+        for(int i = 0; i<tSize.length; i++){
+            T_sum += tSize[i] / T;
+            if(tSize[i] % T != 0)
+                T_sum++;
+        }
 
-      bw.write(cntT + "\n");
-      bw.write(N / P +  " " + N % P + "\n");
-      bw.flush();
-      bw.close();
-      br.close();
-   }
+        P_sum = N / P;
+        P_remain = N % P;
+
+        sb.append(T_sum).append("\n").append(P_sum).append(" ").append(P_remain);
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
