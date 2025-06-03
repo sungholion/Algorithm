8db7b874a6ecf1b0cc7c34364824b933387d6a55
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 19:56:29 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,15 +1,19 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String str = "??!";
-		
-		String s = br.readLine();
-		System.out.println(s + str);
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        String str = br.readLine();
+        sb.append(str).append("??!");
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
