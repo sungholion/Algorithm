5e6ace8a91a89ece164ae046128e76de17d3ecdd
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 14:29:40 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19140 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,100 +1,53 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-   public static int[] q;
-   public static int f;
-   public static int r;
+    public static int[] arr;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-      StringBuilder sb = new StringBuilder();
+        Deque<Integer> q = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String com = st.nextToken();
+            if(com.equals("push")){
+                int num = Integer.parseInt(st.nextToken());
+                q.add(num);
+            } else if(com.equals("pop")){
+                if(q.isEmpty()){
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(q.poll()).append("\n");
+                }
+            } else if(com.equals("size")){
+                sb.append(q.size()).append("\n");
+            } else if(com.equals("empty")){
+                if(q.isEmpty()){
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if(com.equals("front")){
+                if(q.isEmpty()){
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(q.peek()).append("\n");
+                }
+            } else if(com.equals("back")){
+                if(q.isEmpty()){
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(q.peekLast()).append("\n");
+                }
+            }
+        }
 
-      StringTokenizer st;
-      int N = Integer.parseInt(br.readLine());
-      q = new int[N];
-
-      while (N-- > 0) {
-         st = new StringTokenizer(br.readLine());
-
-         switch (st.nextToken()) {
-            case "push":
-               push(Integer.parseInt(st.nextToken()));
-               break;
-
-            case "pop":
-               sb.append(pop()).append('\n');
-               break;
-
-            case "size":
-               sb.append(size()).append('\n');
-               break;
-
-            case "empty":
-               sb.append(empty()).append('\n');
-               break;
-
-            case "front":
-               sb.append(front()).append('\n');
-               break;
-
-            case "back":
-               sb.append(back()).append('\n');
-               break;
-         }
-      }
-
-      bw.write(sb.toString());
-      bw.flush();
-      bw.close();
-      br.close();
-   }
-
-   public static void push(int item) {
-      q[r++] = item;
-   }
-
-   public static int pop() {
-      if((r-f) == 0) {
-         return -1;
-      }
-      else {
-         int res = q[f++];
-         q[f - 1] = 0;
-         return res;
-      }
-   }
-
-   public static int size() {
-      return r - f;
-   }
-
-   public static int empty() {
-      if((r-f) == 0) {
-         return 1;
-      }
-      else {
-         return 0;
-      }
-   }
-
-   public static int front() {
-      if((r-f) == 0) {
-         return -1;
-      }
-      else {
-         return q[f];
-      }
-   }
-
-   public static int back() {
-      if((r-f) == 0) {
-         return -1;
-      }
-      else {
-         return q[r-1];
-      }
-   }
-
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
