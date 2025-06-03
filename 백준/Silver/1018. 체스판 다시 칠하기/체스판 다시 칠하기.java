3928fa82412f9acb33003d75a3832fa65dd4912d
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 10:30:33 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,89 +1,67 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static boolean[][] arr;
-	public static int min = 64;
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
- 
-		arr = new boolean[N][M];
-		
- 
-		// 배열 입력 
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			
-			for (int j = 0; j < M; j++) {
-				if (str.charAt(j) == 'W') {
-					arr[i][j] = true;		// W일 때는 true 
-				} else {
-					arr[i][j] = false;		// B일 때는 false
-				}
- 
-			}
-		}
- 
-		
-		int N_row = N - 7;
-		int M_col = M - 7;
- 
-		for (int i = 0; i < N_row; i++) {
-			for (int j = 0; j < M_col; j++) {
-				find(i, j);
-			}
-		}
-		System.out.println(min);
-	}
- 
-	
-	public static void find(int x, int y) {
-		int end_x = x + 8;
-		int end_y = y + 8;
-		int count = 0;
- 
-		boolean TF = arr[x][y];	// 첫 번째 칸의 색 
- 
-		for (int i = x; i < end_x; i++) {
-			for (int j = y; j < end_y; j++) {
- 
-				// 올바른 색이 아닐경우 count 1 증가 
-				if (arr[i][j] != TF) {	
-					count++;
-				}
-				
-				/* 
-				 * 다음 칸은 색이 바뀌므로
-				 * true라면 false로, false 라면 true 로
-				 * 값을 변경한다.
-				 */
-				TF = (!TF);
-			}
-			
-			TF = !TF;
-		}
-		
-		/*
-		 *  첫 번째 칸을 기준으로 할 때의 색칠 할 개수(count)와
-		 *  첫 번째 칸의 색의 반대되는 색을 기준으로 할 때의
-		 *  색칠 할 개수(64 - count) 중 최솟값을 count 에 저장 
-		 */
-		count = Math.min(count, 64 - count);
-		
-		/*
-		 * 이전까지의 경우 중 최솟값보다 현재 count 값이
-		 * 더 작을 경우 최솟값을 갱신 
-		 */
-		min = Math.min(min, count);
-	}
+   public static boolean[][] board;
+   public static int min = 64;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        board = new boolean[N][M];
+
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                if(str.charAt(j) == 'W')
+                    board[i][j] = true;
+                else
+                    board[i][j] = false;
+            }
+        }
+
+        int row = N - 7;
+        int col = M - 7;
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                chess(i, j);
+            }
+        }
+        sb.append(min).append("\n");
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static void chess(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int cnt = 0;
+
+        boolean tf = board[x][y];    // 첫 칸 색깔
+
+        for(int i = x; i < end_x; i++) {
+            for(int j = y; j < end_y; j++) {
+
+                if(board[i][j] != tf)
+                    cnt++;
+
+                tf = (!tf);
+            }
+
+            tf = (!tf);
+        }
+
+        cnt = Math.min(cnt, 64 - cnt);  // 시작이 B일 때와, W일 때 비교
+
+        min = Math.min(min, cnt);
+    }
 }
