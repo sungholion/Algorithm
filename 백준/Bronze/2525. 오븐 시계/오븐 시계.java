41401758ded40c8c6af61e828998e0cc6de52157
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 20:09:00 +0900

    [Bronze III] Title: 오븐 시계, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,26 +1,33 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
- 
         int C = Integer.parseInt(br.readLine());
- 
-        int min = 60 * A + B;   // 시 -> 분
-        min += C;
- 
-        int hour = (min / 60) % 24;
-        int minute = min % 60;
- 
-        System.out.println(hour + " " + minute);
- 
+
+        B += C;
+        if(B >= 60){
+            int plusA = (B / 60);
+            A += plusA;
+            B -= plusA * 60;
+        }
+
+        if(A >= 24)
+            A -= 24;
+        
+        sb.append(A).append(" ").append(B);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
