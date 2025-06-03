ce1979d4821c987199324e42b6404b89c7a8dc35
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 12:59:00 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,40 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int[] arr;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-      int n = Integer.parseInt(br.readLine());
-      while (n-- > 0) {
-         String s = br.readLine();
-         Stack<Character> stack = new Stack<>();
-
-         boolean flag = true;
-         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '('){
-               stack.push(c);
-            } else if(c == ')'){
-               if(!stack.isEmpty()){
-                  char pop = stack.pop();
-                  if(pop != '(') flag = false;
-               } else {
-                  flag = false;
-               }
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0){
+            Stack<Character> s = new Stack<>();
+            String str = br.readLine();
+            boolean flag = true;
+            for(int i = 0; i < str.length(); i++){
+                char c = str.charAt(i);
+                if(c == '('){
+                    s.push(c);
+                } else if(c == ')'){
+                    if(s.isEmpty()){
+                        flag = false;
+                        break;
+                    } else {
+                        s.pop();
+                    }
+                }
             }
-         }
+            if(flag && s.isEmpty())
+                sb.append("YES\n");
+            else
+                sb.append(("NO\n"));
+        }
 
-         if(!stack.isEmpty()){flag = false;}
-         if(flag){
-            bw.write("YES\n");
-         } else {
-            bw.write("NO\n");
-         }
-      }
-
-      bw.flush();
-      bw.close();
-      br.close();
-   }
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
