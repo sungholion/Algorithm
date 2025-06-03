359ef66d4fb6a37eab59ff9e7ff05a5fda46496d
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 21:15:14 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,30 +1,31 @@
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		int count = 0;
-		int value;
-		
-		while(true) {
-			
-			value = System.in.read();
-			
-			if(value == '\n') {
-				break;
-			}
-			
-			if(value < 68) count += 3;
-			else if(value < 71) count += 4;
-			else if(value < 74) count += 5;
-			else if(value < 77) count += 6;
-			else if(value < 80) count += 7;
-			else if(value < 84) count += 8;
-			else if(value < 87) count += 9;
-			else count += 10;
-			
-			
-		}
-		System.out.print(count);
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        int time = 0;
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if (c >= 'A' && c <= 'C') time += 3;
+            else if (c >= 'D' && c <= 'F') time += 4;
+            else if (c >= 'G' && c <= 'I') time += 5;
+            else if (c >= 'J' && c <= 'L') time += 6;
+            else if (c >= 'M' && c <= 'O') time += 7;
+            else if (c >= 'P' && c <= 'S') time += 8;
+            else if (c >= 'T' && c <= 'V') time += 9;
+            else if (c >= 'W' && c <= 'Z') time += 10;
+        }
+
+        sb.append(time);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
