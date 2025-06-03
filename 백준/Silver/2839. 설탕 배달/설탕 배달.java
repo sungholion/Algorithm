927f9bdcb0040f3921d281ea27bb8158018ad5e1
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 17:19:38 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,30 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-      int N = Integer.parseInt(br.readLine()); //배달해야하는 무게가 총 몇 kg
-      br.close();
+        int N = Integer.parseInt(br.readLine());
 
-      int basket =  0; //총 몇개의 봉지가 들어가는지
+        if(N == 4 || N == 7)
+            sb.append(-1);
+        else if(N % 5 == 0)
+            sb.append(N / 5);
+        else if( (N % 5 == 1) || (N % 5 == 3))
+            sb.append(((N / 5) + 1));
+        else if( (N % 5 == 2) || (N % 5 == 4))
+            sb.append(((N / 5) + 2));
 
-      while(N>0){
-         if(N % 5  == 0){ //5로 나누어지면
-            basket += N/5;
-            break;
-         }else { //만약 아니라면 3KG이거나 봉지에 담지 나눠담지 못하는 것.
-            N -= 3;
-            basket++;
-         }
-         if(N < 0){ //차피 나눠지지 않는다면 else문에서 -3을 했기 때문에 0보다 작을 것임...
-            basket = -1;
-         }
-      }
-      bw.write(basket + "");
-      bw.flush();
-      bw.close();
-      br.close();
-   }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
+
+/*
+    3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+    1-1 1 2-1 2 3 2  3  4  3  4  3  4  5  4  5  4  5  6  5  6  5  6  7  6  7  6     <- 봉지개수
+        1         2              3              4              5              6     <- 5의 몫
+    3 4 0 1 2 3 4 0  1  2  3  4  0  1  2  3  4  0  1  2  3  4  0  1  2  3  4  0     <- 나머지
+
+    1. 4와 7은 불가능
+    2. (5의 배수 + 1번째)는 몫 + 나머지
+    3. (5의 배수 + 3번째)는 (5의 배수 + 1번째)와 같음
+    4. (5의 배수 + 2번째)는 몫 + 나머지(2로 고정)
+    5. (5의 배수 + 4번째)는 (5의 배수 + 2번째)와 같음
+ */
