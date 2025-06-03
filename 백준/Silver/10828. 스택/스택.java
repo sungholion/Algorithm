dc475ec16b0391fd4e1b841b45ff48eae1a891c8
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 12:46:06 +0900

    [Silver IV] Title: 스택, Time: 172 ms, Memory: 19800 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,93 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static int[] stack;
-	public static int size = 0;
- 
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
- 
-		stack = new int[N];
-		
-		while (N-- > 0) {
-			st = new StringTokenizer(br.readLine(), " ");
- 
-			switch (st.nextToken()) {
-			
-			case "push":
-				push(Integer.parseInt(st.nextToken()));
-				break;
-				
-			case "pop":
-				sb.append(pop()).append('\n');
-				break;
-				
-			case "size":
-				sb.append(size()).append('\n');
-				break;
-				
-			case "empty":
-				sb.append(empty()).append('\n');
-				break;
-				
-			case "top":
-				sb.append(top()).append('\n');
-				break;
-			}
- 
-		}
-		System.out.println(sb);
-	}
- 
-	public static void push(int item) {
-		stack[size] = item;
-		size++;
-	}
-	
-	public static int pop() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			int res = stack[size - 1];
-			stack[size - 1] = 0;
-			size--;
-			return res;
-		}
-	}
-	
-	public static int size() {
-		return size;
-	}
-	
-	public static int empty() {
-		if(size == 0) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-	
-	public static int top() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			return stack[size - 1];
-		}
-	}
-	
+    public static int[] arr;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> s = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        while(N-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String com = st.nextToken();
+            if(com.equals("push")){
+                int num = Integer.parseInt(st.nextToken());
+                s.push(num);
+            } else if(com.equals("pop")){
+                if(s.isEmpty()){
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(s.pop()).append("\n");
+                }
+            } else if(com.equals("size")){
+                sb.append(s.size()).append("\n");
+            } else if(com.equals("empty")){
+                if(s.isEmpty()){
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if(com.equals("top")){
+                if(s.isEmpty()){
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(s.peek()).append("\n");
+                }
+            }
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
