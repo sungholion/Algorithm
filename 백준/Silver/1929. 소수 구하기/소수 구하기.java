31ad6a410ea850fffffc1d85fe34cb52bcc79edd
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 15:29:22 +0900

    [Silver III] Title: 소수 구하기, Time: 216 ms, Memory: 20872 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,38 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static boolean[] prime;
-	public static void main(String[] args) throws IOException {
+    public static boolean[] prime;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        prime = new boolean[N + 1];
+        get_prime();
+
+        for(int i = M; i <= N; i++)
+            if(!prime[i])
+                sb.append(i).append("\n");
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static void get_prime() {
  
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		
-		prime = new boolean[N + 1];
-		get_prime();
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = M; i <= N; i++) {
-			if(!prime[i]) sb.append(i).append('\n');
-		}
-		System.out.println(sb);
-	}
- 
-	public static void get_prime() {
-		prime[0] = prime[1] = true;
-		
-		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-			if(prime[i]) continue;
-			for(int j = i * i; j < prime.length; j += i) {
-				prime[j] = true;
-			}
-		}
-	}
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+
 }
- 
