18e9924a51a57164978f4ba780efb85b28366159
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 21:00:54 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,21 +1,21 @@
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		int A = 0;
-		int B = 0;
-		
-		A += System.in.read() - 48;
-		A += (System.in.read() - 48)*10;
-		A += (System.in.read() - 48)*100;
-		
-		System.in.read();
-		
-		B += System.in.read() - 48;
-		B += (System.in.read() - 48)*10;
-		B += (System.in.read() - 48)*100;
-		
-		System.out.println(A > B ? A : B);
-	}
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        sb.append(String.valueOf(Math.max(A, B)));
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
