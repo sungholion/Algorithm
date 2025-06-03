24bbfe8312ac86c0799721eda17f454b88442d11
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 15:15:29 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 23464 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -2,35 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-      int n = Integer.parseInt(st.nextToken());
-      int k = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-      Queue<Integer> q = new LinkedList<>();
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int i = 1; i <= N; i++)
+            q.offer(i);
 
-      for (int i = 1; i <= n; i++) {
-         q.offer(i);
-      }
+        sb.append("<");
+        while(q.size() != 1) {
+            for(int i = 0; i < K - 1; i++)
+                q.offer(q.poll());
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll());
+        sb.append(">");
 
-      sb.append("<");
-      while (q.size() > 1) {
-         for(int i = 0; i < k-1; i++){
-            q.offer(q.poll());
-         }
-
-         sb.append(q.poll()).append(", ");
-      }
-      sb.append(q.poll()).append(">");
-
-      bw.write(sb.toString());
-
-      bw.flush();
-      bw.close();
-      br.close();
-   }
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
