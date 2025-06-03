b51a12abb8de934b1f7ac2e65c53f2e22aab570f
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 20:30:47 +0900

    [Bronze II] Title: 공 바꾸기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,37 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[N];
+        int[] basket = new int[N+1];
+        for(int i = 1; i <= N; i++)
+            basket[i] = i;
+        
+        for(int t = 0; t < M; t++){
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = i+1;
+            int temp = basket[i];
+            basket[i] = basket[j];
+            basket[j] = temp;
         }
 
-        for (int i = 0; i < M; i++) {
-            int tmp;
-            st = new StringTokenizer(br.readLine(), " ");
-            int F = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= N; i++)
+            sb.append(basket[i]).append(" ");
 
-            tmp = arr[F-1];
-            arr[F-1] = arr[E-1];
-            arr[E-1] = tmp;
-        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
         br.close();
 
-        for (int baguni : arr)
-            sb.append(baguni).append(" ");
-            System.out.println(sb);
     }
 }
