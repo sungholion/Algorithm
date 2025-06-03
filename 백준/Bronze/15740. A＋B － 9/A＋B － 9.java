ebe293232a88daf65e4ad86bcffa050c87962ecb
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 19:41:13 +0900

    [Bronze V] Title: A+B - 9, Time: 208 ms, Memory: 17380 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -3,12 +3,21 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        BigInteger a = new BigInteger(st.nextToken());
-        BigInteger b = new BigInteger(st.nextToken());
-        System.out.print(a.add(b));
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        sb.append(A.add(B));
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
