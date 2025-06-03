f7b1550e4b93333a846e19aaa73e30b1c2b4c3ce
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 20:00:34 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,23 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int res1 = (A+B) % C;
+        int res2 = ((A % C) + (B % C)) % C;
+        int res3 = (A * B) % C;
+        int res4 = ((A % C) * (B % C)) % C;
+        sb.append(res1).append("\n").append(res2).append("\n").append(res3).append("\n").append(res4);
         
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
- 		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str," ");
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-        
-		System.out.println( (A+B)%C );
-		System.out.println( (A%C + B%C)%C );
-		System.out.println( (A*B)%C );
-		System.out.println( (A%C * B%C)%C );
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
