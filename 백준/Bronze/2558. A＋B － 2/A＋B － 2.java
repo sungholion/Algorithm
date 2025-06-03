92a3e692344d33ff1a126d52afaca28f4659a3e0
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-03 19:32:16 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,12 +1,21 @@
 import java.io.*;
 import java.util.*;
-public class Main {
 
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int x = Integer.parseInt(br.readLine());
-        int y = Integer.parseInt(br.readLine());
-        System.out.print(x + y);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        sb.append(A+B);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
