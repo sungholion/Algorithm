f3a27376b314a5260f488183926023e700c07711
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-04 16:03:23 +0900

    [Silver V] Title: 집합, Time: 1492 ms, Memory: 314336 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,56 +1,51 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
-		StringTokenizer st;
-		int m = Integer.parseInt(br.readLine());
-		String op;
-		int x;
-		
-		int S = 0;
-		for(int t=0; t<m; t++) {
-			st = new StringTokenizer(br.readLine());
-			op = st.nextToken();
-			if(!op.equals("all") && !op.equals("empty")){
-				x = Integer.parseInt(st.nextToken());
-				if(op.equals("add")) {
-					S = S | (1 << x);
-				}
-				else if(op.equals("remove")) {
-					S = S & ~(1 << x);
-				}
-				else if(op.equals("check")) {
-					if(((1<<x)&S) > 0)
-						bw.write("1\n");
-					else
-						bw.write("0\n");
-				}
-				else if(op.equals("toggle")) {
-					if(((1<<x) & S) > 0)
-						S = S & ~(1 << x);
-					else
-						S = S | (1 << x);
-				}
-			}
-			else {
-				if(op.equals("all")) {
-					S = S | (1 << 21) - 1;
-				}
-				else if(op.equals("empty")) {
-					S = 0;
-				}
-			}
-		}
-		bw.flush();
-		br.close();
-		bw.close();
-	}
-	
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int M = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int x = 0;
+
+            if(command.equals("all")){
+                set.clear();
+                for(int i = 1; i <= 20; i++)
+                    set.add(i);
+            } else if(command.equals("empty")){
+                set.clear();
+            } else if(command.equals("add")){
+                x = Integer.parseInt(st.nextToken());
+                set.add(x);
+            } else if(command.equals("remove")){
+                x = Integer.parseInt(st.nextToken());
+                set.remove(x);
+            } else if(command.equals("check")){
+                x = Integer.parseInt(st.nextToken());
+                sb.append(set.contains(x) ? 1 : 0).append("\n");
+            } else if(command.equals("toggle")){
+                x = Integer.parseInt(st.nextToken());
+                if(set.contains(x)){
+                    set.remove(x);
+                } else {
+                    set.add(x);
+                }
+            }
+
+
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
