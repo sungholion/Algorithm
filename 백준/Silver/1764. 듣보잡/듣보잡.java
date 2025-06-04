1fc05b5b9e6fb1d67ff9866ebfea0c3eefad3881
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-04 15:26:46 +0900

    [Silver IV] Title: 듣보잡, Time: 424 ms, Memory: 30416 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,53 +1,52 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 
-public class Main{
-	static int n, m;
-	
-    public static void main(String[] args) throws IOException{
-//        System.setIn(new FileInputStream("src/input.txt"));
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        HashSet<String> dontListen = new HashSet<String>();
-        
-        for(int i=0; i<n; i++) {
-        	dontListen.add(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < N; i++) {
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0) + 1);
         }
-        
-        List<String> dontSeeListen = new ArrayList<>();
-        for(int i=0; i<m; i++) {
-        	String str = br.readLine();
-        	if(dontListen.contains(str)) {
-        		dontSeeListen.add(str);
-        	}
+
+        for(int i = 0; i < M; i++) {
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0) + 1);
         }
-        
-        Collections.sort(dontSeeListen);
-        sb.append(dontSeeListen.size() + "\n");
-        for(String str : dontSeeListen) {
-        	sb.append(str + "\n");
+
+        int cnt = 0;
+        for(String x : map.keySet()) {
+            if(map.get(x) > 1) {
+                cnt++;
+            }
         }
+
+        sb.append(cnt + "\n");
+        String[] s = new String[cnt];
+        int i = 0;
+        for(String x : map.keySet()) {
+            if(map.get(x) > 1) {
+                s[i++] = x;
+            }
+        }
+        Arrays.sort(s);
+        for(String x : s) {
+            sb.append(x + "\n");
+        }
+
+
+        br.close();
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-        br.close();
-        
     }
-    
-    
 }
