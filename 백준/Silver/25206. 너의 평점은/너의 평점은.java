64c43f6bbba8d31b71982bbb5b6a5aea53c6cb2f
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 11:29:47 +0900

    [Silver V] Title: 너의 평점은, Time: 120 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,65 +1,44 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-// 전공 평점 = (SUM(학점X과목평점)) / SUM(학점)
-// 과목 평점 = 등급에 따라, P는 제외
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        double sumNumxScore = 0;
-        double sumNum = 0;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        Map<String, Double> map = new HashMap<>();
+        map.put("A+", 4.5);
+        map.put("A0", 4.0);
+        map.put("B+", 3.5);
+        map.put("B0", 3.0);
+        map.put("C+", 2.5);
+        map.put("C0", 2.0);
+        map.put("D+", 1.5);
+        map.put("D0", 1.0);
+        map.put("F", 0.0);
 
-        for(int i = 0; i < 20; i++){
-            st = new StringTokenizer(br.readLine());
-            String object = st.nextToken();
-            double num = Double.parseDouble(st.nextToken());
-            String alphabet = st.nextToken();
-            double score = 0;
+        int N = 20;
+        double value_sum = 0;
+        double sum = 0;
+        while(N-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            double value = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
 
-            // P가 아닐 때만 체크
-            if(!(alphabet.equals("P"))){
-                // 과목 평점 계산
-                switch (alphabet) {
-                    case "A+":
-                        score = 4.5;
-                        break;
-                    case "A0":
-                        score = 4.0;
-                        break;
-                    case "B+":
-                        score = 3.5;
-                        break;
-                    case "B0":
-                        score = 3.0;
-                        break;
-                    case "C+":
-                        score = 2.5;
-                        break;
-                    case "C0":
-                        score = 2.0;
-                        break;
-                    case "D+":
-                        score = 1.5;
-                        break;
-                    case "D0":
-                        score = 1.0;
-                        break;
-                    case "F":
-                        score = 0.0;
-                        break;
-                    default:
-                        break;
-                }
+            if(grade.equals("P"))
+                continue;
 
-                sumNumxScore += (num*score);
-                sumNum += num;
-            }
+            value_sum += value;
+            sum += value * map.get(grade);
         }
-        // 소수점 아래 6자리까지 출력
-        System.out.printf("%.6f", sumNumxScore/sumNum);
+
+        sb.append(sum/value_sum).append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
