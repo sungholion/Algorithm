30557dd53b9b0c26467af1c2cacf6c4bc3db8a09
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 15:00:18 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.java
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -1,6 +1,18 @@
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("강한친구 대한육군\n강한친구 대한육군");
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
