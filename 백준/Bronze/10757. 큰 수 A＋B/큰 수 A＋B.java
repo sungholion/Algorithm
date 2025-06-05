1167ac2189c6383874dd4e8f17c4032390d27910
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 15:15:51 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 212 ms, Memory: 17500 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -1,19 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
 import java.math.BigInteger;
- 
+import java.util.*;
+
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		BigInteger A = new BigInteger(st.nextToken());
-		BigInteger B = new BigInteger(st.nextToken());
-		
-		A = A.add(B);
-		System.out.println(A.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        sb.append(A.add(B));
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
