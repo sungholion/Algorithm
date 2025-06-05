76ac54eb52ed4d44093fb8878c8369d37190fa5e
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 09:25:19 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,34 +1,21 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int king = 1;
-		int queen = 1;
-		int rook = 2;
-		int bishop = 2;
-		int knight = 2;
-		int pawn = 8;
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		king = king - Integer.parseInt(st.nextToken());
-		queen = queen - Integer.parseInt(st.nextToken());
-		rook = rook - Integer.parseInt(st.nextToken());
-		bishop = bishop - Integer.parseInt(st.nextToken());
-		knight = knight - Integer.parseInt(st.nextToken());
-		pawn = pawn - Integer.parseInt(st.nextToken());
- 
-		System.out.print(king + " ");
-		System.out.print(queen + " ");
-		System.out.print(rook + " ");
-		System.out.print(bishop + " ");
-		System.out.print(knight + " ");
-		System.out.print(pawn + " ");		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int[] ans = new int []{1, 1, 2, 2, 2, 8};
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 6; i++)
+            sb.append(ans[i] - Integer.parseInt(st.nextToken())).append(' ');
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
