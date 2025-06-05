4bd8bc0cf9a1d0cd5ad0e9e28a4dbabeae3fb489
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 12:28:09 +0900

    [Bronze I] Title: 세로읽기, Time: 116 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,32 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		char[][] c_arr = new char[5][15];
-		int i,j;
-		int max = 0;
-		
-		for(i=0;i<5;i++) {
-			String str = sc.next();
-			
-			if(str.length()>max) 
-				max = str.length();
-			
-			for(j=0;j<str.length();j++) {
-				c_arr[i][j] = str.charAt(j);
-			}
-		}
-		
-		
-		for(i=0;i<max;i++) {
-			for(j=0;j<5;j++) {
-				if(c_arr[j][i] == '\0')
-					continue;
-				System.out.print(c_arr[j][i]);
-			}
-		}
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        char[][] arr = new char[5][15];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                arr[i][j] = ' ';
+            }
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            String str = br.readLine();
+            for(int j = 0; j < str.length(); j++)
+                arr[i][j] = str.charAt(j);
+        }
+
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 5; j++){
+                if(arr[j][i] != ' ')
+                    sb.append(arr[j][i]);}
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
