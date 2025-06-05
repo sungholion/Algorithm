a7cafa879e157d6b84bb2530e8755580e8fc0e4c
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 12:18:12 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,33 +1,37 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		int i,j;
-		int max = 0;
-		int x = 1, y = 1;
-		
-		int arr[][] = new int[9][9];
-		
-		for(i=0;i<9;i++) {
-			for(j=0;j<9;j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		
-		for(i=0;i<9;i++) {
-			for(j=0;j<9;j++) {
-				if(arr[i][j]>max) {
-					max = arr[i][j];
-					x = i+1;
-					y = j+1;
-				}
-			}
-		}
-		sc.close();
-		
-		System.out.println(max);
-		System.out.println(x+" "+y);
-	}
+        int n = 9;
+
+        int[][] arr = new int[n][n];
+        int max = Integer.MIN_VALUE;
+        int maxRow = -1;
+        int maxCol = -1;
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                    maxRow = i + 1;
+                    maxCol = j + 1;
+                }
+            }
+        }
+
+        sb.append(max).append("\n");
+        sb.append(maxRow).append(" ").append(maxCol).append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
