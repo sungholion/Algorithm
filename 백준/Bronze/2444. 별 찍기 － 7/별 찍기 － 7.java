0c0971894a6451fd16002cef31b201164ea0a1fb
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 09:37:36 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 112 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,28 +1,33 @@
 import java.io.*;
 import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++) {
-                sb.append(" ");
-            }
-            for (int j = 1; j <= 2*i-1; j++) {
-                sb.append("*");
-            }
-            sb.append("\n");
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for(int j = 0; j < N - i; j++)
+                sb.append(' ');
+            for(int j = 0; j < (2 * i - 1); j++)
+                    sb.append('*');
+            sb.append('\n');
         }
-        for (int i = n-1; i >= 1; i--) {
-            for (int j = 1; j <= n-i; j++) {
-                sb.append(" ");
-            }
-            for (int j = 1; j <= 2*i-1; j++) {
-                sb.append("*");
-            }
-            sb.append("\n");
+
+        for (int i = N - 1; i >= 1; i--) {
+            for(int j = 0; j < N - i; j++)
+                sb.append(' ');
+            for(int j = 0; j < (2 * i - 1); j++)
+                sb.append('*');
+            sb.append('\n');
         }
-        System.out.print(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
