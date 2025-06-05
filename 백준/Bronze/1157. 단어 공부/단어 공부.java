7b777472657f933f3c31f152b8cc4c14796a1101
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 10:11:02 +0900

    [Bronze I] Title: 단어 공부, Time: 404 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,36 +1,46 @@
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		int[] arr = new int[26]; 
- 
-		int c = System.in.read();
- 
-		while (c > 64) {	
-			
-			if (c < 91) {
-				arr[c - 65]++;
-			} else {
-				arr[c - 97]++;
-			}
-			c = System.in.read();
-		}
- 
- 
-		int max = -1;
-		int ch = -2;	
- 
-		for (int i = 0; i < 26; i++) {
- 
-			if (arr[i] > max) {
-				max = arr[i];
-				ch = i;
-			} else if (arr[i] == max) {
-				ch = -2;
-			}
-		}
-		System.out.print((char) (ch+65));
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        str = str.toUpperCase();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < str.length(); i++) {
+            int val = map.get(str.charAt(i));
+            if(val > max) {
+                max = val;
+            }
+        }
+
+        int cnt = 0;
+        char maxChar = ' ';
+        for(Character c : map.keySet()) {
+            if(map.get(c) == max) {
+                maxChar = c;
+                cnt++;
+            }
+        }
+
+        if(cnt > 1)
+            sb.append('?');
+        else
+            sb.append(maxChar);
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
