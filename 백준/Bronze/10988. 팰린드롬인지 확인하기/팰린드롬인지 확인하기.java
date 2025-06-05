01d07043f9f8aaff80d52930786fc3b3ababe7ce
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-05 09:42:09 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,23 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.next();
-		sc.close();
-		int len = str.length();
-		int ans = 1;
-		
-		StringBuilder sb = new StringBuilder(str);
-		sb.reverse();
-		
-		for(int i=0;i<len/2;i++) {
-			if(str.charAt(i) != sb.charAt(i)) {
-				ans = 0;
-			}
-		}
-		System.out.println(ans);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        boolean flag = true;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag)
+            sb.append(1);
+        else
+            sb.append(0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
