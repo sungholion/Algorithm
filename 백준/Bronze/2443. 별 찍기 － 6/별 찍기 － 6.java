1707e9d65a3a2578bb604fa1d78d4f7b5c0b3456
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-07 16:49:53 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -1,24 +1,34 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	int N = Integer.parseInt(br.readLine());
-	
-	for(int i=1; i<=N; i++) {
-		for(int j=1; j<i; j++) bw.write(" ");
-		for(int k=0; k<2*N-(2*i-1); k++) bw.write("*");
-		bw.write("\n");
-	}
+    public static boolean[] prime;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-	bw.flush();
-	bw.close();
-	br.close();
-	}
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+
+            for(int j = 0; j < i; j++) {
+                sb.append(' ');
+            }
+
+            for(int j = 0; j < (2*N-1-(2*i)); j++) {
+                sb.append('*');
+            }
+
+            sb.append("\n");
+        }
+
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+
 }
