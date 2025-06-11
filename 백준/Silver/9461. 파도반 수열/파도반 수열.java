dd462eb63641bfb721492ae2468e652f089e875e
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-11 10:18:56 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,35 +1,32 @@
 import java.io.*;
+import java.util.*;
+
 
 public class Main {
-	static long[] wave = new long[101];
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		wave[0] = 0L;
-		wave[1] = 1L;
-		wave[2] = 1L;
-		wave[3] = 1L;
-		
-		waveHalf();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int loop = 0; loop < T; loop++) {
-			int n = Integer.parseInt(br.readLine());
-			sb.append(wave[n]+"\n");
-		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	static void waveHalf() {
-		for(int i = 4; i< 101; i++)
-			wave[i] = wave[i-2] + wave[i-3];
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            long[] dp = new long[101];
+
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 1;
+            dp[3] = 1;
+
+            for(int i=4; i<101; i++)
+                dp[i] = dp[i-2] + dp[i-3];
+            sb.append(dp[N]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
 }
