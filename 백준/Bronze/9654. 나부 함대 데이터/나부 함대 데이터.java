0d899baf155273aa446326c7ec27571c734b6ca1
Sungho Cho <102495970+sungholion@users.noreply.github.com>
2025-06-15 23:42:42 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -3,11 +3,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String s = "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
                 "N2 Bomber      Heavy Fighter  Limited    21        \n" +
                 "J-Type 327     Light Combat   Unlimited  1         \n" +
                 "NX Cruiser     Medium Fighter Limited    18        \n" +
                 "N1 Starfighter Medium Fighter Unlimited  25        \n" +
-                "Royal Cruiser  Light Combat   Limited    4         ");
+                "Royal Cruiser  Light Combat   Limited    4         \n";
+
+        sb.append(s);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
+
+
+
 }
