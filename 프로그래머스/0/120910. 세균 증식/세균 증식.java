47bd4b1309e1a96705c8e82d72c2752f691c417a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-08-05 22:23:28 +0900

    [level 0] Title: 세균 증식, Time: 0.06 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = 0;
-        answer = n * (int) Math.pow(2, t);
-        return answer;
+        return (int)Math.pow(2, t) * n;
     }
 }
