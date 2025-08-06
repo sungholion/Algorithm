d2f73a55882998c467ec66b1c692e3471f515f94
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-08-06 16:54:44 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.61 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,36 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
+        Arrays.sort(array);
+        int big = array[array.length-1];
+        int[] cnt = new int[big+1];
         
-        int[] cnt = new int[1000];
+        for(int x : array)
+            cnt[x]++;
         
-        for(int i=0; i<array.length; i++){
-            int num = array[i];
-            cnt[num]++;
-        }
-        
-        int max = -1;
-        for(int i=0; i<1000; i++){
-            if(cnt[i] > max){
-                max = cnt[i];
-                answer = i;
-            } 
-        }
-        
+        int maxCount = 0;
+        int answer = -1;
 
-        
-        int freq = 0;
-        for(int i=0; i<1000; i++){
-            if(cnt[i] == max){
-                freq++;
-            }
-            
-            if(freq >= 2){
+        for (int i = 0; i < cnt.length; i++) {
+            if (cnt[i] > maxCount) {
+                maxCount = cnt[i];
+                answer = i;
+            } else if (cnt[i] == maxCount && maxCount != 0) {
                 answer = -1;
-                break;
             }
         }
-        
         
         return answer;
     }
