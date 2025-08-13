9cf7fb69185f550cd950876f04f29aadebcb4540
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-08-13 13:38:15 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        
+        // 시작점 계산
+        int start = total / num - (num - 1) / 2;
+
+        for (int i = 0; i < num; i++) {
+            answer[i] = start + i;
+        }
+        
+        return answer;
+    }
+}
