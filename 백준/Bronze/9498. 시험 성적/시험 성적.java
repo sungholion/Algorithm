0644984ee9aaaaf5ab1935f8f1ef652d483c49e6
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-08 12:59:33 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,27 +1,20 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int score = Integer.parseInt(br.readLine().trim());
 
-        int n = Integer.parseInt(br.readLine());
-
-        if(n <= 100 && n >= 90){
-            bw.write("A");
-        } else if(n < 90 && n >= 80){
-            bw.write("B");
-        } else if(n < 80 && n >= 70){
-            bw.write("C");
-        } else if(n < 70 && n >= 60){
-            bw.write("D");
-        } else{
-            bw.write("F");
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
         }
-
-        br.close();
-        bw.flush();
-        bw.close();
     }
 }
