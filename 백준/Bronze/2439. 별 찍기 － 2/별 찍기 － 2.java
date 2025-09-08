df78a1e7d15d059255f95b054271ac8b5154855b
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-08 11:48:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -2,24 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N-i-1; j++) {
+            for(int k = 0; k < N-i-1 ; k++) {
                 bw.write(" ");
             }
-            for(int k = 0; k <= i; k++) {
+            for(int j = 0; j <= i; j++) {
                 bw.write("*");
             }
             bw.write("\n");
         }
 
-        br.close();
         bw.flush();
-        bw.close();
     }
 }
