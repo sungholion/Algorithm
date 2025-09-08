e17bb360215732afc5daa0ef2411b104b696e612
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-08 12:43:33 +0900

    [Bronze V] Title: N 찍기, Time: 240 ms, Memory: 22128 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -2,18 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= n; i++) {
-            bw.write(i + "\n");
-        }
-
-        br.close();
-        bw.flush();
-        bw.close();
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= N; i++) 
+            sb.append(i + "\n");
+        System.out.println(sb);
     }
 }
