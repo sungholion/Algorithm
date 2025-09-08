6758d03a5d188cdc7ad7b00ecf1a8c3ec1df41cf
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-08 12:52:40 +0900

    [Bronze III] Title: 알람 시계, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -2,28 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int H = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         M -= 45;
-
         if(M < 0){
             M += 60;
             H -= 1;
         }
 
-        if(H < 0){
+        if(H < 0)
             H += 24;
-        }
 
-        bw.write(H + " " + M + "\n");
-        br.close();
-        bw.flush();
-        bw.close();
+        sb.append(H).append(" ").append(M);
+        System.out.println(sb);
+
     }
 }
