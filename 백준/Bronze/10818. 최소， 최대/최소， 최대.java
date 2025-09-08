edc94809f3ac76a40e7300aed2aa2e4f47d5dec1
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-08 18:35:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 88300 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -2,34 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            if(x > max) max = x;
+            if(x < min) min = x;
         }
 
-        for (int i = 0; i < N; i++) {
-            if(arr[i] > max){
-                max = arr[i];
-            }
-
-            if(arr[i] < min){
-                min = arr[i];
-            }
-        }
-
-        bw.write(min + " " + max + "\n");
-        br.close();
-        bw.flush();
-        bw.close();
+        sb.append(min + " " + max + "\n");
+        System.out.println(sb);
     }
 }
