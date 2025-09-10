b7fee28edf6801c7b2b7e6d23207da0fb4ec8c32
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 15:55:25 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -2,24 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long V = Integer.parseInt(st.nextToken());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-
-        int res = (int)Math.ceil((double)(V-B) / (A-B));
-
-        sb.append(res).append("\n");
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        long result = (V - B - 1) / (A - B) + 1;
+        sb.append(result);
+        System.out.println(sb.toString());
     }
-
 }
