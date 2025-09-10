b824546e6de35049612e23f07aa32fe23c9decd7
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 19:10:12 +0900

    [Silver V] Title: 나이순 정렬, Time: 568 ms, Memory: 46516 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,42 +1,34 @@
 import java.io.*;
 import java.util.*;
 
-class Person{
+class Member{
     int age;
     String name;
 
-    Person(int age, String name){
+    Member(int age, String name){
         this.age = age;
         this.name = name;
     }
 }
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        Person[] p = new Person[N];
 
+        int N = Integer.parseInt(br.readLine());
+        Member[] members = new Member[N];
         for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            p[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken(); 
+            members[i] = new Member(age, name);
         }
 
-        Arrays.sort(p, new Comparator<Person>(){
-            public int compare(Person o1, Person o2){
-                return o1.age - o2.age;
-            }
-        });
+        Arrays.sort(members, (a, b) -> a.age - b.age);
 
-        for(int i = 0; i < N; i++){
-            sb.append(p[i].age).append(" ").append(p[i].name).append("\n");
-        }
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        for(Member m : members)
+            sb.append(m.age).append(" ").append(m.name).append("\n");
+        System.out.println(sb.toString());
     }
 }
