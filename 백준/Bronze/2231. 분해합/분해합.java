e8f900917a7c31ad19237ddb0c538bb2c151c1cd
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 13:45:27 +0900

    [Bronze II] Title: 분해합, Time: 128 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,38 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static final int MOD = 1234567891;
-    static final int R = 31;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        for(int i=1; i<=N; i++){
-            int num = i;
-            int sum = i;
-            while(num != 0){
-                sum += num % 10;
-                num /= 10;
+        for(int i = 1; i<=N; i++){
+            int sum = 0;
+            int x = i;
+            sum+= x;
+
+            while(x!=0){
+                sum += x % 10;
+                x/= 10;
             }
 
             if(sum == N){
-                sb.append(i);
+                sb.append(i).append("\n");
                 break;
             }
-
+            
             if(i == N){
-                sb.append(0);
+                sb.append(0).append("\n");
                 break;
             }
         }
 
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
     }
 }
