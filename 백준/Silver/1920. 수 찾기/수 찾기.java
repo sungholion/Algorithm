e014e00e72bdc6a9f156147cc9c58455a983d702
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 18:44:56 +0900

    [Silver IV] Title: 수 찾기, Time: 536 ms, Memory: 50788 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,51 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] arr;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
+        Set<Integer> set = new HashSet<>();
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);  // 이분 탐색하려면 정렬 되어있어야 함.
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(N-- > 0) set.add(Integer.parseInt(st.nextToken()));
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        int key = -1;
-        for(int i=0; i<M; i++){
-            key = Integer.parseInt(st.nextToken());
-            sb.append(binarysearch(key)).append("\n");
-        }
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
-
-    public static int binarysearch(int key){
-        int low = 0;
-        int high = arr.length - 1;
-
-        while(low <= high){
-            int mid = (low + high) / 2;
-
-            if(key < arr[mid]){
-                high = mid - 1;
-            }
-            else if(key > arr[mid]){
-                low = mid + 1;
-            }
-            else{
-                return 1;
+        while(M --> 0){
+            if(set.contains(Integer.parseInt(st.nextToken()))){
+                sb.append(1).append("\n");
+            } else{
+                sb.append(0).append("\n");
             }
         }
-        return 0;
+
+        System.out.println(sb.toString());
     }
 }
