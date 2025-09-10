c9d4bbeb31f9cd563faeabe258b01f75db84f91e
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 18:01:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 85524 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -2,28 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
 
-        int movie = 666;
-        int cnt = 1;
-      
-        while(cnt != N){
-            movie++;
-            if(String.valueOf(movie).contains("666"))
-                cnt++;
+        int num = 665;
+        while(cnt < N){
+            num++;
+            if(String.valueOf(num).contains("666")) cnt++;
         }
-        
-
-        sb.append(movie);
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(num);
     }
-
-
 }
