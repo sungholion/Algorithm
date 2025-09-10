94edd6fb447ed66b668443939e4a175a488c5f50
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 14:10:18 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,28 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int add = 1;
-        int cnt = 1;    // 1번방도 count
-        int line = 1;
-
-        while (add < N) {
-            cnt++;
-            add += 6 * line;
-            line++;
+        if(N == 1){
+            System.out.println(1);
+            return;
         }
+            int bee = 1;
+            int ans = 1;
+            while(bee < N){
+                bee += 6 * ans;
+                ans++;
+            }
+            sb.append(ans).append("\n");
 
-        sb.append(cnt);
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
     }
 }
