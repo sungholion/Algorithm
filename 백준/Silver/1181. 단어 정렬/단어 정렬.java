7e82eb79059f4585dbe0d7da3d8c08055e416944
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 17:24:03 +0900

    [Silver V] Title: 단어 정렬, Time: 324 ms, Memory: 27284 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,36 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        String[] str = new String[N];
+        Set<String> set = new HashSet<>();
+        while(N-- > 0) set.add(br.readLine());
 
-        for(int i = 0; i < N; i++)
-            str[i] = br.readLine();
+        List<String> words = new ArrayList<>(set);
 
-        Arrays.sort(str, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.length() == o2.length())
-                    return o1.compareTo(o2);
-                else
-                    return o1.length() - o2.length();
-            }
+        words.sort((a,b) -> {
+            if(a.length() != b.length()) return a.length() - b.length();
+            return a.compareTo(b);
         });
 
-        sb.append(str[0]).append("\n");
-        for(int i=1; i<N; i++){
-            if(!str[i].equals(str[i-1]))
-                sb.append(str[i]).append("\n");
+        for(String word : words) {
+            sb.append(word).append("\n");
         }
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
     }
 }
