40b173bd45f0d1cb682880ecd304797277eb4065
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-10 17:03:21 +0900

    [Bronze I] Title: ISBN, Time: 100 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.java
+++ 백준/Bronze/14626. ISBN/ISBN.java
@@ -2,40 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean[] prime;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
-        int sum = 0;
-        int multiplier = 1;
-        int x_multiplier = 1;
-        int m = str.charAt(str.length()-1) - '0';
-        for(int i = 0; i < str.length()-1; i++) {
-            char c = str.charAt(i);
-            multiplier = (i % 2 == 0) ? 1 : 3;
-
-            if(c != '*'){   // 훼손되지 않은 숫자는 규칙에 따라 더하기
-                sum += (c - '0') * multiplier;
-            } else {
-               x_multiplier =  multiplier;
-               continue;
+        int loc = -1;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '*'){
+                loc = i;
+                break;
             }
         }
 
-        for(int i = 0; i < 10; i++){
-            if((sum + (i * x_multiplier) + m) % 10 == 0)
-                sb.append(i);
+        for(int i = 0; i<=9; i++){
+            int sum = 0;
+            for(int j = 0; j<str.length(); j++){
+                int w = j % 2 == 0 ? 1 : 3;
+                int d = j == loc ? i : str.charAt(j) - '0';
+                sum += d * w;
+            }
+            if(sum % 10 == 0){
+                System.out.println(i);
+                return;
+            }
         }
-
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
     }
-
-
 }
