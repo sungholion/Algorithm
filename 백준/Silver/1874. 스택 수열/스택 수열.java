67195cdbef3b7803fbb5dff34b0643ab89066a5c
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-11 21:31:45 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 26704 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,36 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        int start = 0;
         Stack<Integer> s = new Stack<>();
-        while(n-- > 0) {
-            int num = Integer.parseInt(br.readLine());
 
-            if(num > start){
-                for(int i = start + 1; i <= num; i++){
-                    s.push(i);
-                    sb.append('+').append("\n");
-                }
-                start = num;
-            } else if(s.peek() != num){
-                System.out.println("NO");
-                return;
+        int start = 1;
+        boolean possible = true;
+        while(n --> 0){
+            int x = Integer.parseInt(br.readLine());
+            while(start <= x){
+                s.push(start++);
+                sb.append("+\n");
+            }
+
+            if(s.empty() || s.peek() != x){
+                possible = false;
+                break;
             }
 
             s.pop();
-            sb.append('-').append("\n");
+            sb.append("-\n");
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-
+        if(possible)
+            System.out.println(sb);
+        else
+            System.out.println("NO");
     }
 }
+
