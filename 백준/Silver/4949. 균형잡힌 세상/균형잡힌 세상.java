7bc221076f9f959e1158c06d41e5422030337269
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-11 21:00:11 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 176 ms, Memory: 18568 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -2,50 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         String str;
         while(!(str = br.readLine()).equals(".")){
             Stack<Character> s = new Stack<>();
-            boolean tf = true;
-
+            boolean isBalanced = true;
             for(int i = 0; i < str.length(); i++){
                 char c = str.charAt(i);
-                if(c == '(')
-                    s.push(c);
+                char tmp;
+
+                if(c == '(') s.push(c);
+                else if(c == '[') s.push(c);
                 else if(c == ')'){
-                    if(s.isEmpty() || s.peek() != '('){
-                        tf = false;
+                    if(s.isEmpty()){
+                        isBalanced = false;
                         break;
                     }
-                    else
-                        s.pop();
+                    tmp = s.pop();
+                    if((tmp != '(')){
+                        isBalanced = false;
+                        break;
+                    }
+                } else if(c == ']'){
+                    if(s.isEmpty()){
+                        isBalanced = false;
+                        break;
+                    }
+                    tmp = s.pop();
+                    if((tmp != '[')){
+                        isBalanced = false;
+                        break;
+                    }
                 }
-                else if(c == '[')
-                    s.push(c);
-                else if(c == ']'){
-                    if(s.isEmpty() || s.peek() != '['){
-                        tf = false;
-                        break;
-                    }
-                    else
-                        s.pop();
-                } else  // 그냥 알파벳일때
-                    continue;
             }
 
-            if(tf && s.isEmpty())
-                sb.append("yes\n");
-            else
-                sb.append("no\n");
+            if(!s.isEmpty()) isBalanced = false;
+            if(isBalanced) sb.append("yes\n");
+            else sb.append("no\n");
+
         }
 
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
+
