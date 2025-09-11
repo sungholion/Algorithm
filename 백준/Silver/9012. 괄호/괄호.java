05ad8372809cf124c81cdaa9dbd135606b7d70b6
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-11 21:06:38 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,39 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] arr;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        while(N-- > 0){
-            Stack<Character> s = new Stack<>();
+        int T = Integer.parseInt(br.readLine());
+        while(T--> 0){
             String str = br.readLine();
-            boolean flag = true;
+            Stack<Character> s = new Stack<>();
+            boolean isBalanced = true;
+
             for(int i = 0; i < str.length(); i++){
                 char c = str.charAt(i);
-                if(c == '('){
-                    s.push(c);
-                } else if(c == ')'){
-                    if(s.isEmpty()){
-                        flag = false;
+                if(c == '(') s.push(c);
+                else if(c == ')'){
+                    if(s.isEmpty()) {
+                        isBalanced = false;
                         break;
-                    } else {
-                        s.pop();
+                    }
+                    char temp = s.pop();
+                    if(temp != '('){
+                        isBalanced = false;
+                        break;
                     }
                 }
             }
-            if(flag && s.isEmpty())
-                sb.append("YES\n");
-            else
-                sb.append(("NO\n"));
-        }
 
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+            if(!s.isEmpty()) isBalanced = false;
+            if(isBalanced) sb.append("YES\n");
+            else sb.append("NO\n");
+
+
+        }
+        System.out.println(sb);
     }
 }
+
