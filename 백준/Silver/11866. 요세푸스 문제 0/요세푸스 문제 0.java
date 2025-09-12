2c14f1a58b7bf1657e689e266facc9128ed76fb4
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-12 15:20:56 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 140 ms, Memory: 23096 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -2,32 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] arr;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
-        LinkedList<Integer> q = new LinkedList<>();
-        for(int i = 1; i <= N; i++)
-            q.offer(i);
-
         sb.append("<");
-        while(q.size() != 1) {
-            for(int i = 0; i < K - 1; i++)
-                q.offer(q.poll());
-            sb.append(q.poll()).append(", ");
-        }
-        sb.append(q.poll());
-        sb.append(">");
 
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =1 ; i<=N; i++) q.offer(i);
+
+        while(!q.isEmpty()) {
+            for(int i=0; i<K-1; i++) q.offer(q.poll());
+            sb.append(q.poll());
+            if(!q.isEmpty()) sb.append(", ");
+        }
+
+        sb.append(">");
+        System.out.println(sb);
     }
 }
+
