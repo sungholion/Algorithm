850c9292516051bd7eb8cacb7373d660d0e1ecbc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-12 16:56:10 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -2,66 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-   public static boolean[][] board;
-   public static int min = 64;
+    static char[][] white = {
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'}
+    };
 
-    public static void main(String[] args)throws IOException {
+    static char[][] black = {
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'},
+            {'B','W','B','W','B','W','B','W'},
+            {'W','B','W','B','W','B','W','B'}
+    };
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        board = new boolean[N][M];
 
-        for(int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for(int j = 0; j < M; j++) {
-                if(str.charAt(j) == 'W')
-                    board[i][j] = true;
-                else
-                    board[i][j] = false;
+        char[][] map = new char[N][M];
+        for (int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int sr = 0; sr <= N - 8; sr++) {
+            for (int sc = 0; sc <= M - 8; sc++) {
+                int cntW = 0; 
+                int cntB = 0; 
+
+                for (int r = 0; r < 8; r++) {
+                    for (int c = 0; c < 8; c++) {
+                        if (map[sr + r][sc + c] != white[r][c]) cntW++;
+                        if (map[sr + r][sc + c] != black[r][c]) cntB++;
+                    }
+                }
+
+                ans = Math.min(ans, Math.min(cntW, cntB));
             }
         }
 
-        int row = N - 7;
-        int col = M - 7;
-
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                chess(i, j);
-            }
-        }
-        sb.append(min).append("\n");
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
-
-    public static void chess(int x, int y) {
-        int end_x = x + 8;
-        int end_y = y + 8;
-        int cnt = 0;
-
-        boolean tf = board[x][y];    // 첫 칸 색깔
-
-        for(int i = x; i < end_x; i++) {
-            for(int j = y; j < end_y; j++) {
-
-                if(board[i][j] != tf)
-                    cnt++;
-
-                tf = (!tf);
-            }
-
-            tf = (!tf);
-        }
-
-        cnt = Math.min(cnt, 64 - cnt);  // 시작이 B일 때와, W일 때 비교
-
-        min = Math.min(min, cnt);
+        System.out.println(ans);
     }
 }
