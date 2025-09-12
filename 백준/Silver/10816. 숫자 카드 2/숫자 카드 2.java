368f3958294cefb5f2c780e8061ec9f09cf3ebed
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-12 15:29:43 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1040 ms, Memory: 180080 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,58 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] arr;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-
-        Arrays.sort(arr); // 이분 탐색을 위해 정렬
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr1 = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++){
+            arr1[i] = Integer.parseInt(st.nextToken());
+            map.put(arr1[i], map.getOrDefault(arr1[i], 0) + 1);
+        }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++){
-            int key = Integer.parseInt(st.nextToken());
-            int count = upperBound(key) - lowerBound(key);
-            sb.append(count).append(" ");
+        for (int i = 0; i < M; i++){
+            int temp = Integer.parseInt(st.nextToken());
+            if(map.get(temp) == null) sb.append(0).append(" ");
+            else sb.append(map.get(temp)).append(" ");
+
         }
-
-        System.out.println(sb.toString().trim());
-    }
-
-    public static int lowerBound(int key) {
-        int low = 0;
-        int high = arr.length;
-
-        while(low < high){
-            int mid = (low + high) / 2;
-            if(arr[mid] < key){
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
-        }
-        return low;
-    }
-
-    public static int upperBound(int key) {
-        int low = 0;
-        int high = arr.length;
-
-        while(low < high){
-            int mid = (low + high) / 2;
-            if(arr[mid] <= key){
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
-        }
-        return low;
+        System.out.println(sb);
     }
 }
