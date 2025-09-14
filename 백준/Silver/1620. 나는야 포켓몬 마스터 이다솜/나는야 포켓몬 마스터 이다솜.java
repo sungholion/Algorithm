b4c67c9f4d6079e4d155d32995bcecb9aa7fe781
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-14 10:57:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 444 ms, Memory: 49872 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,42 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> map = new HashMap<>();
-        Map<Integer, String> nextmap = new HashMap<>();
+        String[] numToName = new String[N + 1];
+        Map<String, Integer> nameToNum = new HashMap<>(); 
 
         for (int i = 1; i <= N; i++) {
             String name = br.readLine();
-            map.put(name, i);
-            nextmap.put(i, name);
+            numToName[i] = name;
+            nameToNum.put(name, i);
         }
 
-        for(int i = 1; i <= M; i++) {
-            String x = br.readLine();
-            char ch = x.charAt(0);
-            if(ch >= '0' && ch <= '9') {
-               sb.append(nextmap.get(Integer.parseInt(x))).append("\n");
-            } else {
-                sb.append(map.get(x)).append("\n");
+        for (int i = 0; i < M; i++) {
+            String query = br.readLine();
+
+            if (Character.isDigit(query.charAt(0))) { 
+                int num = Integer.parseInt(query);
+                sb.append(numToName[num]).append("\n");
+            } else { 
+                sb.append(nameToNum.get(query)).append("\n");
             }
         }
 
-
-
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
