0477f721a3d308668fbbc615f19a49ad15a91171
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-16 15:19:27 +0900

    [Bronze III] Title: ACM 호텔, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -7,20 +7,30 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
+
+        while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int H = Integer.parseInt(st.nextToken());
             int W = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
 
-            int Y = N % H;
-            if(Y == 0) Y = H;
+            int remain = N % H;
+            int share = N / H;
 
-            int X = N / H;
-            if (N % H != 0) X++;
+            if(remain == 0){
+                sb.append(H);
+            } else {
+                share++;
+                sb.append(remain);
+            }
 
-            sb.append(Y * 100 + X).append("\n");
+            if(share < 10) sb.append("0");
+            sb.append(share);
+            sb.append("\n");
         }
+
         System.out.println(sb);
+
     }
+
 }
