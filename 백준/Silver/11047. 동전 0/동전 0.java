04484aa353283b86f712717d706540b62d6cd715
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-16 15:05:50 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,38 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
 
-        int[] coin = new int[N];
-        for (int i = 0; i < N; i++)
-            coin[i] = Integer.parseInt(br.readLine());
+        long[] a = new long[N];
+        for (int i = 0; i < N; i++) a[i] = Long.parseLong(br.readLine());
 
-        int cnt = 0;
-        while(K != 0){
-            for(int i = N-1; i >= 0; i--){
-                int money = coin[i];
-
-                if(money <= K){
-                    cnt += K / money;
-                    K %= money;
-                }
+        long ans = 0;
+        for (int i = N - 1; i >= 0 && K > 0; i--) {
+            if (a[i] <= K) {
+                ans += K / a[i];
+                K %= a[i];
             }
         }
-
-        sb.append(cnt);
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(ans);
     }
 }
