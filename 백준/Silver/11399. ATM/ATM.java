c8eb678a39db852f4e1c239b6d960c23d37b24dc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-17 11:03:00 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,33 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine().trim());
 
-        int N = Integer.parseInt(br.readLine());
-        int[] time = new int[N];
+        int[] P = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            time[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(time);
+        for (int i = 0; i < N; i++) P[i] = Integer.parseInt(st.nextToken());
 
-        int before = 0;
-        int sum = 0;
-        for(int i = 0; i < N; i++) {
-            sum += time[i] + before;
-            before += time[i];
+        Arrays.sort(P); 
+        long total = 0;   
+        long prefix = 0; 
+        for (int x : P) {
+            prefix += x;   
+            total  += prefix;
         }
 
-        sb.append(sum).append("\n");
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(total);
     }
 }
