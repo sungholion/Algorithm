db358764f47197205b6003d65c22621416574bfc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-19 10:51:03 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,43 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-        static int[][] dp = new int[41][2];
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        dp[0][0] = 1;
-        dp[0][1] = 0;
-        dp[1][0] = 0;
-        dp[1][1] = 1;
+        int T = Integer.parseInt(br.readLine().trim());
 
-        for(int i = 2; i < 41; i++) {
-            for(int j = 0; j < 2; j++)
-                dp[i][j] = -1;
+        int MAX = 40;
+        int[] zero = new int[MAX + 1];
+        int[] one  = new int[MAX + 1];
+
+        zero[0] = 1; one[0] = 0;
+        zero[1] = 0; one[1] = 1;
+
+        for (int i = 2; i <= MAX; i++) {
+            zero[i] = zero[i - 1] + zero[i - 2];
+            one[i]  = one[i - 1]  + one[i - 2];
         }
 
-        int T = Integer.parseInt(br.readLine());
-        while(T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            fibonnaci(N);
-            sb.append(dp[N][0]).append(" ").append(dp[N][1]).append("\n");
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine().trim());
+            sb.append(zero[n]).append(' ').append(one[n]).append('\n');
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb.toString());
     }
-
-    static int[] fibonnaci(int N) {
-        if(dp[N][0] == -1 || dp[N][1] == -1) {
-            dp[N][0] = fibonnaci(N-1)[0] + fibonnaci(N-2)[0];
-            dp[N][1] = fibonnaci(N-1)[1] + fibonnaci(N-2)[1];
-        }
-        return dp[N];
-    }
-
 }
