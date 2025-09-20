529ab9c7b4fe475000aae93bab391e29b8c91316
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-20 14:08:06 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,48 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static int cnt;
-    static int[][] arr;
-    static boolean[] vis;
-    static int n, m;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine().trim());     
+        int m = Integer.parseInt(br.readLine().trim());     
 
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+        List<Integer>[] g = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) g[i] = new ArrayList<>();
 
-        arr = new int[n+1][n+1];
-        vis = new boolean[n+1];
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int v1 = Integer.parseInt(st.nextToken());
-            int v2 = Integer.parseInt(st.nextToken());
-            arr[v1][v2] = arr[v2][v1] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            g[a].add(b);
+            g[b].add(a);
         }
 
-        bfs(1);
-        System.out.println(cnt - 1);
-    }
+        boolean[] vis = new boolean[n + 1];
+        Queue<Integer> q = new ArrayDeque<>();
+        vis[1] = true;
+        q.offer(1);
 
-    public static void bfs(int node){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        vis[node] = true;
-        cnt++;
-
-        while(!q.isEmpty()){
+        int infected = 0;
+        while (!q.isEmpty()) {
             int cur = q.poll();
-
-            for(int i=1; i<=n; i++){
-                if(arr[cur][i] == 1 && !vis[i]){
-                    vis[i] = true;
-                    q.offer(i);
-                    cnt++;
+            for (int nxt : g[cur]) {
+                if (!vis[nxt]) {
+                    vis[nxt] = true;
+                    q.offer(nxt);
+                    infected++;
                 }
             }
         }
+        System.out.println(infected);
     }
 }
