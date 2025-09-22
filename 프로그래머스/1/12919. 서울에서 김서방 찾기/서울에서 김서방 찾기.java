6550a0cbb5b1e8a6ce35ca0302af1b099afa8c9d
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-22 11:47:45 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 14.47 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "";
-        answer += "김서방은 ";
-        
+        String answer = "김서방은 ";
         for(int i=0; i<seoul.length; i++){
-            if(seoul[i].equals("Kim")){
-                answer += i;
-            }
+            String temp = seoul[i];
+            if(temp.equals("Kim")) answer += i;
         }
         answer += "에 있다";
         return answer;
