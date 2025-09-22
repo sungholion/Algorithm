f32a2a1a1a5534997baad448ea04a812d9916d42
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-22 11:18:59 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.11 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,12 +1,12 @@
 class Solution {
     public long solution(long n) {
-        long answer = -1;
-        long temp = (long) Math.sqrt(n);
+        // x = 제곱근(sqrt)로 구한 값을 int로 바꾸고
+        // n을 x로 나눴을 때 x와 같은지 비교
+        long answer = 0;
         
-        if(Math.pow(temp,2) == n ){
-            answer = (long)Math.pow(temp +1,2);
-        }
-
+        long x = (long)Math.sqrt(n);
+        if((x * x) == n) answer = (long)Math.pow((x+1), 2);
+        else answer = -1;
         
         return answer;
     }
