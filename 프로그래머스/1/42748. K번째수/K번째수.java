9ec5dfae2cf4e2b1e8aa154ea402632cb6f2f762
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-22 18:20:01 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,27 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        answer = new int[commands.length];
+        int[] answer = new int[commands.length];
         
-        int cnt = 0;
-        for(int i = 0; i<commands.length; i++){
-            int start = commands[i][0] - 1;
-            int end = commands[i][1];
-            int target = commands[i][2];
-            
-            int len = end - start;
-            int[] newArr = new int[len];
-            int idx = 0;
-            
-            for(int k = 0; k < len; k++){
-                newArr[k] = array[start + k];  // array에서 올바른 값을 복사
+        for(int idx=0; idx<answer.length; idx++){
+            int i = commands[idx][0];
+            int j = commands[idx][1];
+            int k = commands[idx][2];
+            int[] temp = new int[j-i+1];
+            for(int t = 0; t<temp.length; t++){
+                temp[t] = array[i-1+t];
             }
-            Arrays.sort(newArr);
-            
-            answer[cnt++] = newArr[target-1];
-            
-            
+            Arrays.sort(temp);
+            answer[idx] = temp[k-1];
         }
         
         return answer;
