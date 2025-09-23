922d2b8450ad7c52742629b4ec018eedf9b28215
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-23 11:28:28 +0900

    [level 2] Title: 올바른 괄호, Time: 19.88 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,11 +1,9 @@
 import java.util.*;
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        
+
         Stack<Character> stack = new Stack<>();
-        
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             
@@ -13,18 +11,16 @@ class Solution {
                 stack.push(c);
             } else if(c == ')'){
                 if(!stack.isEmpty()){
-                    stack.pop();    
-                } else{
+                    stack.pop();
+                } else {
                     answer = false;
                     break;
                 }
             }
         }
         
-        if(!stack.isEmpty()){
-            answer = false;
-        }
-  
+         if(!stack.isEmpty()) answer = false;
+
         return answer;
     }
 }
