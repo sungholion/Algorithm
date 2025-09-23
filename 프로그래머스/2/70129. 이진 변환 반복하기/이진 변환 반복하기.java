b0497cd6d5cd79c57dc7beb0550b3df3791cde4a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-23 12:31:53 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.05 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,31 +1,21 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
-        answer = new int[2];
+        int cnt = 0;
+        int zero = 0;
         
-        int cntConvert = 0;
-        int cntZero = 0;
-        while(!s.equals("1")){
-            cntConvert++;
-            String temp = "";
-            for(int i=0; i<s.length(); i++){    // 0 제거
-                if(s.charAt(i) == '0'){
-                    cntZero++;
-                } else{
-                    temp += s.charAt(i);
-                }
+        while(true){
+            if(s.equals("1")) break;
+            
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<s.length(); i++){
+                char ch = s.charAt(i);
+                if(ch == '0') zero++;
+                else sb.append(ch);
             }
-            s = temp;
-            
-            int c = s.length();
-            
+            int c = sb.length();
             s = Integer.toBinaryString(c);
-        
+            cnt++;
         }
-        
-        answer[0] = cntConvert;
-        answer[1] = cntZero;
-        
-        return answer;
+        return new int[]{cnt, zero};
     }
 }
