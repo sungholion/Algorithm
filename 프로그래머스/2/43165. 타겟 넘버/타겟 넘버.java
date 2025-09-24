dc1a15900bed7470eb2810c446686e6658fb89e7
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-24 21:20:25 +0900

    [level 2] Title: 타겟 넘버, Time: 4.94 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,16 @@
 class Solution {
-    int cnt = 0;
+    static int answer;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
         dfs(numbers, target, 0, 0);
-        answer = cnt;
         return answer;
     }
-    
-    public void dfs(int[] numbers, int target, int depth, int sum){
+    static void dfs(int[] numbers, int target, int depth, int sum){
         if(depth == numbers.length){
-            if(sum == target)
-                cnt++;
-            return;
+            if(sum == target) answer++;
+            return;   
         }
-        dfs(numbers, target, depth+1, sum + numbers[depth]);
-        dfs(numbers, target, depth+1, sum - numbers[depth]);
+        dfs(numbers, target, depth+1, sum+numbers[depth]);
+        dfs(numbers, target, depth+1, sum-numbers[depth]);
     }
     
-
 }
