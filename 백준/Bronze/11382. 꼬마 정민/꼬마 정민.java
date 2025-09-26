fa7eb84e2ad1201a4a9b86e8ebb4718d9f4dcb6e
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-09-27 08:53:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,24 +1,14 @@
 import java.io.*;
-import java.time.LocalDate;
 import java.util.*;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long A = Long.parseLong(st.nextToken());
         long B = Long.parseLong(st.nextToken());
         long C = Long.parseLong(st.nextToken());
-     
-        sb.append(A + B + C);
 
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(A + B + C);
     }
 }
