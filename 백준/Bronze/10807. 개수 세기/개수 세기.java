15000c1fdae36ebd825a017310cb3b140da978e5
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 18:21:56 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -5,17 +5,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int[] arr = new int[201];
         int res = 0;
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            arr[x+100]++;
         }
         int v = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N; i++) {
-            if(arr[i] == v) res++;
-        }
-        System.out.println(res);
+        System.out.println(arr[v+100]);
     }
 }
