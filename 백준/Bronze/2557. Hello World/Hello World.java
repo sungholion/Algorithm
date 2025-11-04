8735800674fa5258aa4c26c8156bf497c44b8fde
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 13:52:55 +0900

    [Bronze V] Title: Hello World, Time: 92 ms, Memory: 13868 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,8 +1,5 @@
-import java.io.*;
-import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-      System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
