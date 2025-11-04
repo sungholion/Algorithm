ef22dbb6b9613e0b69bcd856c457382865fdadda
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 18:45:30 +0900

    [Bronze V] Title: 이상한 기호, Time: 100 ms, Memory: 14140 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,22 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long A = Long.parseLong(st.nextToken());
         long B = Long.parseLong(st.nextToken());
-        long res = (A + B) * (A - B);
-        sb.append(res);
-        
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+
+        System.out.println(cal(A, B));
+    }
+    public static long cal(long a, long b){
+        return (a+b) * (a-b);
     }
 }
