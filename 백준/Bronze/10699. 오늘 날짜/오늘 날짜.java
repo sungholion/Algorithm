0a7794a21fb439459cf2f13e86949736c4c5d221
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 13:55:42 +0900

    [Bronze V] Title: 오늘 날짜, Time: 156 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -1,20 +1,9 @@
 import java.io.*;
 import java.time.LocalDate;
-import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        LocalDate now = LocalDate.now();
-        sb.append(now);
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
     }
 }
