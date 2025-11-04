83c85ac1c15ae22f789354449483dd56b26f6a06
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 14:09:28 +0900

    [Bronze V] Title: A/B, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,15 +1,13 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         double A = Double.parseDouble(st.nextToken());
         double B = Double.parseDouble(st.nextToken());
 
-        bw.write(String.valueOf(A/B));
-        bw.flush();
+        System.out.println(A / B);
     }
 }
