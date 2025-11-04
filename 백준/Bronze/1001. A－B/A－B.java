d73c3059d3fd0634670a947f6fbd8e0db4502fe8
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 14:00:37 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,15 +1,13 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-
-        bw.write(String.valueOf(A-B));
-        bw.flush();
+        
+        System.out.println(A - B);
     }
 }
