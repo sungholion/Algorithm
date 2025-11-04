a184f60c0806e99af01c3b75ba169a7cff29ab81
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 13:56:46 +0900

    [Bronze V] Title: 등록, Time: 100 ms, Memory: 13844 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -1,18 +1,8 @@
 import java.io.*;
-import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(36).append("\n").append("onda2098");
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(2);
+        System.out.println("eoblue26");
     }
 }
