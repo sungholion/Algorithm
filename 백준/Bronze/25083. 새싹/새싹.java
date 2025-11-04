5bfbb7c215f18425ea82d8096484d2ca64a291af
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 13:59:09 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13936 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,19 +1,12 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String s = "         ,r'\"7\n" +
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7\n" +
                 "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +	
-                "   `~\\/\n" +		
-                "      |\n" +		
-                "      |";
-
-        bw.write(s);
-        bw.flush();
-        bw.close();
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
     }
 }
