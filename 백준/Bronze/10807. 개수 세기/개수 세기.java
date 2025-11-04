30174a63d533502eae1b7c1c2043302be6c0d46a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 18:07:13 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,35 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int cnt = 0;
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
+        int res = 0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         int v = Integer.parseInt(br.readLine());
-
-        for(int i=0; i<N; i++){
-            if(arr[i] == v){
-                cnt++;
-            }
+        for(int i = 0; i < N; i++) {
+            if(arr[i] == v) res++;
         }
-
-        sb.append(cnt);
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(res);
     }
 }
