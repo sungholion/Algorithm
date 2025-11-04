82219939a173871a812e1a89dff40ef280768e8f
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 14:18:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         long A = Long.parseLong(st.nextToken());
