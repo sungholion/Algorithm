e4237284bd6f4e9be3981179d0825cbff2526148
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 14:10:20 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,18 +1,17 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-
+        
         System.out.println(A + B);
         System.out.println(A - B);
         System.out.println(A * B);
-        System.out.println(A / B); // 정수 나눗셈
-        System.out.println(A % B); // 나머지
+        System.out.println(A / B);
+        System.out.println(A % B);
     }
 }
