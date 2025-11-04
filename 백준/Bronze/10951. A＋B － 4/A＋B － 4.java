3fb24e43d139acb717029b4eff8609715cc08944
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-04 14:38:09 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -2,18 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringTokenizer st;
         String str;
         while((str = br.readLine()) != null) {
-            StringTokenizer st = new StringTokenizer(str);
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            bw.write(A+B + "\n");
+            st = new StringTokenizer(str);
+            int A  = Integer.parseInt(st.nextToken());
+            int B  = Integer.parseInt(st.nextToken());
+            System.out.println(A + B);
         }
-
-        bw.flush();
     }
 }
