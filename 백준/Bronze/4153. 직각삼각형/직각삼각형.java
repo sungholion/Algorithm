1055f990b2f7170a287c4dbbe9672e3ab589e0bb
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-05 16:53:57 +0900

    [Bronze III] Title: 직각삼각형, Time: 96 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,13 +1,9 @@
-import com.sun.source.tree.UsesTree;
-
 import java.io.*;
 import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         while(true){
@@ -15,34 +11,13 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0 && c == 0) break;
 
-            if(a == 0 && b == 0 && c == 0)
-                break;
-
-            if(a > b && a > c){
-                if((a*a) == (b*b + c*c)){
-                    sb.append("right\n");
-                } else{
-                    sb.append("wrong\n");
-                }
-            } else if(b > a && b > c){
-                if((b*b) == (a*a + c*c)){
-                    sb.append("right\n");
-                } else{
-                    sb.append("wrong\n");
-                }
-            } else if(c > a && c > b){
-                if((c*c) == (a*a + b*b)){
-                    sb.append("right\n");
-                } else{
-                    sb.append("wrong\n");
-                }
-            }
+            int[] arr = {a, b, c};
+            Arrays.sort(arr);
+            if((arr[2] * arr[2]) == (arr[0] * arr[0]) + (arr[1] * arr[1])) sb.append("right\n");
+            else sb.append("wrong\n");
         }
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
     }
 }
