df235252bb71739ddbff7b2dee96fe919f988284
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-05 13:54:10 +0900

    [Bronze II] Title: 음계, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -2,40 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int[] music = new int[8];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < music.length; i++) {
-            music[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] music = new int[8];
+        for(int i = 0; i < 8; i++) music[i] = Integer.parseInt(st.nextToken());
 
         if(music[0] == 1){
             boolean isAsc = true;
-            for(int i = 1; i < music.length; i++) {
-                if(music[i] != music[i-1] + 1){
-                    isAsc = false;
-                    break;
-                }
+            for(int i = 1; i < 8; i++){
+                if(music[i] <= music[i-1]) isAsc = false;
             }
-            if(isAsc) sb.append("ascending");
-            else sb.append("mixed");
+            if(isAsc) System.out.println("ascending");
+            else System.out.println("mixed");
         } else if(music[0] == 8){
             boolean isDesc = true;
-            for(int i = 1; i < music.length; i++) {
-                if(music[i] != music[i-1] - 1){
-                    isDesc = false;
-                    break;
-                }
+            for(int i = 1; i < 8; i++){
+                if(music[i] >= music[i-1]) isDesc = false;
             }
-            if(isDesc) sb.append("descending");
-            else sb.append("mixed");
-        } else{
-            sb.append("mixed");
+            if(isDesc) System.out.println("descending");
+            else System.out.println("mixed");
+        } else {
+            System.out.println("mixed");
         }
-
-        System.out.println(sb.toString());
     }
 }
