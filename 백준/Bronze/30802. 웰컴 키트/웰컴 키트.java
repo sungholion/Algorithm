a48e8c61f8824b18077cc57134d19bed4a0e902a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-05 17:05:23 +0900

    [Bronze III] Title: 웰컴 키트, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,42 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());    // 총 사람 수
-
-        int[] tSize = new int[6];
-        st = new StringTokenizer(br.readLine());    // 티셔츠 사이즈별 수요
-        for(int i=0; i<tSize.length; i++){
-            tSize[i] = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] t = new int[6];
+        for(int i = 0; i < 6; i++) {
+            t[i] = Integer.parseInt(st.nextToken());
         }
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int needT = 0;
+        int needP = 0;
+        int needPremain = 0;
 
-        st = new StringTokenizer(br.readLine());    // 한번에 구매하는 묶음
-        int T = Integer.parseInt(st.nextToken());   // 티셔츠
-        int P = Integer.parseInt(st.nextToken());   // 펜
-
-        int T_sum = 0;
-        int P_sum = 0;
-        int P_remain = 0;
-        for(int i = 0; i<tSize.length; i++){
-            T_sum += tSize[i] / T;
-            if(tSize[i] % T != 0)
-                T_sum++;
+        for(int i = 0; i < t.length; i++) {
+            needT += t[i] / T;
+            if(t[i] % T != 0) needT++;
         }
+        needP += N / P;
+        needPremain += N % P;
 
-        P_sum = N / P;
-        P_remain = N % P;
-
-        sb.append(T_sum).append("\n").append(P_sum).append(" ").append(P_remain);
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        sb.append(needT).append("\n").append(needP).append(" ").append(needPremain).append("\n");
+        System.out.println(sb.toString());
     }
 }
