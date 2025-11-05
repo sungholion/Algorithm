fea329e3e6c0069633b03b9b8f386e8f5770f144
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-05 19:14:39 +0900

    [Bronze I] Title: 팰린드롬수, Time: 92 ms, Memory: 13972 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
@@ -10,15 +10,14 @@ public class Main {
         while(!(str = br.readLine()).equals("0")){
             boolean isPal = true;
             for(int i = 0; i < str.length() / 2; i++){
-                if(str.charAt(i) != str.charAt(str.length() -1 - i)){
+                if(str.charAt(i) != str.charAt(str.length() - i - 1)){
                     isPal = false;
                     break;
                 }
             }
-            if(isPal)sb.append("yes\n");
+            if(isPal) sb.append("yes\n");
             else sb.append("no\n");
         }
         System.out.println(sb.toString());
-
     }
 }
