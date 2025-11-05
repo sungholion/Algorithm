2e251fa432e0619cc38fc97c620fbe954a81b924
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-11-05 19:35:05 +0900

    [Bronze I] Title: FizzBuzz, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.java
@@ -2,32 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        int x = 0;
         String str1 = br.readLine();
         String str2 = br.readLine();
         String str3 = br.readLine();
-
-        int temp = 0;
-        if(Character.isDigit(str1.charAt(0))){
-            temp = Integer.parseInt(str1) + 3;
-        } else if(Character.isDigit(str2.charAt(0))){
-            temp = Integer.parseInt(str2) + 2;
-        } else if(Character.isDigit(str3.charAt(0))){
-            temp = Integer.parseInt(str3) + 1;
+        if(str1.charAt(0) != 'F' && str1.charAt(0) != 'B'){
+            x = Integer.parseInt(str1) + 3;
+        } else if(str2.charAt(0) != 'F' && str2.charAt(0) != 'B'){
+            x = Integer.parseInt(str2) + 2;
+        } else if(str3.charAt(0) != 'F' && str3.charAt(0) != 'B'){
+            x = Integer.parseInt(str3) + 1;
         }
 
-        if((temp % 3 == 0) && (temp % 5 == 0)){
-            System.out.println("FizzBuzz");
-        } else if((temp % 3 == 0) && (temp % 5 != 0)){
-            System.out.println("Fizz");
-        } else if((temp % 3 != 0) && (temp % 5 == 0)){
-            System.out.println("Buzz");
-        } else{
-            System.out.println(temp);
-        }
+        if((x % 3 == 0) && (x % 5 == 0)) sb.append("FizzBuzz");
+        else if((x % 3 == 0) && (x % 5 != 0)) sb.append("Fizz");
+        else if((x % 3 != 0) && (x % 5 == 0)) sb.append("Buzz");
+        else sb.append(x);
+
+        System.out.println(sb);
 
     }
 }
