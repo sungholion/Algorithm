8be918b6a9f4bfcf31c3accf135644227b6a0c3a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-24 17:18:06 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -2,20 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+        StringBuffer newA = new StringBuffer(A);
+        A = newA.reverse().toString();
+        StringBuffer newB = new StringBuffer(B);
+        B = newB.reverse().toString();
 
-        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int numA = Integer.parseInt(A);
+        int numB = Integer.parseInt(B);
 
-        sb.append(String.valueOf(Math.max(A, B)));
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        if(numA >= numB) System.out.println(numA);
+        else System.out.println(numB);
     }
 }
