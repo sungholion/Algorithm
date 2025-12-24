48d6b6731d513168c43edbb9eea65dd69a224529
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-24 14:34:04 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -2,20 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < T; i++) {
+        for(int t = 1; t <= T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            bw.write(A+B + "\n");
+
+            sb.append(A+B).append("\n");
         }
-
-
-        bw.flush();
+        System.out.print(sb.toString());
     }
 }
