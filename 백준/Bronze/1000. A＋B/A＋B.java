4b91f4b1e1864e042639812581fff99bd3a7ce8d
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-24 13:27:08 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,13 +1,16 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
-        
-        System.out.println(A + B);
+       
+        sb.append(A+B);
+        System.out.println(sb.toString());
+
     }
 }
