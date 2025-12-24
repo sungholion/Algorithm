ddd1592daba7c0db0b4f1ea37bafedafb61fa6bc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-24 15:32:46 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -4,17 +4,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[10];
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[9];
         int max = Integer.MIN_VALUE;
-        int idx = -1;
+        int maxIdx = -1;
         for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             if(arr[i] > max) {
                 max = arr[i];
-                idx = i + 1;
+                maxIdx = i;
             }
         }
-        System.out.println(max);
-        System.out.println(idx);
+        sb.append(max).append("\n").append(maxIdx+1);
+        System.out.println(sb.toString());
     }
 }
