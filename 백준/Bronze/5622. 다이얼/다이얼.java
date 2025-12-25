0fc797d0c7154d7c4e3e254806fbc0b5a22592a2
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-25 11:35:15 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -7,34 +7,17 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
+        String[] dial = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
         int total = 0;
-        for(int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            int num = c - 'A';
-            int time = 0;
-            if(num < 15){
-                time = num / 3 + 3; // 걸리는 시간
-            } else if (num >= 15 && num <= 18){
-                time = 8;
-            } else if (num > 18 && num <= 21){
-                time = 9;
-            } else {
-                time = 10;
+        for(char c : str.toCharArray()) {
+            for(int i = 0; i < dial.length; i++) {
+                if(dial[i].indexOf(c) != -1){
+                    total += i + 3;
+                    break;
+                }
             }
-            total += time;
-
         }
         System.out.println(total);
 
-
-            /* 3으로 나눈 몫에다가 3을 더하면 됌.
-            ABC 012 -> 3으로 나눴을 때 몫 0   : 걸리는 시간 3
-            DEF 345 -> 3으로 나눴을 때 몫 1   : 걸리는 시간 4
-            GHI 678 -> "" 2
-            JKL 91011 -> '' 3
-            MNO 121314 -> "" 4
-            PQRS 15161718
-            TUV 192021
-            */
     }
 }
