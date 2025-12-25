ec1d53ea6c2c1bd267644030cab2ccfe514ea5ac
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-25 11:29:58 +0900

    [Bronze II] Title: 다이얼, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -2,30 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
-        int time = 0;
-        for(int i = 0; i < str.length(); i++){
+        int total = 0;
+        for(int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (c >= 'A' && c <= 'C') time += 3;
-            else if (c >= 'D' && c <= 'F') time += 4;
-            else if (c >= 'G' && c <= 'I') time += 5;
-            else if (c >= 'J' && c <= 'L') time += 6;
-            else if (c >= 'M' && c <= 'O') time += 7;
-            else if (c >= 'P' && c <= 'S') time += 8;
-            else if (c >= 'T' && c <= 'V') time += 9;
-            else if (c >= 'W' && c <= 'Z') time += 10;
+            int num = c - 'A';
+            int time = 0;
+            if(num < 15){
+                time = num / 3 + 3; // 걸리는 시간
+            } else if (num >= 15 && num <= 18){
+                time = 8;
+            } else if (num > 18 && num <= 21){
+                time = 9;
+            } else {
+                time = 10;
+            }
+            total += time;
+
         }
+        System.out.println(total);
 
-        sb.append(time);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
 
+            /* 3으로 나눈 몫에다가 3을 더하면 됌.
+            ABC 012 -> 3으로 나눴을 때 몫 0   : 걸리는 시간 3
+            DEF 345 -> 3으로 나눴을 때 몫 1   : 걸리는 시간 4
+            GHI 678 -> "" 2
+            JKL 91011 -> '' 3
+            MNO 121314 -> "" 4
+            PQRS 15161718
+            TUV 192021
+            */
     }
 }
