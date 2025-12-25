986b9a5a8e4fc19537b20553876e3cd2b6b15314
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-25 23:12:51 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,21 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        int[] ans = new int []{1, 1, 2, 2, 2, 8};
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 6; i++)
-            sb.append(ans[i] - Integer.parseInt(st.nextToken())).append(' ');
+        int k = Integer.parseInt(st.nextToken());
+        sb.append(1 - k).append(" ");
+        int q = Integer.parseInt(st.nextToken());
+        sb.append(1- q).append(" ");
+        int l = Integer.parseInt(st.nextToken());
+        sb.append(2- l).append(" ");
+        int b = Integer.parseInt(st.nextToken());
+        sb.append(2- b).append(" ");
+        int n = Integer.parseInt(st.nextToken());
+        sb.append(2- n).append(" ");
+        int p = Integer.parseInt(st.nextToken());
+        sb.append(8- p).append(" ");
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb.toString());
     }
 }
