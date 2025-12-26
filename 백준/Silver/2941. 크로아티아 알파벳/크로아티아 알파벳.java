bf5f551661648dfb3fdded36874824d0bb028611
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 12:59:51 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,44 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
         String str = br.readLine();
+        str = str.replace("c=", "*");
+        str = str.replace("c-", "*");
+        str = str.replace("dz=", "*");
+        str = str.replace("d-", "*");
+        str = str.replace("lj", "*");
+        str = str.replace("nj", "*");
+        str = str.replace("s=", "*");
+        str = str.replace("z=", "*");
 
-        int cnt = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (ch == 'c' && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
-                    i++;
-                }
-            } else if (ch == 'd' && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == '-') {
-                    i++;
-                } else if (str.charAt(i + 1) == 'z' && i + 2 < str.length() && str.charAt(i + 2) == '=') {
-                    i += 2;
-                }
-            } else if ((ch == 'l' || ch == 'n') && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == 'j') {
-                    i++;
-                }
-            } else if ((ch == 's' || ch == 'z') && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == '=') {
-                    i++;
-                }
-            }
-            cnt++;
-        }
-
-        sb.append(cnt);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(str.length());
     }
 }
