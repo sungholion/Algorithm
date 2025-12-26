fe292ad200525ca05f2bebe771201f6e15832733
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 17:36:34 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        long N = Long.parseLong(br.readLine());
+        sb.append(N * 4);
+        System.out.print(sb);
+    }
+
+}
