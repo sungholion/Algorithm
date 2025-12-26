5141cc19b53b144feaf26fbc375a631fca661430
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 16:31:14 +0900

    [Bronze III] Title: 배수와 약수, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,29 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		while(true) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			if(a == 0 && b == 0) break;
-			
-			if(a % b == 0) bw.write("multiple\n");
-			else if( b % a == 0) bw.write("factor\n");
-			else bw.write("neither\n");
-		}
-		
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            if(num1 == 0 && num2 == 0) break;
+
+            if(num1 > num2 && num1 % num2 == 0) sb.append("multiple").append("\n");
+            else if(num2 > num1 && num2 % num1 == 0) sb.append("factor").append("\n");
+            else sb.append("neither").append("\n");
+
+        }
+        System.out.print(sb.toString());
+    }
 }
