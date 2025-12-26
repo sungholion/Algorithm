d317acc4ddc57dcd61effaa96517306c0fea0c77
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 14:15:23 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,35 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        boolean[][] arr = new boolean[101][101];
-        int sum = 0;
-        while (N-- > 0) {
+        int T = Integer.parseInt(br.readLine());
+        int[][] ans = new int[101][101];
+        for(int t = 0; t < T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-
-            for(int i=x; i<x+10; i++){
-                for(int j=y; j<y+10; j++){
-                    if(!arr[i][j]){
-                        arr[i][j] = true;
-                        sum++;
-                    }
+            for(int i = x; i < x + 10; i++){
+                for(int j = y; j < y + 10; j++){
+                    ans[i][j] = 1;
                 }
             }
         }
 
-        sb.append(sum);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        int res = 0;
+        for(int i = 0; i < 101; i++){
+            for(int j = 0; j < 101; j++){
+                if(ans[i][j] == 1) res++;
+            }
+        }
+        System.out.println(res);
     }
 }
