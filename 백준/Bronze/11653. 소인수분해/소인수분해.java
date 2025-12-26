706b346567bd3ba2ed36f490a09733cd64620612
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 17:00:09 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,23 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
- 
-		for (int i = 2; i <= Math.sqrt(N); i++) {
-			while (N % i == 0) {
-				sb.append(i).append('\n');
-				N /= i;
-			}
-		}
-		if (N != 1) {
-			sb.append(N);
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        if(N == 1) return;
+
+        for(int i = 2; i <= Math.sqrt(N); i++) {
+            while(N % i == 0){
+                sb.append(i).append("\n");
+                N /= i;
+            }
+        }
+        
+        if(N != 1) sb.append(N).append("\n");
+
+        System.out.print(sb.toString());
+    }
+
 }
