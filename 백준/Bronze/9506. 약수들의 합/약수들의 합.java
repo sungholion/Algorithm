e95c964fe8db547548a3fd92e1852aae237880f5
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 16:44:33 +0900

    [Bronze I] Title: 약수들의 합, Time: 104 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,51 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        while(true) {
+        while(true){
+            sb = new StringBuilder();
             int n = Integer.parseInt(br.readLine());
+            if(n == -1) break;
 
-            if (n == -1) {
-                break;
-            }
-
-            int[] array = new int[n];
-
-            int sum = 0;
-
-            for (int i = 1; i <= n; i++) {
-                if (n % i == 0 && i != n) {
-                    array[i] = i;
-                    sum += i;
+            if(isPerfect(n)){
+                String temp = sb.toString();
+                for(int i = 0; i < temp.length()-3; i++){
+                    System.out.print(temp.charAt(i));
                 }
-                //차피 아니면 0이 들어감(약수는 0이 나올 수 X)
+                System.out.println();
+            } else {
+                sb = new StringBuilder();
+                sb.append(n).append(" is NOT perfect.\n");
+                System.out.print(sb);
             }
-
-            //완전수가 아닐 경우
-            if (sum != n) { //다른 것과 구분하려면 앞으로 빼는 것이 나음!
-                sb.append(n+ " is NOT perfect. \n");
-                continue; //완전수가 아니기때문에 밑의 코드는 수행 X
-            }
-
-            sb.append(n + " = 1"); //1의 약수는 차피 무조건 추가  이기 때문에
-
-            for(int i = 1; i <= n; i++){ //약수의 최대 몇개 인지 알고 있기 때문에 index만큼 반복문
-                if(array[i-1] != 0 && array[i-1] != 1){ //숫자 출력 부분 1출력은 생략한다.
-                    sb.append(" + " + array[i-1]);
-                }
-
-            }
-            sb.append("\n");
-
         }
-        br.close();
-        System.out.println(sb);
     }
 
+    static boolean isPerfect(int n){
+        int sum = 0;
+        sb.append(n).append(" = ");
+        for(int i = 1; i < n; i++){
+            if(n % i == 0){
+                sum += i;
+                sb.append(i).append(" + ");
+            }
+        }
+        if(sum == n) return true;
+        return false;
+    }
 }
