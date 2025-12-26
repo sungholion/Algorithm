3f6b293f3454af32cafb4be02f88bd7c21f55951
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-26 16:53:32 +0900

    [Bronze II] Title: 소수, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,41 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		int min = N;
-		int sum = 0;
-		
-		for(int i = M; i <= N; i++) {
-			//소수인지 판별할 변수
-			boolean check = true;
-			//1이 올 수 있지만 1은 소수가 아니기에 chech에 false 저장.
-			if(i == 1) check = false;
-			
-			for(int j = 2; j < i; j++) {
-				//나누어 떨어진다면 check에 false 저장하고 반복 종료
-				if(i % j == 0) {
-					check = false;
-					break;
-				}
-			}
-			
-			//check가 true라면 실수이니 합하고, 최솟값 저장.
-			if(check) {
-				if(min > i) min = i;
-				sum += i;
-			}
-		}
-		if(sum == 0) {
-			System.out.println(-1);
-		}else {
-			System.out.println(sum + "\n" + min);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        boolean isFirst = true;
+        int min = 0;
+        for(int i = N; i <= M; i++) {
+            if(isPrime(i)) {
+                sum += i;
+                if(isFirst){
+                    min = i;
+                    isFirst = false;
+                }
+            }
+
+        }
+        if(sum == 0) System.out.println(-1);
+        else{
+            System.out.println(sum);
+            System.out.println(min);   
+        }
+    }
+    static boolean isPrime(int n){
+        if(n < 2) return false;
+        for(int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0) return false;
+        return true;
+    }
 }
