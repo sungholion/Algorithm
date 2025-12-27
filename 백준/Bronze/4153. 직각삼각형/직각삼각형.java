d6d17ec16b331cbc3f2e40a6d7f616d2e943ef1e
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-27 20:11:14 +0900

    [Bronze III] Title: 직각삼각형, Time: 96 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -6,18 +6,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        while(true){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        while (true) {
+            String str = br.readLine();
+            if (str == null) break;
+
+            StringTokenizer st = new StringTokenizer(str);
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            if(a == 0 && b == 0 && c == 0) break;
+
+            if (a == 0 && b == 0 && c == 0) break;
 
             int[] arr = {a, b, c};
             Arrays.sort(arr);
-            if((arr[2] * arr[2]) == (arr[0] * arr[0]) + (arr[1] * arr[1])) sb.append("right\n");
+
+            int x = arr[0], y = arr[1], z = arr[2];
+            if (x * x + y * y == z * z) sb.append("right\n");
             else sb.append("wrong\n");
         }
-        System.out.println(sb.toString());
+        
+        System.out.print(sb);
     }
 }
