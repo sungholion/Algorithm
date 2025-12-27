4d4f6fabf6450f50d4c8f7e0ffd4bb46e8876442
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-27 09:38:00 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -2,35 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] card = new int[N];
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
-            card[i] = Integer.parseInt(st.nextToken());
-        int ans = -1;
-        //--------------------------------------------------------- 입력
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-        for(int i=0; i<N-2; i++){
-            for(int j=i+1; j<N-1; j++){
-                for(int k=j+1; k<N; k++){
-                    int sum = card[i] + card[j] + card[k];
-                    if(sum > M) continue;
-                    if(sum == M){
-                        System.out.println(M);
-                        return;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < N-2; i++) {
+            int card1 = arr[i];
+            for(int j = i+1; j < N-1; j++) {
+                int card2 = arr[j];
+                for(int k = j+1; k < N; k++) {
+                    int card3 = arr[k];
+                    int sum = card1 + card2 + card3;
+                    if(sum > max && sum <= M){
+                        max = sum;
                     }
-                    ans = Math.max(ans, sum);
                 }
             }
         }
-
-        sb.append(ans).append("\n");
-        System.out.println(sb.toString());
+        System.out.print(max);
     }
 }
