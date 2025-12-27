b1a94b7b2406f685eaccff9b6cc00ed28cdfcf5b
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-27 19:09:20 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -2,19 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int sum = 0;
         for(int i=0; i<5; i++){
-            sum += (int) Math.pow(Integer.parseInt(st.nextToken()), 2);
+            int n = Integer.parseInt(st.nextToken());
+            sum += n * n % 10;
         }
-
-        bw.write(sum % 10 + "\n");
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.print(sum%10);
     }
 }
