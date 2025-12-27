e531a8943360c82592d000338a816f2db28383fc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-27 18:28:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 560 ms, Memory: 46352 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,34 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-class Member{
+class Person{
     int age;
     String name;
+    int order;
 
-    Member(int age, String name){
+    Person(int age, String name, int order){
         this.age = age;
         this.name = name;
+        this.order = order;
     }
 }
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        Member[] members = new Member[N];
-        for(int i = 0; i < N; i++){
+        List<Person> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int age = Integer.parseInt(st.nextToken());
-            String name = st.nextToken(); 
-            members[i] = new Member(age, name);
+            String name = st.nextToken();
+            int order = i;
+            list.add(new Person(age, name, order));
         }
 
-        Arrays.sort(members, (a, b) -> a.age - b.age);
+        list.sort((a, b) -> {
+           if(a.age == b.age) return a.order - b.order;
+           return a.age - b.age;
+        });
 
-        for(Member m : members)
-            sb.append(m.age).append(" ").append(m.name).append("\n");
-        System.out.println(sb.toString());
+        for(Person p : list) {
+            sb.append(p.age).append(" ").append(p.name).append("\n");
+        }
+        System.out.print(sb);
     }
 }
