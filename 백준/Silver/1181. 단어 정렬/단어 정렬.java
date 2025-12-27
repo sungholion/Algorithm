176fcc80a5706a497a185678ce14a4412bd2d023
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-27 18:22:59 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 26704 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,24 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
-        while(N-- > 0) set.add(br.readLine());
 
-        List<String> words = new ArrayList<>(set);
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
 
-        words.sort((a,b) -> {
-            if(a.length() != b.length()) return a.length() - b.length();
-            return a.compareTo(b);
+        List<String> list = new ArrayList<>(set);
+
+        list.sort((a, b) -> {
+           if(a.length() == b.length()) return a.compareTo(b);
+           return a.length() - b.length();
         });
 
-        for(String word : words) {
-            sb.append(word).append("\n");
+        for(String s : list) {
+            sb.append(s).append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.print(sb);
     }
 }
