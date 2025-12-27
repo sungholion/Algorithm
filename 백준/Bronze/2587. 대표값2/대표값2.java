b597cce13e2e9d674bb47dca4923e72420faa955
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-27 15:07:44 +0900

    [Bronze II] Title: 대표값2, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,41 +1,31 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = 5;
-		int[] arr = new int[n];
-		
-		int sum = 0;
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			sum += arr[i];
-		}
-		
-		for(int i=0; i<n-1; i++) {
-			for(int j=i+1; j<n; j++) {
-				if(arr[i] > arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		
-		
-		bw.write(sum/5 + "\n");
-		bw.write(arr[n/2] + "\n");
-		
-		
-			
-		
-		bw.flush();
-		bw.close();
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        for(int i = 0; i < 5 - 1; i++){
+            for(int j = 0; j < 5 - 1 - i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        sb.append(sum / arr.length).append('\n');
+        sb.append(arr[arr.length/2]);
+        System.out.print(sb.toString());
+
+    }
 }
