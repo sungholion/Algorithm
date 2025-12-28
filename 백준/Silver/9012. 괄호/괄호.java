ff7d0a0a84885ff8756491d7aac43252cfdb8600
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-28 19:03:19 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -2,39 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-        while(T--> 0){
+        for(int t = 0; t < T; t++) {
             String str = br.readLine();
-            Stack<Character> s = new Stack<>();
-            boolean isBalanced = true;
-
-            for(int i = 0; i < str.length(); i++){
+            Stack<Character> stack = new Stack<>();
+            boolean flag = true;
+            for(int i = 0; i < str.length(); i++) {
                 char c = str.charAt(i);
-                if(c == '(') s.push(c);
-                else if(c == ')'){
-                    if(s.isEmpty()) {
-                        isBalanced = false;
+                if(c == '('){
+                    stack.push(c);
+                } else if(c == ')'){
+                    if(stack.isEmpty() || !stack.peek().equals('(')){
+                        flag = false;
                         break;
                     }
-                    char temp = s.pop();
-                    if(temp != '('){
-                        isBalanced = false;
-                        break;
-                    }
+                    stack.pop();
                 }
             }
 
-            if(!s.isEmpty()) isBalanced = false;
-            if(isBalanced) sb.append("YES\n");
-            else sb.append("NO\n");
-
-
+            if(flag && stack.isEmpty()) sb.append("YES").append("\n");
+            else sb.append("NO").append("\n");
         }
-        System.out.println(sb);
-    }
-}
 
+        System.out.print(sb.toString());
+    }
+
+}
