34b577b687b4c2b3e452cda4b222ecde33017bdd
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-28 19:15:23 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23940 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,31 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] arr;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
         int K = Integer.parseInt(br.readLine());
-        Stack<Integer> s = new Stack<>();
-        while(K-- > 0){
-            int num = Integer.parseInt(br.readLine());
-            if(num == 0){
-                s.pop();
-            } else{
-                s.push(num);
-            }
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0) stack.pop();
+            else stack.push(n);
         }
 
-        int sum = 0;
-        while(!s.isEmpty())
-            sum += s.pop();
+        while(!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        sb.append(sum);
 
-        sb.append(sum).append(("\n"));
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+
+        System.out.print(sb.toString());
     }
+
 }
