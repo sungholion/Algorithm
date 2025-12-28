f3251becbd8691adb1ccac6b1c9653c643d8e0fd
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-28 19:38:37 +0900

    [Silver IV] Title: solved.ac, Time: 292 ms, Memory: 37544 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,36 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-class Coord{
-    int x;
-    int y;
-
-    Coord(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
+        int except = (int)Math.round(N * 0.15);
         int[] arr = new int[N];
-        for(int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+        }
         Arrays.sort(arr);
-
-        int cut = (int)Math.round(N * 0.15); // 앞뒤 절삭 개수
         int sum = 0;
-        for(int i = cut; i < N - cut; i++) {
+        for (int i = except; i < arr.length - except; i++){
             sum += arr[i];
         }
-
-        int avg = (int)Math.round((double)sum / (N - 2*cut));
-        sb.append(avg).append("\n");
-
-        System.out.println(sb.toString());
+        double avg = (double)sum / (N- 2 * except);
+        sb.append(Math.round(avg));
+        System.out.print(sb);
     }
 }
