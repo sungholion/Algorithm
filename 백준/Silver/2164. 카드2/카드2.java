81682fee441fba902b9a70fc10a31a25dc05dd26
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-28 18:31:25 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 29020 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,22 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> q = new LinkedList<>();
-
-        for (int i = 1; i <= N; i++)
-            q.offer(i);
-        
-        while(q.size() > 1){
-            q.poll();
-            q.offer(q.poll());
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for(int i = 1; i <= N; i++) {
+            q.addLast(i);
         }
+        while(q.size() > 1) {
+            q.pollFirst();
+            q.addLast(q.pollFirst());
+        }
+        sb.append(q.pollFirst());
 
-        sb.append(q.poll());
-        System.out.println(sb);
+        System.out.print(sb.toString());
     }
+
 }
