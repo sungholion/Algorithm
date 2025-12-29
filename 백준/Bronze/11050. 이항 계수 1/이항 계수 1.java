463efcc0a041987deb2d94355c4385b6b358c920
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-29 10:30:30 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,28 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        sb.append(factorial(N) / (factorial(K) * factorial(N-K)));
+        System.out.print(sb.toString());
+    }
 
-        // n!, k!, (n-k)! 계산
-        int factN = 1;
-        for (int i = 1; i <= n; i++) factN *= i;
-
-        int factK = 1;
-        for (int i = 1; i <= k; i++) factK *= i;
-
-        int factNK = 1;
-        for (int i = 1; i <= n - k; i++) factNK *= i;
-
-        // 공식 적용: n! / (k! * (n-k)!)
-        int result = factN / (factK * factNK);
-
-        System.out.println(result);
+    static long factorial(int x) {
+        if(x <= 1) return 1;
+        return x * factorial(x-1);
     }
 }
