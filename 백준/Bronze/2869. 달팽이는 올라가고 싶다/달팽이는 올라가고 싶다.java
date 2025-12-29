63833e31e1066c2370dd35d59fdd47bcaa32c0d0
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-29 11:12:13 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -2,19 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long A = Long.parseLong(st.nextToken()); 
-        long B = Long.parseLong(st.nextToken()); 
-        long V = Long.parseLong(st.nextToken()); 
-
-        long days = (V - B) / (A - B);
-        if ((V - B) % (A - B) != 0) days++; 
-        
-        sb.append(days);
-        System.out.println(sb);
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int res = (int)Math.ceil((double)(V - A) / (A - B)) + 1;
+        sb.append(res);
+        System.out.print(sb);
     }
 }
