94f8671a866621a3af937bdf302161179d2b05f0
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-29 14:34:49 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,20 +1,27 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
+        int N = Integer.parseInt(br.readLine());
         int cnt = 0;
-        while (N >= 0) {
-            if (N % 5 == 0) {            // 5로 깔끔히 나눠지면 마무리
+
+        while(true){
+            if(N % 5 == 0){
                 cnt += N / 5;
-                System.out.println(cnt);
-                return;
+                System.out.print(cnt);
+                break;
             }
-            N -= 3;                      // 5로 안 나눠지면 3 한 봉지 사용
+            N -= 3;
             cnt++;
+
+            if(N < 0){
+                System.out.println(-1);
+                break;
+            }
         }
-        System.out.println(-1);          // 끝까지 못 맞추면 불가능
     }
 }
