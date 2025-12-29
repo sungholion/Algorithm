b9e2cc4fdc204de76bc727f6e41554fb774cb30c
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-29 11:20:58 +0900

    [Silver V] Title: 단어 정렬, Time: 320 ms, Memory: 26648 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -7,22 +7,22 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
-
-        for (int i = 0; i < N; i++) {
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0; i < N; i++) {
             set.add(br.readLine());
         }
-
         List<String> list = new ArrayList<>(set);
-
         list.sort((a, b) -> {
-           if(a.length() == b.length()) return a.compareTo(b);
+           if(a.length() == b.length()) {
+               return a.compareTo(b);
+           }
            return a.length() - b.length();
         });
 
         for(String s : list) {
             sb.append(s).append("\n");
         }
+
         System.out.print(sb);
     }
 }
