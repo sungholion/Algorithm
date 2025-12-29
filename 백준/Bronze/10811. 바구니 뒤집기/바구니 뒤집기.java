a7f8134717ac7f93bd3cd5a6094f1b40d548bd56
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-29 11:07:38 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 116 ms, Memory: 15876 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -2,38 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int[] basket = new int[N+1];
-        for(int i = 1; i <= N; i++)
-            basket[i] = i;
-
-        for(int t = 0; t < M; t++){
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) arr[i] = i + 1;
+        for(int t = 0; t < M; t++) {
             st = new StringTokenizer(br.readLine());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
-            for(int k = i, l = j ; k <= l; k++, l--){
-                int temp = basket[k];
-                basket[k] = basket[l];
-                basket[l] = temp;
+
+            for(int start = i-1, end = j-1; start < end; start++, end--) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
             }
-
         }
-        
-        for(int i = 1; i <= N; i++)
-            sb.append(basket[i]).append(" ");
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i] + " ");
+        }
+        System.out.print(sb);
     }
 }
