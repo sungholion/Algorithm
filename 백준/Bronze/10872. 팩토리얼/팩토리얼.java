dfe8b20e4acab2bd61fbfb8aec8aa07726c195aa
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-30 10:49:58 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,26 +1,17 @@
 import java.io.*;
-import java.time.LocalDate;
 import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        int sum = 1;
-
-        while(N != 0){
-            sum *= N;
-            N--;
-        }
-
-        sb.append(sum);
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        int n = Integer.parseInt(br.readLine());
+        sb.append(factorial(n));
+        System.out.print(sb);
+    }
+    static long factorial(int n) {
+        if(n <= 1) return 1;
+        return n * factorial(n-1);
     }
 }
