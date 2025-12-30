8df10dc4ffbd486acc5b82d72e898bd77fcea342
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-30 10:40:35 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        sb.append(N * N - N);
+        
+        System.out.print(sb);
+    }
+}
