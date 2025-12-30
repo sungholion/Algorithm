34a4b427cecd21a7af82e1abea689b9a228d080a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2025-12-30 13:57:32 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,38 +1,31 @@
-import java.beans.Visibility;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
-	static int n, m;
-	static int[][] table = new int[31][31];
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int loop = 0; loop < T; loop++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
-			
-			System.out.println(dfs(m, n));
-		}
-		
-	}
-	
-	static int dfs(int m, int n) {
-		if(table[m][n] > 0) {
-			return table[m][n];
-		}
-		
-		else if(m == n || n == 0) {
-			return table[m][n] = 1;
-		}
-		else {
-			return table[m][n] = dfs(m-1, n-1) + dfs(m-1, n);
-		}
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            if(N == 0 || M == 0){
+                sb.append("0\n");
+                continue;
+            }
+            sb.append(combination(N, M)).append("\n");
+        }
+        System.out.print(sb);
+    }
+    static BigInteger combination(int n, int m) {
+        BigInteger result = BigInteger.ONE;
+        for(int i = 0; i < n; i++) {
+            result = result.multiply(BigInteger.valueOf(m-i));
+            result = result.divide(BigInteger.valueOf(i+1));
+        }
+        return result;
+    }
 }
