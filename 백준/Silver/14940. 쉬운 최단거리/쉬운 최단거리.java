1736765c5837939bd26b3bc891585f8d8f9b5b3a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-02 13:29:27 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 612 ms, Memory: 51968 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -2,60 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int[] dr = {1, -1, 0, 0};
-    static final int[] dc = {0, 0, 1, -1};
-
-    public static void main(String[] args) throws Exception {
+    static int N, M;
+    static int[][] map;
+    static int[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] start;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); 
-        int m = Integer.parseInt(st.nextToken()); 
 
-        int[][] map = new int[n][m];
-        int[][] dist = new int[n][m];
-        for (int i = 0; i < n; i++) Arrays.fill(dist[i], -1);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(visited[i], -1);
+        }
 
-        int sr = -1, sc = -1;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 2) {
-                    sr = i; sc = j;
-                }
-                if (map[i][j] == 0) {
-                    dist[i][j] = 0;
-                }
+                if(map[i][j] == 2) start = new int[]{i, j};
+                else if(map[i][j] == 0) visited[i][j] = 0;
             }
         }
 
-        // BFS from (sr, sc)
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{sr, sc});
-        dist[sr][sc] = 0;
+        bfs();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(visited[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+
+    static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(start);
+        visited[start[0]][start[1]] = 0;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
+            for(int i = 0; i < 4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-            for (int d = 0; d < 4; d++) {
-                int nr = r + dr[d];
-                int nc = c + dc[d];
-                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                if (map[nr][nc] == 0) continue;       
-                if (dist[nr][nc] != -1) continue;     
-                dist[nr][nc] = dist[r][c] + 1;
-                q.offer(new int[]{nr, nc});
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(map[nx][ny] == 0 || visited[nx][ny] != -1) continue;
+                visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
+                q.offer(new int[]{nx, ny});
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                sb.append(dist[i][j]).append(' ');
-            }
-            sb.append('\n');
-        }
-        System.out.print(sb.toString());
+
     }
 }
