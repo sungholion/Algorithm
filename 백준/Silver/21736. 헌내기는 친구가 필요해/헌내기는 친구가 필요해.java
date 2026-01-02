a0d6e982a7ed0492e09d5ed6a62720d48901d176
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-02 13:11:30 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 336 ms, Memory: 36484 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -4,43 +4,54 @@ import java.util.*;
 public class Main {
     static int N, M;
     static char[][] map;
-    static boolean[][] vis;
-    static int[] dx = {-1,1,0,0}, dy = {0,0,-1,1};
-
-    public static void main(String[] args) throws Exception {
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[] start;
+    static int people = 0;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new char[N][M];
-        vis = new boolean[N][M];
-        int sx = -1, sy = -1;
+        visited = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
+            String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = s.charAt(j);
-                if (map[i][j] == 'I') { sx = i; sy = j; }
+                char c = str.charAt(j);
+                if(c == 'I') start = new int[]{i, j};
+                map[i][j] = str.charAt(j);
             }
         }
 
-        int ans = 0;
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{sx, sy});
-        vis[sx][sy] = true;
+        bfs();
+
+        System.out.print(people == 0 ? "TT" : people);
+    }
+
+    static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(start);
+        visited[start[0]][start[1]] = true;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int x = cur[0], y = cur[1];
-            if (map[x][y] == 'P') ans++;
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d], ny = y + dy[d];
-                if (nx<0||ny<0||nx>=N||ny>=M) continue;
-                if (vis[nx][ny] || map[nx][ny]=='X') continue;
-                vis[nx][ny] = true;
+            for(int i = 0; i < 4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(map[nx][ny] == 'I' || map[nx][ny] == 'X' || visited[nx][ny]) continue;
+                if(map[nx][ny] == 'P'){
+                    people++;
+                }
+                visited[nx][ny] = true;
                 q.offer(new int[]{nx, ny});
             }
         }
-        System.out.print(ans == 0 ? "TT" : ans);
+
     }
 }
