cad1f72611c70d1edb02343fcfba2d5dbc1067dd
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-02 13:38:35 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 26568 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -2,22 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
-        while (N-- > 0) {
+        for(int i = 0; i < N; i++) {
             int x = Integer.parseInt(br.readLine());
             if(x == 0){
-                if(pq.isEmpty()){sb.append(0).append("\n");}
+                if(pq.isEmpty()) sb.append("0\n");
                 else sb.append(pq.poll()).append("\n");
+            } else{
+                pq.offer(x);
             }
-            else pq.offer(x);
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
-
 }
