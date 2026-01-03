08edf7a5fd1d54e86dae896ff1aff483ec1279cc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-03 23:22:32 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,47 +1,41 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args)throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int[] arr = new int[K];
+        int[] lines = new int[K];
+        int max = Integer.MIN_VALUE;
 
-        long max = 0;
-        for (int i = 0; i < K; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if(max < arr[i])
-                max = arr[i];
+        for(int i = 0; i < K; i++){
+            lines[i] = Integer.parseInt(br.readLine());
+            if(lines[i] > max) max = lines[i];
         }
-        
-        max++;  // max +1 (mid 0 방지)
 
-        long min = 0;
-        long mid = 0;
+        long left = 1;
+        long right = max;
+        long ans = 0;
 
-        while (min < max) {
-            mid = (max + min) / 2;
-
+        while(left <= right){
+            long mid = (left + right) / 2;
             long cnt = 0;
-            for (int i = 0; i < arr.length; i++)  // 구한 중간 길이로 몇개 나오는지
-                cnt += (arr[i] / mid);
 
-            if(cnt < N) // 요구한 개수보다 적으면 길이를 -
-                max = mid;
-            else        // 그 외에는 길이를 +
-                min = mid + 1;
+            for(int i = 0; i < K; i++){
+                cnt += lines[i] / mid;
+            }
+
+            if(cnt >= N){
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-
-        sb.append(min -1);  // 아까 +1 했으므로
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(ans);
 
     }
 }
