5c435cc797b3a5b37d4eb4fce95fa3c44f86b752
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-03 16:23:44 +0900

    [Bronze III] Title: 학점계산, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -1,27 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        String grade = br.readLine();
 
-        Map<String, String> map = new HashMap<>();
-        map.put("A+", "4.3");
-        map.put("A0", "4.0");
-        map.put("A-", "3.7");
-        map.put("B+", "3.3");
-        map.put("B0", "3.0");
-        map.put("B-", "2.7");
-        map.put("C+", "2.3");
-        map.put("C0", "2.0");
-        map.put("C-", "1.7");
-        map.put("D+", "1.3");
-        map.put("D0", "1.0");
-        map.put("D-", "0.7");
-        map.put("F",  "0.0");
+        double score = 0.0;
+        switch (grade) {
+            case "A+": score = 4.3; break;
+            case "A0": score = 4.0; break;
+            case "A-": score = 3.7; break;
+            case "B+": score = 3.3; break;
+            case "B0": score = 3.0; break;
+            case "B-": score = 2.7; break;
+            case "C+": score = 2.3; break;
+            case "C0": score = 2.0; break;
+            case "C-": score = 1.7; break;
+            case "D+": score = 1.3; break;
+            case "D0": score = 1.0; break;
+            case "D-": score = 0.7; break;
+            case "F":  score = 0.0; break;
+        }
 
-        System.out.println(map.get(str));
-
+        System.out.println(score);
     }
 }
