0367637271a6534b97cc6facb9f3f00a3b56db1c
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-03 09:53:05 +0900

    [Gold V] Title: 적록색약, Time: 120 ms, Memory: 14908 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,85 +1,82 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     static int N;
     static char[][] map;
-    static boolean[][] vis;
+    static boolean[][] visited;
+    static int rgb, nonRgb;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int normal, abnormal;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(br.readLine());
         map = new char[N][N];
-        for(int i=0; i<N; i++){
-            String str = br.readLine();
-            for(int j=0; j<N; j++){
-                map[i][j] = str.charAt(j);
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = s.charAt(j);
             }
         }
 
-        vis = new boolean[N][N];
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(vis[i][j]) continue;
-                if(map[i][j] == 'R'){
-                    normal++;
-                    bfs(i, j, 'R', false);
-                }
-                else if(map[i][j] == 'G'){
-                    normal++;
-                    bfs(i, j, 'G', false);
-                } else if(map[i][j] == 'B'){
-                    normal++;
-                    bfs(i, j, 'B', false);
+        visited = new boolean[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j]) {
+                    nonRgb++;
+                    bfs(new int[]{i, j}, map[i][j], true);
                 }
             }
-        }
 
-        vis = new boolean[N][N];
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(vis[i][j]) continue;
-                if(map[i][j] == 'R' || map[i][j] == 'G'){
-                    abnormal++;
-                    bfs(i, j, 'R', true);
-                } else if(map[i][j] == 'B'){
-                    abnormal++;
-                    bfs(i, j, 'B', true);
+        }
+        visited = new boolean[N][N];
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(!visited[i][j]) {
+                    rgb++;
+                    bfs(new int[]{i, j}, map[i][j], false);
                 }
             }
         }
 
-        System.out.println(normal + " " + abnormal);
+        sb.append(nonRgb).append(" ").append(rgb).append("\n");
+        System.out.print(sb.toString());
     }
-
-    private static void bfs(int x, int y, char color, boolean mode){
+    static void bfs(int[] point, char color, boolean isRgb) {
         ArrayDeque<int[]> q = new ArrayDeque<>();
-        vis[x][y] = true;
-        q.offer(new int[]{x, y});
+        visited[point[0]][point[1]] = true;
+        q.offer(point);
+        if(isRgb){  // 적록색약 아닐때
+            while(!q.isEmpty()) {
+                int[] cur = q.poll();
+                for(int i = 0; i < 4; i++) {
+                    int nx = cur[0] + dx[i];
+                    int ny = cur[1] + dy[i];
 
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-            for(int i=0; i<4; i++){
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-                if(vis[nx][ny]) continue;
-                if(!mode){  // 정상
-                    if(map[nx][ny] != color) continue;
-                } else {    //적록색약
-                    if(map[nx][ny] == 'B'){ // 만약에 B인데 B가 안나오면 안됌
-                        if(color != 'B') continue;
-                    } else{ // 만약 B가 아닌데 B가 나오면 안됌
-                        if(color == 'B') continue;
+                    if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                    if(map[nx][ny] == color && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        q.offer(new int[]{nx, ny});
                     }
                 }
-                vis[nx][ny] = true;
-                q.offer(new int[]{nx, ny});
+            }
+        } else {
+            while(!q.isEmpty()) {
+                int[] cur = q.poll();
+                for(int i = 0; i < 4; i++) {
+                    int nx = cur[0] + dx[i];
+                    int ny = cur[1] + dy[i];
+
+                    if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
+                    if((color == map[nx][ny]) || (color == 'R' && map[nx][ny] == 'G') || color == 'G' && map[nx][ny] == 'R'){
+                        visited[nx][ny] = true;
+                        q.offer(new int[]{nx, ny});
+                    }
+                }
             }
         }
+
     }
 }
