625c55c5502f558e91db689ecbc030a086184786
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-04 17:30:37 +0900

    [Bronze III] Title: ACM 호텔, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -5,23 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
 
+        int T = Integer.parseInt(br.readLine());
         for(int t = 0; t < T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int H = Integer.parseInt(st.nextToken());   // 층수
-            int W = Integer.parseInt(st.nextToken());   // 방수
-            int N = Integer.parseInt(st.nextToken());   // 몇번째 손
-            int[][] hotel = new int[H][W];
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
 
             int floor = N % H;
             int room = N / H + 1;
             if(floor == 0) {
                 floor = H;
-                room -= 1;
+                room--;
             }
-            sb.append(floor).append(String.format("%02d", room)).append("\n");
+
+            sb.append(floor);
+            if(room < 10) sb.append("0");
+            sb.append(room).append("\n");
+
         }
-        System.out.println(sb.toString());
+        System.out.print(sb);
+
+
     }
 }
