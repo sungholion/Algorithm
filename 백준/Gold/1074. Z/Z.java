f6425cb1e723a133f4dedc645257401e2a9337bc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-04 16:08:55 +0900

    [Gold V] Title: Z, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -2,48 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, r, c, ans;
-    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int size = 1 << N;
+        int answer = 0;
 
-        N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        while(size >= 1){
+            size /= 2;
+            int quad = size * size;
 
-        int size = (int)Math.pow(2, N);
-        find(0, 0, size);
-
-        sb.append(ans).append("\n");
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+            if (r < size && c < size) {
+            } else if (r < size && c >= size) {
+                answer += quad;
+                c -= size;
+            } else if (r >= size && c < size) {
+                answer += quad * 2;
+                r -= size;
+            } else {
+                answer += quad * 3;
+                r -= size;
+                c -= size;
+            }
+        }
+        System.out.print(answer);
     }
-
-    public static void find(int row, int col, int size) {
-        if(size == 1){
-            return;
-        }
-        int newSize = size / 2;
-        if((r < row + newSize) && (c < col + newSize)){
-            find(row, col, newSize);
-        }
-        if((r < row + newSize) && (c >= col + newSize)){
-            ans += (size * size) / 4;
-            find(row, col + newSize, newSize);
-        }
-        if((r >= row + newSize) && (c < col + newSize)){
-            ans += ((size * size) / 4) * 2;
-            find(row + newSize, col, newSize);
-        }
-        if((r >= row + newSize) && (c >= col + newSize)){
-            ans += ((size * size) / 4) * 3;
-            find(row + newSize, col + newSize, newSize);
-        }
-    }
-
 }
