8bb027766a3581ea75d043f2309f30c6880bd6a4
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-04 23:15:50 +0900

    [Silver V] Title: 집합, Time: 1920 ms, Memory: 313080 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -7,42 +7,32 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int M = Integer.parseInt(br.readLine());
-        int bitmask = 0; // 공집합 시작
-
-        for (int i = 0; i < M; i++) {
-            String[] cmd = br.readLine().split(" ");
-
-            switch (cmd[0]) {
-                case "add":
-                    int x1 = Integer.parseInt(cmd[1]);
-                    bitmask |= (1 << x1);
-                    break;
-
-                case "remove":
-                    int x2 = Integer.parseInt(cmd[1]);
-                    bitmask &= ~(1 << x2);
-                    break;
-
-                case "check":
-                    int x3 = Integer.parseInt(cmd[1]);
-                    sb.append((bitmask & (1 << x3)) != 0 ? "1\n" : "0\n");
-                    break;
-
-                case "toggle":
-                    int x4 = Integer.parseInt(cmd[1]);
-                    bitmask ^= (1 << x4);
-                    break;
-
-                case "all":
-                    bitmask = (1 << 21) - 1; // 1~20 모두 포함
-                    break;
-
-                case "empty":
-                    bitmask = 0;
-                    break;
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if(command.equals("add")) {
+                int x = Integer.parseInt(st.nextToken());
+                set.add(x);
+            } else if(command.equals("remove")) {
+                int x = Integer.parseInt(st.nextToken());
+                set.remove(x);
+            } else if(command.equals("check")) {
+                int x = Integer.parseInt(st.nextToken());
+                if(set.contains(x)) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
+            } else if(command.equals("toggle")) {
+                int x = Integer.parseInt(st.nextToken());
+                if(set.contains(x)) set.remove(x);
+                else set.add(x);
+            } else if(command.equals("all")) {
+                set = new HashSet<>();
+                for(int t = 1; t <= 20; t++)
+                    set.add(t);
+            } else if(command.equals("empty")) {
+                set.clear();
             }
         }
-
-        System.out.print(sb);
+        System.out.print(sb.toString());
     }
 }
