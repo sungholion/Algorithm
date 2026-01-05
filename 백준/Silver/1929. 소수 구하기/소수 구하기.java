67bd26d5c15d26f62b1e4ce72f574a9251806ed7
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-05 09:59:59 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 21488 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,38 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean[] prime;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
+        boolean[] isPrime = new boolean[N + 1];
+        for(int i = 0; i <= N; i++) isPrime[i] = true;
+        isPrime[0] = false;
+        isPrime[1] = false;
 
-        prime = new boolean[N + 1];
-        get_prime();
-
-        for(int i = M; i <= N; i++)
-            if(!prime[i])
-                sb.append(i).append("\n");
-
-        br.close();
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
-
-    public static void get_prime() {
- 
-        prime[0] = prime[1] = true;
-
-        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
-            for(int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
+        for(int i = 2; i * i <= N; i++){
+            if(isPrime[i]){
+                for(int j = i * i; j <= N; j += i){
+                    isPrime[j] = false;
+                }
             }
         }
+
+        for(int i = M; i <= N; i++){
+            if(isPrime[i]) sb.append(i).append("\n");
+        }
+
+
+        System.out.print(sb);
+
     }
 
 }
