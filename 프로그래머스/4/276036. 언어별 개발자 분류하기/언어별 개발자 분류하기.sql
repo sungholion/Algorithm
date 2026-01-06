d04ad01d9d2c1f0b42418a9849629df0f95c6295
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-06 17:16:00 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,21 @@
+SELECT 
+    CASE
+        WHEN 
+            SUM(CASE WHEN SC.CATEGORY = 'Front End' THEN 1 ELSE 0 END) > 0
+         AND SUM(CASE WHEN SC.NAME = 'Python' THEN 1 ELSE 0 END) > 0
+            THEN 'A'
+        WHEN 
+            SUM(CASE WHEN SC.NAME = 'C#' THEN 1 ELSE 0 END) > 0
+            THEN 'B'
+        WHEN 
+            SUM(CASE WHEN SC.CATEGORY = 'Front End' THEN 1 ELSE 0 END) > 0
+            THEN 'C'
+    END AS GRADE,
+    D.ID,
+    D.EMAIL
+FROM DEVELOPERS D
+JOIN SKILLCODES SC
+  ON (D.SKILL_CODE & SC.CODE) = SC.CODE
+GROUP BY D.ID, D.EMAIL
+HAVING GRADE IS NOT NULL
+ORDER BY GRADE ASC, D.ID ASC;
