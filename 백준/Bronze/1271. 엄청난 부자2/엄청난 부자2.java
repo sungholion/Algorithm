6877ddf8cc4c1d56e96e7109ad7a1b7f98c16b4c
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-07 15:20:00 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -3,13 +3,19 @@ import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
+    static int[][] maps;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-       
-        BigInteger n = new BigInteger(st.nextToken());
-        BigInteger m = new BigInteger(st.nextToken());
-        
-        System.out.print(n.divide(m) + "\n" + n.remainder(m));
+        BigInteger n  = new BigInteger(st.nextToken());
+        BigInteger m  = new BigInteger(st.nextToken());
+
+        sb.append(n.divide(m)).append("\n");
+        sb.append(n.mod(m)).append("\n");
+
+        System.out.println(sb);
     }
 }
