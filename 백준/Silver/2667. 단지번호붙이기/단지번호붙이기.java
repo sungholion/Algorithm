47649cd94d37f4a6fa550b1afba5cb8d76943b12
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-08 17:47:33 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -3,62 +3,63 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[][] map;
-    static boolean[][] visited;
+    static char[][] map;
+    static boolean[][] visted;
+    static ArrayList<Integer> houseCnt =  new ArrayList<>();
+    static int totalCnt;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static ArrayList<Integer> list = new ArrayList<>();
-    static StringBuilder sb;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
+        StringBuilder sb =  new StringBuilder();
+
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        visited = new boolean[N][N];
-        for(int i = 0; i < N; i++) {
+        map = new char[N][N];
+        visted = new boolean[N][N];
+        for(int i = 0; i < N; i++){
             String str = br.readLine();
-            for(int j = 0; j < N; j++) {
-                map[i][j] = str.charAt(j) - '0';
+            for(int j = 0; j < N; j++){
+                char ch = str.charAt(j);
+                map[i][j] = ch;
             }
         }
 
-
-        int cnt = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                if(map[i][j] == 1 && !visited[i][j]){
-                    cnt++;
-                    bfs(new int[]{i, j});
+                if(map[i][j] == '1' && !visted[i][j]){
+                    totalCnt++;
+                    houseCnt.add(bfs(i, j));
                 }
             }
         }
-        sb.append(cnt).append("\n");
-        Collections.sort(list);
-        for(int x : list)
-            sb.append(x).append("\n");
-        System.out.print(sb);
+        sb.append(totalCnt).append("\n");
+        Collections.sort(houseCnt);
+        for(int i = 0; i < totalCnt; i++){
+            sb.append(houseCnt.get(i)).append("\n");
+        }
+        System.out.println(sb.toString());
     }
 
-    static void bfs(int[] point){
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        visited[point[0]][point[1]] = true;
-        q.offer(point);
-
+    static int bfs(int x, int y){
+        ArrayDeque<int[]> q =  new ArrayDeque<>();
+        q.offer(new int[]{x, y});
+        visted[x][y] = true;
         int cnt = 1;
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            for(int i = 0; i < 4; i++){
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
 
-                if(nx >= N || nx < 0 || ny >= N || ny < 0) continue;
-                if(map[nx][ny] == 1 && !visited[nx][ny]){
-                    visited[nx][ny] = true;
+        while(!q.isEmpty()){
+            int[] cur =  q.poll();
+            for(int i = 0; i < 4; i++){
+                int nx =  cur[0] + dx[i];
+                int ny =  cur[1] + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if(map[nx][ny] == '1' && !visted[nx][ny]){
+                    visted[nx][ny] = true;
                     q.offer(new int[]{nx, ny});
                     cnt++;
                 }
             }
         }
-        list.add(cnt);
+        return cnt;
     }
 }
