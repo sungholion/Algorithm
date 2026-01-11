446b62c3c8c34542556fce8c5517b407ebba4df6
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 01:35:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,25 +1,18 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		long[] table = new long[1001];
-		
-		table[1] = 1;
-		table[2] = 2;
-		
-		for(int i=3; i<=n; i++)
-			table[i] = (table[i-2] + table[i-1]) % 10007;
-		
-		System.out.println(table[n]);
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        if (n > 1) dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n] % 10007);
+    }
 }
