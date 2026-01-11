d060be2551b3a4f356d6b6d617a7a9fa7a352f34
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-11 21:34:49 +0900

    [Silver III] Title: 피보나치 함수, Time: 92 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,27 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine().trim());
+        int T = Integer.parseInt(br.readLine());
 
-        int MAX = 40;
-        int[] zero = new int[MAX + 1];
-        int[] one  = new int[MAX + 1];
+        int[] zero = new int[41];
+        int[] one = new int[41];
 
-        zero[0] = 1; one[0] = 0;
-        zero[1] = 0; one[1] = 1;
+        zero[0] = 1;  one[0] = 0;
+        zero[1] = 0;  one[1] = 1;
 
-        for (int i = 2; i <= MAX; i++) {
+        for (int i = 2; i <= 40; i++) {
             zero[i] = zero[i - 1] + zero[i - 2];
-            one[i]  = one[i - 1]  + one[i - 2];
+            one[i] = one[i - 1] + one[i - 2];
         }
 
-        for (int t = 0; t < T; t++) {
-            int n = Integer.parseInt(br.readLine().trim());
-            sb.append(zero[n]).append(' ').append(one[n]).append('\n');
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(zero[n]).append(" ").append(one[n]).append("\n");
         }
 
         System.out.print(sb.toString());
