474e8ebe0a36f1ae3fd78981f9235040a586288d
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 16:29:30 +0900

    [level 1] Title: 폰켓몬, Time: 6.79 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,15 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
-        int n = nums.length / 2;
-        HashSet<Integer> set = new HashSet<>();
-        for(int x : nums) set.add(x);
+        int answer = 0;
         
-        if(set.size() >= n) return n;
-        else return set.size();
-
+        int pick = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        for(int x : nums) set.add(x);
+        int cnt = set.size();
+        
+        if(cnt >= pick) return pick;
+        else return cnt;
     }
 }
