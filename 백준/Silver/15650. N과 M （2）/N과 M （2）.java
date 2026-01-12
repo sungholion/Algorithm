08b23ce6b6eab0e026190682c1161dc0a6d3fcac
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 12:07:27 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,45 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] num;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		num = new int[M];
-		visited = new boolean[N+1];
-		
-		dfs(1, 0);
-		
-		System.out.println(sb.toString());
-	}
-	
-	static void dfs(int start, int depth) {
-		if (depth == M) {
-			for(int x : num) {
-				sb.append(x).append(" ");
-			}
-			
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=start; i<=N; i++) {
-			num[depth] = i;
-			dfs(i+1, depth+1);
-		}
-		
-	}
-	
+
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(1, 0);
+
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
 }
