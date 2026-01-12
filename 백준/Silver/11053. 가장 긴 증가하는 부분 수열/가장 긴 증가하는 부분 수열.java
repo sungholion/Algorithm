abebb8a62ef0df3d6c478c44616123227367f245
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 17:16:04 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,30 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
-        int[] length = new int[n];
-        int ans = 1;
+        Arrays.fill(dp, 1); // 최소 길이는 1
 
-        for(int k = 0; k < n; k++){
-            length[k] = 1;
-
-            for(int i=0; i<k; i++){
-                if(arr[i] < arr[k]){
-                    length[k] = Math.max(length[k], length[i] + 1);
-                    ans = Math.max(ans, length[k]);
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
 
+        int answer = 0;
+        for (int i = 0; i < N; i++) answer = Math.max(answer, dp[i]);
 
-//        for(int x : length){
-//            System.out.print(x + " ");
-//        }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
