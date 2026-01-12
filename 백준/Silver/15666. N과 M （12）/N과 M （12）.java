f82ab2e9bac1c3d4529e8ebe9681a38f9f51a12a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 12:22:01 +0900

    [Silver II] Title: N과 M (12), Time: 108 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,75 +1,50 @@
-/*
-문제
-N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
-N개의 자연수 중에서 M개를 고른 수열
-
-입력
-첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
-둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
-
-출력
-한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
-수열은 사전 순으로 증가하는 순서로 출력해야 한다.
- */
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] num;
-	static int[] input;
-	static boolean[] visited;
-	static HashSet<String> set = new LinkedHashSet<>();
-	static StringBuilder sb = new StringBuilder();
-	static int o;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		num = new int[M];
-		visited = new boolean[N+1];
-		input = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++)
-			input[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(input);
-		
-		dfs(0, 0);
-		Iterator iter = set.iterator();
-		while(iter.hasNext()){
-		    System.out.println(iter.next());
-		}
-		    
-	}
-	
-	static void dfs(int start, int depth) {
-		if (depth == M) {
-			sb.setLength(0);
-			
-			for(int x : num) {
-				sb.append(x).append(" ");
-			}
-			set.add(sb.toString());
-			
-			return;
-		}
-		
-		for(int i=start; i<N; i++) {
-				num[depth] = input[i];
-				dfs(i, depth + 1);
-		}
-		
-	}
-	
+
+    static int N, M;
+    static int[] nums;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums); 
+        dfs(0, 0);
+
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1; 
+        for (int i = start; i < N; i++) {
+            if (nums[i] == prev) continue;
+
+            arr[depth] = nums[i];
+            prev = nums[i];
+            dfs(i, depth + 1); 
+        }
+    }
 }
