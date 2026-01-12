a2ce587fcf4f52a431bc563fb254f113d8ff72b2
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 12:08:42 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,58 +1,37 @@
-/*
-문제
-자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
-1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
-고른 수열은 오름차순이어야 한다.
-
-입력
-첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
-
-출력
-한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
-수열은 사전 순으로 증가하는 순서로 출력해야 한다.
- */
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[] num;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		num = new int[M];
-		visited = new boolean[N+1];
-		
-		dfs(1, 0);
-		
-		System.out.println(sb.toString());
-	}
-	
-	static void dfs(int start, int depth) {
-		if (depth == M) {
-			for(int x : num) {
-				sb.append(x).append(" ");
-			}
-			
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=start; i<=N; i++) {
-			num[depth] = i;
-			dfs(i, depth+1);
-		}
-		
-	}
-	
+
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(1, 0);
+
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
 }
