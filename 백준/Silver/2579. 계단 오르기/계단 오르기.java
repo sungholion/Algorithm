685e95112bc59051c3c6f87fc0e07a75eaa881ef
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 09:18:34 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,40 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-
-public class Main {
-    static Integer[] dp;
-    static int[] stair;
-    public static void main(String[] args) throws IOException {
+class Main{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        dp = new Integer[n+1];
-        stair = new int[n+1];
-        for (int i = 1; i <= n; i++)
-            stair[i] = Integer.parseInt(br.readLine());
-
-        dp[0] = stair[0];
-        dp[1] = stair[1];
-
-        if(n >= 2)
-            dp[2] = stair[1] + stair[2];
-
-        sb.append(find(n)).append("\n");
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    public static int find(int N) {
-        if(dp[N] == null) {
-            dp[N] = Math.max(find(N - 2), find(N - 3) + stair[N - 1]) + stair[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] stairs =  new int[N+1];
+        int[] dp =  new int[N+1];
+        for(int i=1;i<=N;i++){
+            stairs[i] = Integer.parseInt(br.readLine());
         }
-        return dp[N];
+
+        dp[1] = stairs[1];
+        if(N >= 2) dp[2] = stairs[1] + stairs[2];
+
+        for(int i = 3; i <= N; i++){
+            dp[i] = Math.max((dp[i-2] + stairs[i]), (dp[i-3] + stairs[i-1] + stairs[i]));
+        }
+        sb.append(dp[N]).append("\n");
+        System.out.println(sb.toString());
     }
 
 }
