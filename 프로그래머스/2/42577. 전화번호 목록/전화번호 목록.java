707eda06971afca35eab79d9b3fd63d7585576a3
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-12 16:37:02 +0900

    [level 2] Title: 전화번호 목록, Time: 344.35 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         Arrays.sort(phone_book);
-
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            String temp = phone_book[i];
-            String next = phone_book[i + 1];
-            if (next.startsWith(temp)) { 
-                return false;
-            }
+        for(int i = 0; i < phone_book.length - 1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
-        return true;
+        return answer;
     }
 }
