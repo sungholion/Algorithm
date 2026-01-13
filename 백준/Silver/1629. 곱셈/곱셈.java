081fc343979829a19e4b9d8f5bd4e2d06103bcdb
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-13 12:56:00 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,50 +1,28 @@
+import java.io.*;
+import java.util.*;
 
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
 public class Main {
- 
-	public static long C;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		long A = Long.parseLong(st.nextToken());
-		long B = Long.parseLong(st.nextToken());
-		C = Long.parseLong(st.nextToken());
-		
-		System.out.println(pow(A, B));
-	}
-	
-	// A^exponent
-	public static long pow(long A, long exponent) {
-		
-		// 지수가 1일 경우 A^1 이므로 A를 그대로 리턴
-		if(exponent == 1) {
-			return A % C;
-		}
-		
-		// 지수의 절반에 해당하는 A^(exponent / 2) 을 구한다.
-		long temp = pow(A, exponent / 2);
-		
-		/*
-		 * 현재 지수가 홀 수 였다면
-		 * A^(exponent / 2) * A^(exponent / 2) * A 이므로
-		 * A를 한 번 더 곱해주어야 한다.
-		 * 
-		 * ex) A^9 = A^4 * A^4 * A
-		 */
-		if(exponent % 2 == 1) {
-			return (temp * temp % C) * A % C;
-		}
-		return temp * temp % C;
-		
-	}
-	
+    static long A, B, C;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+        C = Long.parseLong(st.nextToken());
+
+        System.out.println(pow(A, B) % C);
+    }
+
+    static long pow(long a, long b) {
+        if (b == 0) return 1;
+
+        long half = pow(a, b / 2);
+        long result = (half * half) % C;
+
+        // b가 홀수면 한 번 더 a 곱하기
+        if (b % 2 == 1) result = (result * (a % C)) % C;
+
+        return result;
+    }
 }
