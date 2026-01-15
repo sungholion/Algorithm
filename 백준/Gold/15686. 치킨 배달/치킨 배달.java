dd8fb3203d2af47ae3f83060b29ab3b32a1c7900
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-15 09:04:12 +0900

    [Gold V] Title: 치킨 배달, Time: 168 ms, Memory: 15672 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,78 +1,60 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    static int N, M;
+    static List<int[]> houses = new ArrayList<>();
+    static List<int[]> chickens = new ArrayList<>();
+    static int[] selected;
+    static int ans = Integer.MAX_VALUE;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-public class Main{
-	static int N, M;
-	static List<int[]> chicken, home;
-	static boolean[] isSelected;
-	static int res = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());	// 도시의 크기 N x N
-		M = Integer.parseInt(st.nextToken());	// 폐업시키지 않을 치킨집 개수
-		
-		home = new ArrayList<>();
-		chicken = new ArrayList<>();
-		
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				int temp = Integer.parseInt(st.nextToken());
-				
-				if(temp == 0) continue;	// 빈칸
-				else if(temp == 1) home.add(new int[] {i,j});	// 집
-				else if(temp == 2) chicken.add(new int[] {i,j});	// 치킨집
-			}
-		}
-		
-		isSelected = new boolean[chicken.size()];
-		
-		comb(-1, 0);
-		
-		bw.write(res+"");
-		
-		bw.flush();
-		bw.close();
-		br.close();
-		
-	}
-	
-	static void comb(int idx, int cnt) {
-		if(cnt == M) {
-			int dis = 0;
-			
-			for(int[] h : home) {
-				int tmp = Integer.MAX_VALUE;
-				for(int i=0; i<isSelected.length; i++) {
-					if(isSelected[i]) {
-						tmp = Math.min(tmp, Math.abs(h[0] - chicken.get(i)[0]) + Math.abs(h[1] - chicken.get(i)[1]));
-					}
-				}
-				dis += tmp;
-			}
-			res = Math.min(res, dis);
-			return;
-		}
-		
-		for(int i=idx+1; i<chicken.size(); i++) {
-			isSelected[i] = true;
-			comb(i, cnt + 1);
-			isSelected[i] = false;
-		}
-		
-	}
-	
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                int info = Integer.parseInt(st.nextToken());
+                if(info == 1) houses.add(new int[]{i, j});
+                else if(info == 2) chickens.add(new int[]{i, j});
+            }
+        }
+
+        selected = new int[M];
+        comb(0,0);
+
+        System.out.print(ans);
+    }
+
+    static void comb(int idx, int start){
+        if(idx == M){
+            ans = Math.min(ans, calcCity());
+            return;
+        }
+
+        for(int i = start; i < chickens.size(); i++){
+            selected[idx] = i;
+            comb(idx + 1, i + 1);
+        }
+    }
+
+    static int calcCity(){
+        int sum = 0;
+        for(int[] h : houses){
+            int min = Integer.MAX_VALUE;
+            for(int i = 0; i < M; i++){
+                int[] chicken = chickens.get(selected[i]);
+                int dis = Math.abs(h[0] - chicken[0]) +  Math.abs(h[1] - chicken[1]);
+                if(dis < min) min = dis;
+            }
+            sum += min;
+
+            if(sum >= ans) return sum;
+        }
+        return sum;
+    }
 }
