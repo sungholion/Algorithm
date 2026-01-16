c87e3f27d1101eb57ce5d09b8d16323c434c7746
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-16 10:35:51 +0900

    [Silver III] Title: 숫자 야구, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -2,86 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class Hint {
+        int[] d = new int[3];
+        int s, b;
+        Hint(int num, int s, int b) {
+            d[0] = num / 100;
+            d[1] = (num / 10) % 10;
+            d[2] = num % 10;
+            this.s = s;
+            this.b = b;
+        }
+    }
 
-   static class Baseball {
-      String num;
-      int strike;
-      int ball;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
+        Hint[] hints = new Hint[N];
 
-      Baseball(String num, int strike, int ball) {
-         this.num = num;
-         this.strike = strike;
-         this.ball = ball;
-      }
-   }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            hints[i] = new Hint(num, s, b);
+        }
 
-   static ArrayList<Baseball> baseball = new ArrayList<>();
+        int ans = 0;
 
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-      StringTokenizer st;
+        for (int a = 1; a <= 9; a++) {
+            for (int c = 1; c <= 9; c++) {
+                if (c == a) continue;
+                for (int e = 1; e <= 9; e++) {
+                    if (e == a || e == c) continue;
 
-      int n = Integer.parseInt(br.readLine());
+                    int[] cand = {a, c, e};
+                    boolean ok = true;
 
-      // 질문 입력받기
-      for (int i = 0; i < n; i++) {
-         st = new StringTokenizer(br.readLine());
-         baseball.add(new Baseball(st.nextToken(), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-      }
+                    for (Hint h : hints) {
+                        int strike = 0, ball = 0;
+                        
+                        for (int k = 0; k < 3; k++) {
+                            if (cand[k] == h.d[k]) strike++;
+                        }
 
-      int cnt = 0;
+                        for (int i = 0; i < 3; i++) {
+                            for (int j = 0; j < 3; j++) {
+                                if (i == j) continue;
+                                if (cand[i] == h.d[j]) ball++;
+                            }
+                        }
 
-      // 123 ~ 987까지의 모든 세 자리 수 탐색
-      for (int i = 123; i <= 987; i++) {
-         if (!isValid(i)) continue; // 각 자리 숫자가 중복되지 않는 유효한 숫자인지 체크
+                        if (strike != h.s || ball != h.b) {
+                            ok = false;
+                            break;
+                        }
+                    }
 
-         boolean isPossible = true;
-
-         // 모든 질문에 대해 검사
-         for (Baseball b : baseball) {
-            int strike = 0;
-            int ball = 0;
-
-            String guess = Integer.toString(i); // 추측하는 숫자
-            String target = b.num; // 주어진 질문의 숫자
-
-            // 스트라이크 계산
-            for (int j = 0; j < 3; j++) {
-               if (guess.charAt(j) == target.charAt(j)) {
-                  strike++;
-               }
+                    if (ok) ans++;
+                }
             }
+        }
 
-            // 볼 계산
-            for (int j = 0; j < 3; j++) {
-               if (guess.charAt(j) != target.charAt(j) && target.contains(Character.toString(guess.charAt(j)))) {
-                  ball++;
-               }
-            }
-
-            // 질문의 strike와 ball이 일치하지 않으면 후보에서 제외
-            if (strike != b.strike || ball != b.ball) {
-               isPossible = false;
-               break;
-            }
-         }
-
-         if (isPossible) cnt++; // 가능한 답이면 카운트 증가
-      }
-
-      bw.write(Integer.toString(cnt));
-      bw.flush();
-      br.close();
-      bw.close();
-   }
-
-   // 각 자리 숫자가 중복되지 않도록 하는 함수
-   static boolean isValid(int num) {
-      String s = Integer.toString(num);
-      
-      if(s.charAt(0) == '0' || s.charAt(1) == '0' || s.charAt(2) == '0') return false;
-      
-      return s.charAt(0) != s.charAt(1) && s.charAt(1) != s.charAt(2) && s.charAt(0) != s.charAt(2);
-   }
+        System.out.println(ans);
+    }
 }
