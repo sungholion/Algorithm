e47ee91f18625016983326079e0f49aab6e85640
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-24 20:58:36 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 112 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String s = br.readLine();
+            if (s == null) break;      
+            if (s.equals("end")) break;
+
+            if (s.equals("animal")) sb.append("Panthera tigris\n");
+            else if (s.equals("flower")) sb.append("Forsythia koreana\n");
+            else if (s.equals("tree")) sb.append("Pinus densiflora\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
