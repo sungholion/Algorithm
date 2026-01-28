6f53d0b97ded35e108c8623a229545add3941d6b
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-28 13:27:19 +0900

    [Bronze V] Title: WARBOY, Time: 88 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/26082. WARBOY/WARBOY.java
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -1,15 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-        System.out.println(C * (B/A*3));
+
+        int competition = B / A;
+        int warboy = competition * 3;
+        sb.append(warboy * C);
+
+        System.out.print(sb);
     }
 }
