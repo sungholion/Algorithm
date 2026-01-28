55f0c49facc7dd4c074e3ffc39c0ec828c0f9392
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-28 13:21:26 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,21 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-class Main{
-    public static void main(String[] args) throws Exception{
+public class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        long[] dp =  new long[101];
-        dp[1] = 1; dp[2] = 1; dp[3] = 1; dp[4] = 2; dp[5] = 2;
-        for(int i = 6; i <= 100; i++) dp[i] = dp[i-2] + dp[i-3];
-        int T = Integer.parseInt(br.readLine());
-        for(int t=0;t<T;t++){
-            int x = Integer.parseInt(br.readLine());
-            sb.append(dp[x]).append("\n");
+        long[] dp = new long[101];
+        dp[1] = 1; dp[2] = 1; dp[3] = 1;
+        for(int i = 4; i <= 100; i++){
+            dp[i] = dp[i - 2] + dp[i - 3];
         }
-        
-        System.out.print(sb.toString());
-    }
+        int T = Integer.parseInt(br.readLine());
+        for(int i  = 0; i < T; i++){
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
 
+        System.out.print(sb);
+    }
 }
