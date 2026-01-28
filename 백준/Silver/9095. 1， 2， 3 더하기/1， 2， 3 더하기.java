b808881140cde5946d8238579cfa666e6a0e6cd7
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-28 13:06:23 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,32 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] map = new int[101];
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-        int[] dp = new int[12];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        for(int i = 4; i <= 11; i++) {
+        int[] dp = new int[11];
+        dp[1] = 1; dp[2] = 2; dp[3] = 4;
+        for(int i = 4; i < 11; i++){
             dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-        for(int t = 0; t < T; t++) {
-            int x = Integer.parseInt(br.readLine());
-            sb.append(dp[x]).append("\n");
+        for(int i = 0; i < T; i++){
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
         }
-        System.out.println(sb);
-
-        /*
-        1 = 1
-        2 = 1 + 1 / 2
-        3 = 1 + 1 + 1 / 1 + 2 / 3
-
-        4 = 1 + 1 + 1 + 1 / 1 + 1 + 2 / 1 + 2 + 1 / 2 + 1 + 1 / 2 + 2 / 1 + 3 / 3 + 1
-         */
-
+        System.out.print(sb);
     }
 }
