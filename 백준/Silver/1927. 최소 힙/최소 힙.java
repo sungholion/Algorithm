87aed9ca88e9206f49e8f318f8aa5e9e2f883de7
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-29 11:18:45 +0900

    [Silver II] Title: 최소 힙, Time: 240 ms, Memory: 26392 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,23 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+class Main{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int N = Integer.parseInt(br.readLine());
-        for(int i = 0; i < N; i++) {
+        int N  = Integer.parseInt(br.readLine());
+        for(int i=0;i<N;i++){
             int x = Integer.parseInt(br.readLine());
+            if(x != 0) pq.offer(x);
             if(x == 0){
-                if(pq.isEmpty()) sb.append("0\n");
-                else sb.append(pq.poll()).append("\n");
-            } else{
-                pq.offer(x);
+                sb.append(pq.isEmpty()? 0 :pq.poll()).append("\n");
             }
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
