128b735c6cf29ac654f0f044159ada44a90b93a2
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-29 09:56:48 +0900

    [Bronze IV] Title: 모음의 개수, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -1,31 +1,24 @@
-import java.util.Scanner;
+    import java.io.*;
+    import java.math.BigInteger;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
 
-        for(;;) {
-            String str = sc.nextLine();
-            int count = 0;
-            if(str.equals("#")) {
-                break;
+            String str;
+            while(!(str = br.readLine()).equals("#")){
+                int cnt = 0;
+                for(int i = 0; i < str.length(); i++){
+                    char c = str.charAt(i);
+                    c = Character.toLowerCase(c);
+                    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                        cnt++;
+                    }
+                }
+                sb.append(cnt).append("\n");
             }
-
-            str = str.toLowerCase();
-
-            for(int i=0; i<str.length(); i++) {
-               switch(str.charAt(i)) {
-                     case 'a':
-                     case 'e':
-                     case 'i':
-                     case 'o':   
-                     case 'u':
-                        count++;
-                    default: 
-                        break;
-               }
-            }            
-            System.out.println(count);
+            System.out.print(sb);
         }
     }
-}
