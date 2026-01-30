f2f90b3b82bcf137e0dc69b0634548f40220c458
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-01-30 12:48:44 +0900

    [Gold V] Title: 암호 만들기, Time: 108 ms, Memory: 17788 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,52 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int L, C;
-	static char[] input, pw;
+    static int L, C;
+    static StringBuilder sb = new StringBuilder();
+    static char[] pwOrg;
+    static char[] pick;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        pwOrg = new char[C];
+        pick = new char[L];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < C; i++)
+            pwOrg[i] = st.nextToken().charAt(0);
 
-		L = Integer.parseInt(st.nextToken()); 
-		C = Integer.parseInt(st.nextToken()); 
+        Arrays.sort(pwOrg); // 암호는 증가하는 순서로만
+        comb(0, 0);
 
-		pw = new char[L]; // 암호
-		input = new char[C]; // 문자의 개수
+        System.out.print(sb);
+    }
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < C; i++) 
-			input[i] = st.nextToken().charAt(0);
-		
-		Arrays.sort(input);
-		makePw(0, 0);
-	}
+    static void comb(int start, int depth){
+//        if (s.length - start < 6 - depth) return; // 가지치기?
 
-	public static void makePw(int start, int cnt) {
+        if(depth == L){
+            boolean canMake = false;
+            int cnt1 = 0; // 모음 개수 최소 1개
+            int cnt2 = 0; // 자음 개수 최소 2개
+            for(int i = 0; i < L; i++){
+                if(pick[i] == 'a' || pick[i] == 'e' || pick[i] == 'i'
+                || pick[i] == 'o' || pick[i] == 'u'){
+                    cnt1++;
+                }
+                else{
+                    cnt2++;
+                }
+            }
 
-		if (cnt == L) {
-			if (checkCondition(pw)) 
-				System.out.println(pw);
-			return;
-		}
-		
-		for (int i = start; i < C; i++) {
-			pw[cnt] = input[i];
-			makePw(i + 1, cnt + 1);
-		}
-	}
+            if(cnt1 < 1 || cnt2 < 2) return;
 
-	public static boolean checkCondition(char[] pw) {
-		int vow = 0; // 모음
-		int con = 0; 
+            for(int i = 0; i < L; i++)
+                sb.append(pick[i]);
+            sb.append("\n");
+            return;
+        }
 
-		for (char c : pw) {
-			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') 
-				vow++;
-			else con++;
-		}
+        for(int i = start; i < pwOrg.length; i++){
+            pick[depth] = pwOrg[i];
+            comb(i + 1, depth + 1);
+        }
 
-		if (vow >= 1 && con >= 2) return true;
-		return false;
-	}
+    }
 }
