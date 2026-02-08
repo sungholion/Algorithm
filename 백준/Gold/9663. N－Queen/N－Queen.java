f3b2cfcd07dfc5a8c35b8958425d4b1a5fd8a4b4
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-08 13:13:14 +0900

    [Gold IV] Title: N-Queen, Time: 2520 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,45 +1,47 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	static int n;
-	static int cnt;
-	static int[] arr;
-    public static void main(String[] args) throws NumberFormatException, IOException {
+
+    static int N;
+    static int answer = 0;
+
+    static boolean[] col;
+    static boolean[] diag1;
+    static boolean[] diag2;
+
+    static void dfs(int row) {
+        if (row == N) {
+            answer++;
+            return;
+        }
+
+        for (int c = 0; c < N; c++) {
+            if (col[c]) continue;
+            if (diag1[row - c + N]) continue;
+            if (diag2[row + c]) continue;
+
+            col[c] = true;
+            diag1[row - c + N] = true;
+            diag2[row + c] = true;
+
+            dfs(row + 1);
+
+            col[c] = false;
+            diag1[row - c + N] = false;
+            diag2[row + c] = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        
-        nqueens(0);
-        System.out.println(cnt);
-        
-    }
-    
-    static void nqueens(int d) {
-    	if(d == n) {
-    		cnt++;
-    		return;
-    	}
-    	
-    	for(int i=0; i<n; i++) {
-    		arr[d] = i;
-    		
-    		if(promising(d)) {
-    			nqueens(d+1);
-    		}
-    	}
-    }
-    
-    static boolean promising(int col) {
-    	for(int i=0; i< col; i++) {	// 같은 열
-    		if(arr[col] == arr[i]) return false;
-    		else if(Math.abs(col-i) == Math.abs(arr[col] - arr[i])) return false;
-    	}
-    	return true;
+        N = Integer.parseInt(br.readLine());
+
+        col = new boolean[N];
+        diag1 = new boolean[2 * N];
+        diag2 = new boolean[2 * N];
+
+        dfs(0);
+        System.out.println(answer);
     }
 }
