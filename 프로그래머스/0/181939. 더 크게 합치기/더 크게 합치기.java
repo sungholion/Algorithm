df686a1a8b50bb078b6647df87fe123f58714a9a
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-14 12:26:52 +0900

    [level 0] Title: 더 크게 합치기, Time: 8.20 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
+        String str1 = a + "" + b;
+        int ab = Integer.parseInt(str1);
+        String str2 = b + "" + a;
+        int ba = Integer.parseInt(str2);
         
-        String temp1 = a + "" + b;
-        String temp2 = b + "" + a;
-        
-        int result1 = Integer.parseInt(temp1);
-        int result2 = Integer.parseInt(temp2);
-        
-        answer = (result1 > result2) ? result1 : result2;
-        
-        return answer;
+        return ab > ba ? ab : ba;
     }
 }
