c964d33ab37d3376aa9546b3fd8a1b3e7a8b4191
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-14 19:17:32 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 88 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,24 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
+        long[] fibo = new long[N + 1];
+        fibo[0] = 1;
+        fibo[1] = 1;
 
-        long[] dp = new long[91];
-        dp[1] = 1;
-        for(int i = 2; i <= N; i++)
-            dp[i] = dp[i - 1] + dp[i - 2];
-
-        sb.append(dp[N]);
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        for(int i = 2; i <= N; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        System.out.println(fibo[N-1]);
     }
+
+
 }
