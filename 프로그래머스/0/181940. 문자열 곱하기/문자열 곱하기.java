7ea33668fe2127b410d2d134a3d826ac38226cf7
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-14 12:24:37 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.06 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public String solution(String my_string, int k) {
-        String answer = "";
-        
-        for(int i=0; i<k; i++){
-            answer += my_string;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < k; i++){
+            sb.append(my_string);
         }
         
-        return answer;
+        return sb.toString();
     }
 }
