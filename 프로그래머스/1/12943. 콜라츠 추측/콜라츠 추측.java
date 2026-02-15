65bf3594626a60eadafcf6f443324765bbeb845c
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-15 17:44:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,15 +1,15 @@
 class Solution {
     public int solution(int num) {
-        long x = num;
-        if(x == 1) return 0;
+        if(num == 1) return 0;
+        long n = num;
         int cnt = 0;
-        while(x != 1){
-            if((x % 2) == 0) x /= 2;
-            else x = (x * 3) + 1;
-            cnt++;
-            if(cnt == 500) return -1;
-        }
         
+        while(n != 1){
+            if(cnt == 500) return -1;
+            if(n % 2 == 0) n /= 2;
+            else n = (n * 3) + 1;
+            cnt++;
+        }
         return cnt;
     }
 }
