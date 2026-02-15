813daa1061a2da8081e15b8fff2757dce936f6cc
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-15 20:35:12 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,12 +1,15 @@
+// 3중 for문 OR DFS 깊이 3까지
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
         int len = number.length;
-        for(int i=0; i<len-2; i++){
-            for(int j=i+1; j<len-1; j++){
-                for(int k=j+1; k<len; k++){
-                    if((number[i] + number[j] + number[k]) == 0)
+        for(int i = 0; i < len - 2; i++){
+            for(int j = i + 1; j < len - 1; j++){
+                for(int k = j + 1; k < len; k++){
+                    int sum = number[i] + number[j] + number[k];
+                    if(sum == 0){
                         answer++;
+                    }
                 }
             }
         }
