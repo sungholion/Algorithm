10e6a8df44c83d6ce2bb563d48dddd18e2db5dfb
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-15 22:32:33 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.32 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,22 +1,18 @@
 class Solution {
     public int solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        while(n >= 3){
-            sb.append(n % 3);
+        int answer = 0;
+        String str = "";
+        while(n != 0){
+            int x = n % 3;
             n /= 3;
+            str += x;
         }
-        sb.append(n);
-        
-        String str = sb.toString();
         System.out.print(str);
-        int sum = 0;
-        int mul = 1;
-        for(int i=str.length()-1; i>=0; i--){
-            int num = str.charAt(i) - '0';
-            sum += num * mul;
-            mul *= 3;
+        int base = 0;
+        for(int i = str.length() - 1; i >= 0; i--){
+            int x = str.charAt(i) - '0';
+            answer += x * Math.pow(3, base++);
         }
-        
-        return sum;
+        return answer;
     }
 }
