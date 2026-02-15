c0f48a5af68a11895a16f791473d0a9bd670c7e5
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-15 16:41:19 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        int temp = x;
         int sum = 0;
-        
-        while(temp != 0){
+        int temp = x;
+        while(temp > 0){
             sum += temp % 10;
             temp /= 10;
         }
         
-        if((x % sum) != 0) answer = false;
-        
-        return answer;
+        if(x % sum == 0) return true;
+        else return false;
     }
 }
