02481d803e89b9f32f0913e51d1d8c077c9864d5
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-16 11:42:53 +0900

    [Silver III] Title: 두 수의 합, Time: 284 ms, Memory: 25028 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,45 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		int[] a = new int[n];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) 
-			a[i] = Integer.parseInt(st.nextToken());
-		Arrays.sort(a);
-		
-		int x = Integer.parseInt(br.readLine());
-		
-		int cnt = 0;
-		
-		int i=0;
-		int j=n-1;
-		while(i<j) {
-			int tmp = a[i] + a[j];
-			
-			if(tmp == x) cnt++;
-			if(tmp < x) i++;
-			else j--;
-		}
-		
-		
-		bw.write(cnt+"");
-		
-		bw.flush();
-		bw.close();
-		br.close();
-		
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	}
+        int answer = 0;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int x = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right){
+            int sum = arr[left] + arr[right];
+
+            if(sum == x){
+                answer++;
+                left++;
+                right--;
+            } else if(sum < x){
+                left++;
+            } else if(sum > x){
+                right--;
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
 }
