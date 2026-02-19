c201ce6ebedc621565b6043537d9b5f947e41d07
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-19 10:42:36 +0900

    [Silver I] Title: 그림, Time: 412 ms, Memory: 46560 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,87 +1,64 @@
 import java.io.*;
 import java.util.*;
 
-class Coord{
-    int x;
-    int y;
-
-    public Coord(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
-public class Main{
+public class Main {
     static int n, m;
     static int[][] map;
-    static boolean[][] vis;
+    static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int cnt;
-    static int maxArea;
-    
-    public static void main(String[] args) throws IOException{
+    static int maxWidth;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         map = new int[n][m];
-        vis = new boolean[n][m];
-        for(int i=0; i<n; i++){
+        visited = new boolean[n][m];
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++){
+            for(int j = 0; j < m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(map[i][j] == 1 && !vis[i][j]){
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(map[i][j] == 1 && visited[i][j] == false){
+                    bfs(i, j);
                     cnt++;
-                    int area = bfs(new Coord(i, j));
-                    if(area > maxArea){
-                        maxArea = area;
-                    }
                 }
             }
         }
-
-        sb.append(cnt).append("\n").append(maxArea).append("\n");
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(cnt);
+        System.out.print(maxWidth);
     }
+    static void bfs(int x, int y){
+        ArrayDeque<int[]> q =  new ArrayDeque<>();
+        q.offer(new int[]{x, y});
+        visited[x][y] = true;
 
-    static int bfs(Coord start){
-        Queue<Coord> q = new ArrayDeque<>();
-
-        vis[start.x][start.y] = true;
-        q.offer(start);
-
-        int area = 1;
+        int width = 1;
         while(!q.isEmpty()){
-            Coord cur = q.poll();
+            int[] cur = q.poll();
+            for(int i = 0; i < 4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
 
-            for(int i=0; i<4; i++){
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m || map[nx][ny] == 0 || vis[nx][ny]){
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny])
                     continue;
+                if(map[nx][ny] == 1){
+                    q.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    width++;
                 }
-
-                area++;
-                vis[nx][ny] = true;
-                q.offer(new Coord(nx, ny));
             }
         }
-        return area;
-    }
 
+        if(width > maxWidth){
+            maxWidth = width;
+        }
+    }
 }
