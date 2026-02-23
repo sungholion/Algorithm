998f61a4c59537ad674e27123ee054c814e91607
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-02-23 11:10:45 +0900

    [Silver I] Title: 영역 구하기, Time: 104 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -18,8 +18,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
-        visited = new boolean[N][M];
+        map = new int[M][N];
+        visited = new boolean[M][N];
 
         for(int k = 0;  k < K; k++){
             st = new StringTokenizer(br.readLine());
@@ -28,16 +28,16 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            for(int i = x1; i < x2; i++){
-                for(int j = y1; j < y2; j++){
+            for(int i = y1; i < y2; i++){
+                for(int j = x1; j < x2; j++){
                     map[i][j] = 1;
                 }
             }
         }
 
         int cnt = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
                 if(map[i][j] == 0 && !visited[i][j]){
                     cnt++;
                     bfs(i, j);
@@ -65,7 +65,7 @@ public class Main {
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
 
-                if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if(nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
                 if(map[nx][ny] == 0 && !visited[nx][ny]){
                     q.offer(new int[]{nx, ny});
                     visited[nx][ny] = true;
