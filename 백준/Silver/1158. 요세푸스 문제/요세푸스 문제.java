3358b3224b02ee2d0dbca3d206480bf8594851b4
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-03-04 16:51:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 244 ms, Memory: 16968 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,35 +1,29 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	int N = Integer.parseInt(st.nextToken());
-	int K = Integer.parseInt(st.nextToken());
-	
-	LinkedList<Integer> lst = new LinkedList<>();
-	for(int i=1; i<=N; i++) lst.add(i);
-	
-	bw.write("<");
-	int next = 0;
-	while(!lst.isEmpty()) {
-		next = (next + K -1) % lst.size();
-		bw.write(String.valueOf(lst.get(next)));
-		if(lst.size()!=1) bw.write(", ");
-		lst.remove(next);
-	}
-	bw.write(">");
-	
-	bw.flush();
-	bw.close();
-	br.close();
-	
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        sb.append("<");
+        for(int i = 1; i <= N; i++) q.offer(i);
+        while(q.size()!= 1){
+            for(int i = 0; i < K - 1; i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll());
+        sb.append(">");
+        System.out.print(sb.toString());
+
+        // 6 7 1 2 4 5
+        // 1 4 5 7
+    }
 }
