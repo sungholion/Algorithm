61a92f9b6196c95581703a5ae804ad86268df2cb
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-03-05 18:47:50 +0900

    [Bronze V] Title: 홍익대학교, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/16394. 홍익대학교/홍익대학교.java
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -1,10 +1,9 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.print(n - 1946);
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N - 1946);
     }
 }
