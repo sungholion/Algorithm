fb86984d8c036de3867c1c0f76098f73bccbd178
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-03-10 23:31:58 +0900

    [Bronze V] Title: 크냐?, Time: 112 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.java
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -1,23 +1,19 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            
-            if (a == 0 && b == 0) break;  
-            if (a > b) {
-                sb.append("Yes\n");
-            } else {
-                sb.append("No\n");
-            }
+
+            if (a == 0 && b == 0) break;
+
+            if (a > b) System.out.println("Yes");
+            else System.out.println("No");
         }
-        System.out.print(sb);
     }
 }
