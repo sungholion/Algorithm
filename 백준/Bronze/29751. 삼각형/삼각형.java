5edf33f13e52f399364e35a62dce319110077e52
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-03-29 18:42:49 +0900

    [Bronze V] Title: 삼각형, Time: 108 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Bronze/29751. 삼각형/삼각형.java
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -1,21 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
- 
+
 public class Main {
- 
     public static void main(String[] args) throws IOException {
- 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
- 
+        
         int W = Integer.parseInt(st.nextToken());
         int H = Integer.parseInt(st.nextToken());
- 
-        double result = 0;
- 
-        result = (W*H)/2.0;
-        System.out.printf("%.1f", result);
+        
+        double area = (W * H) / 2.0;
+        System.out.printf("%.1f", area);
     }
 }
