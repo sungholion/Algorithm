a9d047bb66a05be92b882912db89bf50ed8c59d3
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-03-30 10:38:50 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        switch (s) {
+            case "NLCS":
+                System.out.println("North London Collegiate School");
+                break;
+            case "BHA":
+                System.out.println("Branksome Hall Asia");
+                break;
+            case "KIS":
+                System.out.println("Korea International School");
+                break;
+            case "SJA":
+                System.out.println("St. Johnsbury Academy");
+                break;
+        }
+    }
+}
