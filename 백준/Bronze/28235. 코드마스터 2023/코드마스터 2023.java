a7eaf72967b67eb476f5f4ba33459a91af9bbce1
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-04-03 07:57:48 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -1,27 +1,13 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+        String s = br.readLine();
 
-        String str = br.readLine();
-        if (str.equals("SONGDO")) {
-            sb.append("HIGHSCHOOL");
-        } else if (str.equals("CODE")) {
-            sb.append("MASTER");
-        } else if (str.equals("2023")) {
-            sb.append("0611");
-        } else if (str.equals("ALGORITHM")) {
-            sb.append("CONTEST");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        if (s.equals("SONGDO")) System.out.println("HIGHSCHOOL");
+        else if (s.equals("CODE")) System.out.println("MASTER");
+        else if (s.equals("2023")) System.out.println("0611");
+        else if (s.equals("ALGORITHM")) System.out.println("CONTEST");
     }
 }
