bc42edbf5c76eff2df42f296e9a5b64ea31c1a99
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-04-06 08:03:45 +0900

    [Bronze V] Title: 라면 공식, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+
+            int result = A * (X - 1) + B;
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
