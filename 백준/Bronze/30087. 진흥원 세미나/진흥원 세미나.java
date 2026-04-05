5bbf689dfe97e86b15fbda31d7a13fe1344b6678
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-04-05 14:53:08 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        Map<String, String> map = new HashMap<>();
+        map.put("Algorithm", "204");
+        map.put("DataAnalysis", "207");
+        map.put("ArtificialIntelligence", "302");
+        map.put("CyberSecurity", "B101");
+        map.put("Network", "303");
+        map.put("Startup", "501");
+        map.put("TestStrategy", "105");
+        
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            System.out.println(map.get(input));
+        }
+    }
+}
