7b4d62eb273251dc9df88f60f157741f64f92aa3
SUNGHO CHO <102495970+sungholion@users.noreply.github.com>
2026-04-08 09:18:30 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.java
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.java
@@ -1,24 +1,16 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-
-        if(a > b)
-            sb.append("flight");
-        else
-            sb.append("high speed rail");
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        
+        int S = Integer.parseInt(br.readLine());
+        int F = Integer.parseInt(br.readLine());
+        
+        if (S <= F) {
+            System.out.println("high speed rail");
+        } else {
+            System.out.println("flight");
+        }
     }
 }
